protolambda/eth2-beaconchain-explorer

index/export transactions and execution payloads

types/exporter.go
@@ -91,6 +91,35 @@ type Block struct {
 	Deposits          []*Deposit
 	VoluntaryExits    []*VoluntaryExit
 	Canonical         bool
+	ExecutionPayload  *ExecutionPayload
+}
+
+type Transaction struct {
+	Raw []byte
+	// Note: below values may be nil/0 if Raw fails to decode into a valid transaction
+	TxHash       []byte
+	AccountNonce uint64
+	// big endian
+	Price     []byte
+	GasLimit  uint64
+	Recipient []byte
+	// big endian
+	Amount  []byte
+	Payload []byte
+}
+
+type ExecutionPayload struct {
+	BlockHash    []byte
+	ParentHash   []byte
+	Coinbase     []byte
+	StateRoot    []byte
+	Number       uint64
+	GasLimit     uint64
+	GasUsed      uint64
+	Timestamp    uint64
+	ReceiptRoot  []byte
+	LogsBloom    []byte
+	Transactions []*Transaction
 }
 
 // Eth1Data is a struct to hold the ETH1 data

gobitfly/beaconchain

v1/types/exporter.go
@@ -94,24 +94,6 @@ type SyncAggregate struct {
 	SyncAggregateParticipation float64
 }
 
-type ExecutionPayload struct {
-	ParentHash    []byte
-	CoinBase      []byte
-	StateRoot     []byte
-	ReceiptRoot   []byte
-	LogsBloom     []byte
-	Random        []byte
-	BlockNumber   uint64
-	GasLimit      uint64
-	GasUsed       uint64
-	Timestamp     uint64
-	ExtraData     []byte
-	BaseFeePerGas []byte
-	BlockHash     []byte
-	// TODO
-	Transactions [][]byte
-}
-
 // Block is a struct to hold block data
 type Block struct {
 	Status            uint64
@@ -133,6 +115,40 @@ type Block struct {
 	SyncAggregate     *SyncAggregate    // warning: sync aggregate may be nil, for phase0 blocks
 	ExecutionPayload  *ExecutionPayload // warning: payload may be nil, for phase0/altair blocks
 	Canonical         bool
+}
+
+type Transaction struct {
+	Raw []byte
+	// Note: below values may be nil/0 if Raw fails to decode into a valid transaction
+	TxHash       []byte
+	AccountNonce uint64
+	// big endian
+	Price     []byte
+	GasLimit  uint64
+	Recipient []byte
+	// big endian
+	Amount  []byte
+	Payload []byte
+
+	MaxPriorityFeePerGas uint64
+	MaxFeePerGas uint64
+}
+
+type ExecutionPayload struct {
+	ParentHash    []byte
+	CoinBase      []byte
+	StateRoot     []byte
+	ReceiptRoot   []byte
+	LogsBloom     []byte
+	Random        []byte
+	BlockNumber   uint64
+	GasLimit      uint64
+	GasUsed       uint64
+	Timestamp     uint64
+	ExtraData     []byte
+	BaseFeePerGas []byte
+	BlockHash     []byte
+	Transactions []*Transaction
 }
 
 // Eth1Data is a struct to hold the ETH1 data