e66ca715c9e08d8f5c75be803f393fa9650371e1
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-01-18 15:56:03 +0100

    initial setup

---
+++ v2/frontend/app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style lang="scss">
+@import "./assets/css/colors.scss";
+@import "./assets/css/global.scss";
+
+</style>
---
+++ v2/frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div>
+    <NuxtLink to="/login" class="test-link">
+      Login
+    </NuxtLink>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.test-link{
+  background-color: var(--text-color);
+  color: var(--background-color);
+}
+</style>
