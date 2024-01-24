6d81e9b04613e964ad045484dd4b206daa16da7f
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-01-24 16:22:30 +0100

    font fix and dashboard

--- v2/frontend/components/bc/MainHeader.vue
+++ v2/frontend/components/bc/MainHeader.vue
@@ -7,37 +7,56 @@ const { latest } = storeToRefs(useLatestStateStore())
 </script>
 <template>
   <div class="header top">
-    <div>Current Epoch: {{ latest?.currentEpoch }}</div>
+    <div class="content">
+      <div>Current Epoch: {{ latest?.currentEpoch }}</div>
 
-    <NuxtLink to="/login">
-      Login
-    </NuxtLink>
+      <NuxtLink to="/login">
+        Login
+      </NuxtLink>
+    </div>
   </div>
   <div class="header bottom">
-    <NuxtLink to="/" class="">
-      <IconBeaconchainLogo alt="Beaconcha.in logo" />
-    </NuxtLink>
+    <div class="content">
+      <NuxtLink to="/" class="">
+        <IconBeaconchainLogo alt="Beaconcha.in logo" />
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.header{
+.header {
   width: 100%;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   color: var(--light-grey);
 
-  &.top{
+  &.top {
     height: var(--navbar-height);
     background-color: var(--dark-blue);
   }
-  &.bottom{
+
+  &.bottom {
     height: var(--navbar2-height);
     background-color: var(--container-background);
     color: var(--container-color);
     border-bottom: 1px solid var(--container-border-color);
   }
+
+  .content {
+    width: var(--content-width);
+    margin-left: var(--content-margin);
+    margin-right: var(--content-margin);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
 }
 
+.page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
