d757ee3e3528d409858cd61a2ce55b7e99faee76
RamiRond <ramirond@beaconcha.in>
2024-01-29 12:59:33 +0100

    aligned structure to UI

--- backend/api/structs/block.go
+++ backend/api/structs/block.go
@@ -6,88 +6,97 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-type Block struct {
-	Number        uint64           `json:"number"`
-	TxCount       uint16           `json:"tx_count"`
-	UncleCount    uint16           `json:"uncle_count,omitempty"`
-	Hash          Hash             `json:"hash"`
-	ParentHash    Hash             `json:"parent_hash"`
-	TxFees        *decimal.Decimal `json:"tx_fees"`
-	GasUsed       uint64           `json:"gas_used"`
-	GasLimit      uint64           `json:"gas_limit"`
-	Ts            time.Time        `json:"ts"`
-	State         string           `json:"state"`
-	Extra         string           `json:"extra,omitempty"`
-	DepositsCount uint64           `json:"deposits_count,omitempty"`
-
-	Pow     PoWInfo     `json:"pow,omitempty"`
-	Eip1559 Eip1559Info `json:"eip1559,omitempty"`
-	Pos     PosInfo     `json:"pos,omitempty"`
-	Blob    BlobInfo    `json:"blob,omitempty"` // eip 4844
-}
-
-type PoWInfo struct {
-	Miner          Address          `json:"miner"`
-	LowestGasPrice *decimal.Decimal `json:"lowest_gas_price"`
-	Difficulty     *decimal.Decimal `json:"difficulty"`
-}
-
-type Eip1559Info struct {
-	BurnedFees   *decimal.Decimal `json:"burned_fees"`
-	BurnedTxFees *decimal.Decimal `json:"burned_tx_fees"`
-	BaseFee      *decimal.Decimal `json:"base_fee"`
-}
-
-type PosInfo struct {
-	Epoch              uint64  `json:"epoch"`
-	EpochFinalized     bool    `json:"epoch_finalized"`
-	PrevEpochFinalized bool    `json:"prev_epoch_finalized"`
-	Proposer           uint64  `json:"proposer"`
-	ProposerName       string  `json:"proposer_name,omitempty"`
-	FeeRecipient       Address `json:"fee_recipient"`
-	BlockRoot          Hash    `json:"block_root"`
-	ParentRoot         Hash    `json:"parent_root"`
-	StateRoot          Hash    `json:"state_root"`
-	Signature          Hash    `json:"signature"`
-	RandaoReveal       Hash    `json:"randao_reveal"`
-	Graffiti           string  `json:"graffiti"`
-
-	Eth1Data struct {
-		DepositRoot   string `json:"deposit_root"`
-		DepositsCount string `json:"deposits_count"`
-		Hash          string `json:"hash"`
-	} `json:"eth1data"`
-
-	SyncAggregate struct {
-		Bits          []byte  `json:"bits"`
-		Signature     []byte  `json:"signature"`
-		Participation float64 `json:"participation"`
-	} `json:"sync_aggregate"`
-
-	ProposerSlashingsCount uint64 `json:"proposer_slashings_count,omitempty"`
-	AttesterSlashingsCount uint64 `json:"attester_slashings_count,omitempty"`
-	AttestationsCount      uint64 `json:"attestations_count"`
-	DepositCount           uint64 `json:"deposit_count"`
-	WithdrawalCount        uint64 `json:"withdrawal_count"`
-	BLSChangeCount         uint64 `json:"bls_change_count"`
-	VoluntaryExitsCount    uint64 `json:"voluntary_exits_count"`
-	VotesCount             uint64 `json:"votes_count"`
-	VotingValidatorsCount  uint64 `json:"voting_validators_count"`
-
-	MevTags          []MevTag         `json:"mev_tags,omitempty"`
-	MevReward        *decimal.Decimal `json:"mev_reward"`
-	MevBribe         *decimal.Decimal `json:"mev_bribe"`
-	MevIsValid       bool             `json:"mev_is_valid"`
-	MevRecipient     Address          `json:"mev_recipient"`
-	MevRecipientName string           `json:"mev_recipient_name,omitempty"`
-}
-
 type MevTag struct {
 	Name        string `json:"name"`
 	Summary     string `json:"summary"`
 	PublicLink  string `json:"public_url"`
 	Description string `json:"description"`
 	Color       string `json:"color"`
+}
+
+type Block struct {
+	// Old blocks
+	Miner          Address          `json:"miner,omitempty"`
+	MinerName      string           `json:"miner_name,omitempty"`
+	Rewards        *decimal.Decimal `json:"rewards,omitempty"`
+	TxFees         *decimal.Decimal `json:"tx_fees,omitempty"`
+	GasUsage       *decimal.Decimal `json:"gas_usage,omitempty"`
+	GasLimit       uint64           `json:"gas_limit,omitempty"`
+	LowestGasPrice *decimal.Decimal `json:"lowest_gas_price,omitempty"`
+	// Time
+	Difficulty *decimal.Decimal `json:"difficulty,omitempty"`
+	BaseFee    *decimal.Decimal `json:"base_fee,omitempty"`
+	BurnedFees *decimal.Decimal `json:"burned_fees,omitempty"`
+	Extra      string           `json:"extra,omitempty"`
+	Hash       Hash             `json:"hash,omitempty"`
+	ParentHash Hash             `json:"parent_hash,omitempty"`
+
+	// New blocks
+	MevTags                     []MevTag         `json:"mev_tags,omitempty"`
+	Epoch                       uint64           `json:"epoch,omitempty"`
+	Slot                        uint64           `json:"slot,omitempty"`
+	Block                       uint64           `json:"block"`
+	Proposer                    uint64           `json:"proposer,omitempty"`
+	ProposerRewardEL            *decimal.Decimal `json:"proposer_reward_el,omitempty"`
+	ProposerRewardCL            *decimal.Decimal `json:"proposer_reward_cl,omitempty"`
+	ProposerRewardRecipient     Address          `json:"proposer_reward_recipient,omitempty"`
+	ProposerRewardRecipientName string           `json:"proposer_reward_recipient_name,omitempty"`
+	Status                      string           `json:"status,omitempty"`
+	Time                        time.Time        `json:"time,omitempty"`
+	PriorityFees                *decimal.Decimal `json:"priority_fees,omitempty"`
+	Transactions                uint16           `json:"transactions,omitempty"`
+	TransactionsInternal        uint16           `json:"transcations_internal,omitempty"`
+	TransactionsBlobs           uint32           `json:"transcations_blob,omitempty"`
+	BlockRoot                   Hash             `json:"block_root,omitempty"`
+	ParentRoot                  Hash             `json:"parent_root,omitempty"`
+
+	ExecutionPayload ExecutionPayloadInfo `json:"execution_payload,omitempty"`
+	ConsensusLayer   ConsensusLayerInfo   `json:"consensus_layer,omitempty"`
+	SyncCommittee    SyncCommitteeInfo    `json:"sync_committee,omitempty"`
+	Eth1Data         Eth1DataInfo         `json:"eth1_data,omitempty"`
+}
+
+type ExecutionPayloadInfo struct {
+	// Block
+	BlockHash     Hash             `json:"block_hash"`
+	ParentHash    Hash             `json:"parent_hash"`
+	PriorityFees  *decimal.Decimal `json:"priority_fees"`
+	Recipient     Address          `json:"priority_fees_recipient"`
+	RecipientName string           `json:"priority_fees_recipient_name,omitempty"`
+	GasUsed       uint64           `json:"gas_used"`
+	GasLimit      uint64           `json:"gas_limit"`
+	BaseFeePerGas *decimal.Decimal `json:"base_fee_per_gas"`
+	BaseFees      *decimal.Decimal `json:"base_fees"`
+	// Transactions
+	// Time
+	ExtraData string `json:"extra_data,omitempty"`
+	Graffiti  string `json:"graffiti"`
+}
+
+type ConsensusLayerInfo struct {
+	StateRoot         Hash   `json:"state_root"`
+	Signature         Hash   `json:"signature"`
+	RandaoReveal      Hash   `json:"randao_reveal"`
+	Attestations      uint64 `json:"attestations"`
+	Votes             uint64 `json:"votes"`
+	VotingValidators  uint64 `json:"voting_validators"`
+	VoluntaryExits    uint64 `json:"voluntary_exits"`
+	AttesterSlashings uint64 `json:"attester_slashings"`
+	ProposerSlashings uint64 `json:"proposer_slashings"`
+	Deposits          uint64 `json:"deposit"`
+}
+
+type SyncCommitteeInfo struct {
+	Participation float64  `json:"participation"`
+	Bits          []byte   `json:"bits"`
+	SyncCommittee []uint64 `json:"sync_committee"`
+	Signature     []byte   `json:"signature"`
+}
+
+type Eth1DataInfo struct {
+	BlockHash    string `json:"block_hash"`
+	DepositCount string `json:"deposit_count"`
+	DepositRoot  []byte `json:"deposit_root"`
 }
 
 type BlobInfo struct {
