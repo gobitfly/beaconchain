bd7325b336c34c29c72dd6bee8b383c32e20f742
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2024-01-30 14:23:37 +0100

    Dashboard slot viz (#8)
    * validator dashboard slot viz

---
+++ v2/frontend/components/bc/BcTooltip.vue
@@ -0,0 +1,197 @@
+<script setup lang="ts">
+import { useTooltipStore } from '~/stores/useTooltipStore'
+
+interface Props {
+  text?: string,
+  layout?: 'dark' | 'default'
+  position?: 'top' | 'left' | 'right' | 'bottom'
+}
+
+const props = defineProps<Props>()
+const bcTooltip = ref<HTMLElement | null>(null)
+const { doSelect } = useTooltipStore()
+const { selected } = storeToRefs(useTooltipStore())
+
+const hover = ref(false)
+const isSelected = computed(() => !!bcTooltip.value && selected.value === bcTooltip.value)
+const isOpen = computed(() => isSelected.value || hover.value)
+
+const pos = ref<{ top: string, left: string }>({ top: '0', left: '0' })
+
+const classList = computed(() => {
+  return [props.layout || 'default', props.position || 'bottom', isOpen.value ? 'open' : 'closed']
+})
+
+const setPosition = () => {
+  const rect = bcTooltip.value?.getBoundingClientRect()
+  if (!rect) {
+    return
+  }
+  const padding = 4
+  let top = rect.bottom + padding
+  let left = rect.left + rect.width / 2
+  switch (props.position) {
+    case 'left':
+      left = rect.left - padding
+      top = rect.top + rect.height / 2
+      break
+    case 'top':
+      top = rect.top - padding
+      break
+    case 'right':
+      left = rect.left + rect.width + padding
+      top = rect.top + rect.height / 2
+      break
+  }
+  pos.value = { top: `${top}px`, left: `${left}px` }
+}
+
+const handleClick = () => {
+  if (isSelected.value) {
+    doSelect(null)
+  } else {
+    doSelect(bcTooltip.value)
+    setPosition()
+  }
+}
+
+const onHover = (enter:boolean) => {
+  if (!enter) {
+    hover.value = false
+  } else if (!selected.value) {
+    hover.value = true
+    setPosition()
+  }
+}
+
+const hide = (event: MouseEvent) => {
+  if (event.target === bcTooltip.value || isParent(bcTooltip.value, event.target as HTMLElement)) {
+    return
+  }
+  if (isSelected.value) {
+    doSelect(null)
+  }
+  hover.value = false
+  if (!isOpen.value) {
+    bcTooltip.value?.blur()
+  }
+}
+
+onMounted(() => {
+  document.addEventListener('click', hide)
+})
+
+onUnmounted(() => {
+  document.removeEventListener('click', hide)
+  if (isSelected.value) {
+    doSelect(null)
+  }
+})
+
+</script>
+<template>
+  <div ref="bcTooltip" @mouseover="onHover(true)" @mouseleave="hover = false" @click="handleClick()" @blur="onHover(false)">
+    <slot />
+    <Teleport v-if="isOpen" to="body">
+      <div class="bc-tooltip-wrapper" :style="pos">
+        <div class="bc-tooltip" :class="classList" @click="$event.stopImmediatePropagation()">
+          <slot name="tooltip">
+            {{ text }}
+          </slot>
+        </div>
+      </div>
+    </Teleport>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.bc-tooltip-wrapper {
+  position: fixed;
+  width: 1px;
+  height: 1px;
+  overflow: visible;
+  z-index: 99999;
+
+}
+
+.bc-tooltip {
+
+  --tt-bg-color: var(--light-grey);
+  --tt-color: var(--light-black);
+
+  position: relative;
+  display: inline-flex;
+  opacity: 0;
+  transition: opacity 1s;
+  text-align: center;
+  padding: 9px 12px;
+  min-width: 120px;
+  border-radius: var(--border-radius);
+  color: var(--tt-color);
+  background: var(--tt-bg-color);
+  font-family: var(--roboto-family);
+  font-size: 10px;
+  pointer-events: none;
+  transform: translate(-50%, 0);
+
+  &.dark {
+    --tt-bg-color: var(--light-black);
+    --tt-color: var(--light-grey);
+    border: solid 1px var(--container-border-color);
+  }
+
+  &::after {
+    position: relative;
+    transition: opacity 1s;
+    opacity: 0;
+    content: "";
+    border-width: 5px;
+    border-style: solid;
+    position: absolute;
+    z-index: 1;
+    pointer-events: none;
+
+    top: -10px;
+    left: 50%;
+    border-color: transparent transparent var(--tt-bg-color) transparent;
+  }
+
+  &.hover,
+  &.open {
+    opacity: 1;
+    pointer-events: unset;
+
+    &:not(.dark)::after {
+      opacity: 1;
+    }
+  }
+
+  &.top {
+    transform: translate(-50%, -100%);
+    &::after {
+      top: 100%;
+      left: 50%;
+      border-color: var(--tt-bg-color) transparent transparent transparent;
+    }
+
+  }
+
+  &.right {
+    transform: translate(0, -50%);
+    &::after {
+      top: calc(50% - 5px);
+      left: -10px;
+      border-color: transparent var(--tt-bg-color) transparent transparent;
+    }
+  }
+
+  &.left {
+    transform: translate(-100%, -50%);
+    &::after {
+      top: calc(50% - 5px);
+      left: 100%;
+      border-color: transparent transparent transparent var(--tt-bg-color);
+    }
+  }
+}
+</style>
---
+++ v2/frontend/components/dashboard/ValidatorSlotViz.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import { useValidatorSlotVizStore } from '~/stores/dashboard/useValidatorSlotVizStore'
+
+// TODO: implement dashboard switching
+const { getSlotViz } = useValidatorSlotVizStore()
+await useAsyncData('validator_dashboard_slot_viz', () => getSlotViz())
+
+const { slotViz } = storeToRefs(useValidatorSlotVizStore())
+
+</script>
+<template>
+  <SlotVizViewer v-if="slotViz" :data="slotViz" />
+</template>
+
+<style lang="scss" scoped>
+</style>
