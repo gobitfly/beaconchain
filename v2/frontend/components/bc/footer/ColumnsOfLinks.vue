0de3e8287fe5d64110ba73893f1f5c50de721938
thib-wien <thibaut.barthelemy@bitfly.at>
2024-01-31 13:04:21 +0100

    (BIDS-2962) icons and links are now set

--- v2/frontend/components/bc/footer/ColumnsOfLinks.vue
+++ v2/frontend/components/bc/footer/ColumnsOfLinks.vue
@@ -4,28 +4,28 @@ const columns = [
   {
     title: $t('footer.legal_notices'),
     links: [
-      [$t('footer.imprint'), ''],
-      [$t('footer.terms'), ''],
-      [$t('footer.privacy'), '']
+      [$t('footer.imprint'), '/imprint', 'fas fa-building'],
+      [$t('footer.terms'), 'https://storage.googleapis.com/legal.beaconcha.in/tos.pdf', 'fas fa-file-contract'],
+      [$t('footer.privacy'), 'https://storage.googleapis.com/legal.beaconcha.in/privacy.pdf', 'fas fa-user-secret']
     ]
   },
   {
     title: $t('footer.resources'),
     links: [
-      [$t('footer.api_docs'), ''],
-      [$t('footer.pricing'), ''],
-      [$t('footer.advertise'), ''],
-      ['Swag Shop', '']
+      [$t('footer.api_docs'), '/api/v2/docs/index.html', 'fas fa-laptop-code'],
+      [$t('footer.pricing'), '/premium', 'fas fa-file-invoice-dollar'],
+      [$t('footer.advertise'), '/advertisewithus', 'fas fa-ad'],
+      ['Swag Shop', 'https://shop.beaconcha.in', 'fas fa-shopping-cart']
     ]
   },
   {
     title: $t('footer.links'),
     links: [
-      ['Discord', ''],
-      ['Twitter', ''],
-      ['Github', ''],
-      ['Github Mobile App', ''],
-      [$t('footer.press_kit'), '']
+      ['Discord', 'https://dsc.gg/beaconchain', 'fab fa-discord'],
+      ['Twitter', 'https://twitter.com/beaconcha_in', 'fab fa-twitter'],
+      ['Github', 'https://github.com/gobitfly', 'fab fa-github'],
+      ['Github Mobile App', 'https://github.com/gobitfly/eth2-beaconchain-explorer-app', 'fab fa-github'],
+      [$t('footer.press_kit'), '/presskit', 'fas fa-newspaper']
     ]
   }
 ]
@@ -36,8 +36,11 @@ const columns = [
     <div class="title">
       {{ column.title }}
     </div>
-    <div v-for="link in column.links" :key="link[0]" class="link-line">
-      <a :href="link[1]" class="link">{{ link[0] }}</a>
+    <div v-for="line in column.links" :key="line[0]" class="link-line">
+      <a :href="line[1]" class="link">
+        <span class="icon"><span :class="line[2]" /></span>
+        {{ line[0] }}
+      </a>
     </div>
   </div>
 </template>
@@ -55,7 +58,14 @@ const columns = [
     margin-top: 10px;
   }
 }
+
 .link-line {
   line-height: 27px;
+}
+
+.icon{
+  display: inline-block;
+  width: 20px;
+  text-align: center;
 }
 </style>
