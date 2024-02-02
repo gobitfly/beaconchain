002d64f123cbde8f66a112d2f913bb4b6e936521
thib-wien <thibaut.barthelemy@bitfly.at>
2024-02-02 12:25:58 +0100

    (BIDS-2962) last improvements

---
+++ frontend/types/searchtypes.ts
@@ -0,0 +1,17 @@
+export enum SearchTypes {
+    slots = 'slots',
+    blocks = 'blocks',
+    graffiti = 'graffiti',
+    transactions = 'transactions',
+    epochs = 'epochs',
+    validators= 'validators',
+    addresses= 'addresses',
+    indexed_validators= 'indexed_validators',
+    validators_by_pubkey= 'validators_by_pubkey',
+    count_indexed_validators_by_deposit_address= 'count_indexed_validators_by_deposit_address',
+    count_indexed_validators_by_withdrawal_credential= 'count_indexed_validators_by_withdrawal_credential',
+    indexed_validators_by_graffiti= 'indexed_validators_by_graffiti',
+    indexed_validators_by_name= 'indexed_validators_by_name',
+    ens = 'ens',
+    network = 'network'
+  }
