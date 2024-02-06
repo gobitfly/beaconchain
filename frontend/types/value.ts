bfabbdf88f52ce33ddcbcb3b006fe4dbd44ecff5
thib-wien <thibaut.barthelemy@bitfly.at>
2024-02-06 17:15:40 +0100

    (NOBIDS) makes Arbitrum testnet future-proof and add functions isMainNet() and isL1()

--- frontend/types/value.ts
+++ frontend/types/value.ts
@@ -3,7 +3,7 @@ import type { CryptoCurrency, CryptoUnits, Currency } from '~/types/currencies'
 export type ValueConvertOptions = {
   sourceCurrency?: CryptoCurrency // source crypto currency - default: ETH
   sourceUnit?: CryptoUnits // source unit - default main unit (like eth)
-  targetCurrency?: Currency // terget currency - overrides the selected currency
+  targetCurrency?: Currency // target currency - overrides the selected currency
   fixedDecimalCount?: number // can override the usual settings, but can't go over 2 for fiat
   minUnit?: CryptoUnits // if output should only be in higher units (for example GWEI -> then it will never go down to WEI)
   fixedUnit?: CryptoUnits // fixed output unit - overrides min unit
