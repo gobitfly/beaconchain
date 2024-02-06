afbf6e9aa8aa6abdccce82e356f2fb00e18607d9
thib-wien <thibaut.barthelemy@bitfly.at>
2024-02-06 14:34:59 +0100

    (NOBIDS) new naming convention + new L1 field

--- frontend/types/networks.ts
+++ frontend/types/networks.ts
@@ -15,7 +15,7 @@
    * ChainInfo[myNetwork].path  is the beginning of the path used to address this network in API endpoints.
    * ChainInfo[myNetwork].mainNet  is equal to the chain ID of the mainnet of Gnosis (100)
      /!\ not the ID of the testnet! if you want the testnet ID, it is simply myNetwork.
-     So, to test whether your network is a testnet, you can do  myNetwork != ChainInfo[myNetwork].mainNet
+     So, to check whether your network is a testnet, you can do  myNetwork != ChainInfo[myNetwork].mainNet
    * ChainInfo[myNetwork].elCurrency is equal to 'xDAI' whereas ChainInfo[myNetwork].clCurrency is 'GNO'
 */
 
@@ -27,42 +27,136 @@ export const enum ChainIDs {
   Sepolia = 11155111,
   Goerli = 5,
 
-  ArbitrumOne = 42161,
+  ArbitrumOneEthereum = 42161,
+  ArbitrumNovaEthereum= 42170,
   ArbitrumSepolia = 421614,
 
-  Optimism = 10,
+  OptimismEthereum = 10,
   OptimismGoerli = 420,
 
-  Base = 8453,
+  BaseEthereum = 8453,
   BaseSepolia = 84532,
 
   Gnosis = 100,
-  GnosisChiado = 10200
+  Chiado = 10200
 }
 
 interface ChainInfoFields {
   name: string,
-  mainNet: ChainIDs,
+  mainNet: ChainIDs, // if the network is a testnet, this field points to the non-test network
+  L1: ChainIDs, // if the network is a L2, this field points to the L1
   clCurrency: CryptoCurrency,
   elCurrency: CryptoCurrency,
   path: string
 }
 
 export const ChainInfo: Record<ChainIDs, ChainInfoFields> = {
-  [ChainIDs.Ethereum]: { name: 'Ethereum Mainnet', mainNet: ChainIDs.Ethereum, clCurrency: 'ETH', elCurrency: 'ETH', path: '/ethereum' },
-  [ChainIDs.Holesky]: { name: 'Holesky Testnet', mainNet: ChainIDs.Ethereum, clCurrency: 'ETH', elCurrency: 'ETH', path: '/holesky' },
-  [ChainIDs.Sepolia]: { name: 'Sepolia Testnet', mainNet: ChainIDs.Ethereum, clCurrency: 'ETH', elCurrency: 'ETH', path: '/sepolia' },
-  [ChainIDs.Goerli]: { name: 'Görli Testnet', mainNet: ChainIDs.Ethereum, clCurrency: 'ETH', elCurrency: 'ETH', path: '/goerli' },
+  [ChainIDs.Ethereum]: {
+    name: 'Ethereum Mainnet',
+    mainNet: ChainIDs.Ethereum,
+    L1: ChainIDs.Ethereum,
+    clCurrency: 'ETH',
+    elCurrency: 'ETH',
+    path: '/ethereum'
+  },
+  [ChainIDs.Holesky]: {
+    name: 'Holesky Testnet',
+    mainNet: ChainIDs.Ethereum,
+    L1: ChainIDs.Holesky,
+    clCurrency: 'ETH',
+    elCurrency: 'ETH',
+    path: '/holesky'
+  },
+  [ChainIDs.Sepolia]: {
+    name: 'Sepolia Testnet',
+    mainNet: ChainIDs.Ethereum,
+    L1: ChainIDs.Sepolia,
+    clCurrency: 'ETH',
+    elCurrency: 'ETH',
+    path: '/sepolia'
+  },
+  [ChainIDs.Goerli]: {
+    name: 'Görli Testnet',
+    mainNet: ChainIDs.Ethereum,
+    L1: ChainIDs.Goerli,
+    clCurrency: 'ETH',
+    elCurrency: 'ETH',
+    path: '/goerli'
+  },
 
-  [ChainIDs.ArbitrumOne]: { name: 'Arbitrum One L2', mainNet: ChainIDs.ArbitrumOne, clCurrency: 'ETH', elCurrency: 'ETH', path: '/arbitrum-one' },
-  [ChainIDs.ArbitrumSepolia]: { name: 'Arbitrum Sepolia Testnet', mainNet: ChainIDs.ArbitrumOne, clCurrency: 'ETH', elCurrency: 'ETH', path: '/arbitrum-sepolia' },
+  [ChainIDs.ArbitrumOneEthereum]: {
+    name: 'Arbitrum One L2',
+    mainNet: ChainIDs.ArbitrumOneEthereum,
+    L1: ChainIDs.Ethereum,
+    clCurrency: 'ETH',
+    elCurrency: 'ETH',
+    path: '/arbitrum-one-ethereum'
+  },
+  [ChainIDs.ArbitrumNovaEthereum]: {
+    name: 'Arbitrum Nova L2',
+    mainNet: ChainIDs.ArbitrumNovaEthereum,
+    L1: ChainIDs.Ethereum,
+    clCurrency: 'ETH',
+    elCurrency: 'ETH',
+    path: '/arbitrum-nova-ethereum'
+  },
+  [ChainIDs.ArbitrumSepolia]: {
+    name: 'Arbitrum Sepolia Testnet',
+    mainNet: ChainIDs.ArbitrumOneEthereum,
+    L1: ChainIDs.Sepolia,
+    clCurrency: 'ETH',
+    elCurrency: 'ETH',
+    path: '/arbitrum-sepolia'
+  },
 
-  [ChainIDs.Optimism]: { name: 'Optimism L2', mainNet: ChainIDs.Optimism, clCurrency: 'ETH', elCurrency: 'ETH', path: '/optimism' },
-  [ChainIDs.OptimismGoerli]: { name: 'Optimism Görli Testnet', mainNet: ChainIDs.Optimism, clCurrency: 'ETH', elCurrency: 'ETH', path: '/optimism-goerli' },
+  [ChainIDs.OptimismEthereum]: {
+    name: 'Optimism L2',
+    mainNet: ChainIDs.OptimismEthereum,
+    L1: ChainIDs.Ethereum,
+    clCurrency: 'ETH',
+    elCurrency: 'ETH',
+    path: '/optimism-ethereum'
+  },
+  [ChainIDs.OptimismGoerli]: {
+    name: 'Optimism Görli Testnet',
+    mainNet: ChainIDs.OptimismEthereum,
+    L1: ChainIDs.Goerli,
+    clCurrency: 'ETH',
+    elCurrency: 'ETH',
+    path: '/optimism-goerli'
+  },
 
-  [ChainIDs.Base]: { name: 'Base L2', mainNet: ChainIDs.Base, clCurrency: 'ETH', elCurrency: 'ETH', path: '/base' },
-  [ChainIDs.BaseSepolia]: { name: 'Base Sepolia Testnet', mainNet: ChainIDs.Base, clCurrency: 'ETH', elCurrency: 'ETH', path: '/base-sepolia' },
+  [ChainIDs.BaseEthereum]: {
+    name: 'Base L2',
+    mainNet: ChainIDs.BaseEthereum,
+    L1: ChainIDs.Ethereum,
+    clCurrency: 'ETH',
+    elCurrency: 'ETH',
+    path: '/base-ethereum'
+  },
+  [ChainIDs.BaseSepolia]: {
+    name: 'Base Sepolia Testnet',
+    mainNet: ChainIDs.BaseEthereum,
+    L1: ChainIDs.Sepolia,
+    clCurrency: 'ETH',
+    elCurrency: 'ETH',
+    path: '/base-sepolia'
+  },
 
-  [ChainIDs.Gnosis]: { name: 'Gnosis', mainNet: ChainIDs.Gnosis, clCurrency: 'GNO', elCurrency: 'xDAI', path: '/gnosis' },
-  [ChainIDs.GnosisChiado]: { name: 'Gnosis Chiado Testnet', mainNet: ChainIDs.Gnosis, clCurrency: 'GNO', elCurrency: 'xDAI', path: '/gnosis-chiado' }
+  [ChainIDs.Gnosis]: {
+    name: 'Gnosis',
+    mainNet: ChainIDs.Gnosis,
+    L1: ChainIDs.Gnosis,
+    clCurrency: 'GNO',
+    elCurrency: 'xDAI',
+    path: '/gnosis'
+  },
+  [ChainIDs.Chiado]: {
+    name: 'Gnosis Chiado Testnet',
+    mainNet: ChainIDs.Gnosis,
+    L1: ChainIDs.Chiado,
+    clCurrency: 'GNO',
+    elCurrency: 'xDAI',
+    path: '/chiado'
+  }
 }
