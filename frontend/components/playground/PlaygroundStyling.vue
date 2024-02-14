4d907ffbd065ae46d42487964f53c4da3313c336
D13ce <stefan.benker@bitfly.at>
2024-02-14 15:40:57 +0100

    Add styling for Button

--- frontend/components/playground/PlaygroundStyling.vue
+++ frontend/components/playground/PlaygroundStyling.vue
@@ -1,13 +1,31 @@
 <template>
   <TabView>
     <TabPanel header="Buttons">
-      <Button>
-        Simple Button
-      </Button>
+      <div class="button_container">
+        <Button>
+          Simple Text Button
+        </Button>
+        <Button label="Labeled" />
+        <NuxtLink to="/dashboard">
+          <Button label="Dashboard Link" />
+        </NuxtLink>
+        <Button :disabled="true">
+          Disabled
+        </Button>
+        <Button class="p-button-icon-only">
+          <IconPlus alt="Plus icon" />
+        </Button>
+      </div>
     </TabPanel>
     <TabPanel :disabled="true" header="Disabled" />
   </TabView>
 </template>
 
 <style lang="scss" scoped>
+.button_container {
+  margin: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
 </style>
