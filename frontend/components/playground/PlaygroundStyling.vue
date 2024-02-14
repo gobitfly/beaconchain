c83d0714ec5fa488cd967d5002220aef8473a97b
D13ce <stefan.benker@bitfly.at>
2024-02-14 13:59:26 +0100

    Prepare playground for styling tests

---
+++ frontend/components/playground/PlaygroundStyling.vue
@@ -0,0 +1,13 @@
+<template>
+  <TabView>
+    <TabPanel header="Buttons">
+      <Button>
+        Simple Button
+      </Button>
+    </TabPanel>
+    <TabPanel :disabled="true" header="Disabled" />
+  </TabView>
+</template>
+
+<style lang="scss" scoped>
+</style>
