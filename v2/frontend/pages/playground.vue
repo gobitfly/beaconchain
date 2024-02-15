95d0241d03c241fb80740e313981698bd8ed3849
D13ce <stefan.benker@bitfly.at>
2024-02-15 09:38:02 +0100

    Move ads on playground into component and tab

--- v2/frontend/pages/playground.vue
+++ v2/frontend/pages/playground.vue
@@ -23,7 +23,6 @@ onMounted(async () => {
 
 <template>
   <div class="content">
-    <BcAdControl />
     <h1>Playground for testing UI components</h1>
     <NuxtLink to="/" class="row">
       <Button class="row">
@@ -41,41 +40,11 @@ onMounted(async () => {
       <TabPanel header="Conversion">
         <PlaygroundConversion />
       </TabPanel>
+      <TabPanel header="Ads">
+        <PlaygroundAds />
+      </TabPanel>
       <TabPanel header="Slot Viz">
         <SlotVizViewer v-if="slotVizData" :data="slotVizData" />
-      </TabPanel>
-      <TabPanel header="Ads">
-        <div class="ad_test_container">
-          No blue box should be left here
-          <div id="replace_me" class="ad_test">
-            Ok come on and replace me
-          </div>
-        </div>
-        <div class="ad_test_container">
-          Ad should be within the box
-          <div id="inside_me" class="ad_test">
-            Should be iniside
-          </div>
-        </div>
-        <div class="ad_test_container">
-          Ad should be after this text, but before the box
-          <div id="before_me" class="ad_test">
-            Should come before me
-          </div>
-        </div>
-        <div class="ad_test_container">
-          <div id="after_me" class="ad_test">
-            Should come after me
-          </div>
-          Ad should be before this text, but after the box
-        </div>
-        <div class="ad_test_container">
-          Ad should be after this text, but before the box
-          <div id="around_me" class="ad_test">
-            Should come around me
-          </div>
-          Ad should be before this text, but after the box
-        </div>
       </TabPanel>
       <TabPanel :disabled="true" header="Disabled" />
     </TabView>
@@ -85,26 +54,6 @@ onMounted(async () => {
 </template>
 
 <style lang="scss" scoped>
-.icon_holder {
-  margin: 10px;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
-}
-
-.ad_test_container{
-  color: red;
-  border: 1px solid red;
-  background-color: aqua;
-  padding: 10px;
-}
-.ad_test{
-  color: pink;
-  border: 1px solid pink;
-  background-color: darkblue;
-  padding: 10px;
-}
-
 .content {
   padding: var(--padding-large);
 }
