e1ccad88c836c4cbedb647d9ff9f7a7cca5930f8
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2024-02-15 08:28:07 +0100

    (BIDS-2986) ad control (#12)
    * ad control

--- v2/frontend/pages/playground.vue
+++ v2/frontend/pages/playground.vue
@@ -22,6 +22,7 @@ onMounted(async () => {
 
 <template>
   <div class="content">
+    <BcAdControl />
     <h1>Playground for testing UI components</h1>
     <div class="row">
       latest epoch: {{ latest?.currentEpoch }}
@@ -70,11 +71,55 @@ onMounted(async () => {
     <div class="icon_holder">
       <SlotVizViewer v-if="slotVizData" :data="slotVizData" />
     </div>
+    <div class="ad_test_container">
+      No blue box should be left here
+      <div id="replace_me" class="ad_test">
+        Ok come on and replace me
+      </div>
+    </div>
+    <div class="ad_test_container">
+      Ad should be within the box
+      <div id="inside_me" class="ad_test">
+        Should be iniside
+      </div>
+    </div>
+    <div class="ad_test_container">
+      Ad should be after this text, but before the box
+      <div id="before_me" class="ad_test">
+        Should come before me
+      </div>
+    </div>
+    <div class="ad_test_container">
+      <div id="after_me" class="ad_test">
+        Should come after me
+      </div>
+      Ad should be before this text, but after the box
+    </div>
+    <div class="ad_test_container">
+      Ad should be after this text, but before the box
+      <div id="around_me" class="ad_test">
+        Should come around me
+      </div>
+      Ad should be before this text, but after the box
+    </div>
     <BcFooterMainFooter />
   </div>
 </template>
 
 <style lang="scss" scoped>
+
+.ad_test_container{
+  color: red;
+  border: 1px solid red;
+  background-color: aqua;
+  padding: 10px;
+}
+.ad_test{
+  color: pink;
+  border: 1px solid pink;
+  background-color: darkblue;
+  padding: 10px;
+}
 .icon_holder {
   margin: 10px;
   display: flex;
