b27c5845326ec3871f8fd5dd9adc2a97be17b774
D13ce <stefan.benker@bitfly.at>
2024-02-16 11:10:21 +0100

    Wrap Dialog with BcDialog for consistent behavior

---
+++ frontend/components/bc/BcDialog.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+interface Props {
+  header?: string,
+}
+const props = defineProps<Props>()
+
+const visible = defineModel<boolean>()
+</script>
+
+<template>
+  <Dialog
+    v-model:visible="visible"
+    modal
+    :header="props.header"
+    :dismissable-mask="true"
+    :closable="false"
+    :draggable="false"
+  >
+    <slot />
+  </Dialog>
+</template>
