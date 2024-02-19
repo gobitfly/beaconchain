95dca73cc8a8bbbac7835ad2f275ee8ee04a9e5a
D13ce <stefan.benker@bitfly.at>
2024-02-19 10:27:57 +0100

    Implement modal draft

---
+++ v2/frontend/components/dashboard/ValidatorSubsetModal.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+interface Props {
+  caption?: string,
+}
+const props = defineProps<Props>()
+
+const header = ref('Validator Subset Modal')
+const visible = defineModel<boolean>()
+</script>
+
+<template>
+  <BcDialog v-model="visible" :header="header">
+    <div>
+      {{ props.caption }} Search Element
+    </div>
+    <div>
+      Validator List and copy button
+    </div>
+  </BcDialog>
+</template>
