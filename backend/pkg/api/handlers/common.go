9ffa29b97a8439691d02902f76e61b21c5218dde
LUCCA DUKIC <109136188+LuccaBitfly@users.noreply.github.com>
2024-02-20 12:24:13 +0100

    init data access service

--- backend/pkg/api/handlers/common.go
+++ backend/pkg/api/handlers/common.go
@@ -2,9 +2,19 @@ package apihandlers
 
 import (
 	"net/http"
+
+	dataaccess "github.com/gobitfly/beaconchain/pkg/api/data_access"
 )
 
 // All changes to common functions MUST NOT break any public handler behavior
+
+type HandlerService struct {
+	dai dataaccess.DataAccessInterface
+}
+
+func NewHandlerService(das dataaccess.DataAccessInterface) HandlerService {
+	return HandlerService{dai: das}
+}
 
 func ReturnOk(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
