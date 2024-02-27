24118555fe89ba64f976cd6d72fe072f4be6883e
RamiRond <ramirond@beaconcha.in>
2024-02-27 16:34:54 +0100

    applied feedback

--- v2/backend/pkg/api/types/data_access.go
+++ v2/backend/pkg/api/types/data_access.go
@@ -1,6 +1,6 @@
 package types
 
-type Sort[T ~int] struct {
+type Sort[T ColEnum] struct {
 	Column T
 	Desc   bool
 }
@@ -8,10 +8,19 @@ type Sort[T ~int] struct {
 // ----------------
 // Table Column Enums
 // shouldn't be converted to typescript, so they are defined here
+type ColEnum interface {
+	GetColNames() []string
+}
+
+// Define a generic type constraint that extends ColEnum and includes a method to instantiate itself.
+type ColEnumFactory[T ColEnum] interface {
+    ColEnum
+    NewFromIndex(i int) T
+}
+
 
 type VDBSummaryTableColumn int
 // prob rather use maps
-var VDBSummaryTableColumnSortNames = []string{"group_id", "efficiency_day", "efficiency_week", "efficiency_month", "efficiency_total", "validators"}
 const (
 	VDBSummaryGroup VDBSummaryTableColumn = iota
 	VDBSummaryEfficiencyDay
@@ -20,23 +29,39 @@ const (
 	VDBSummaryEfficiencyTotal
 	VDBSummaryValidators // Sort by count, not by index
 )
+func (VDBSummaryTableColumn) GetColNames() []string {
+	return []string{"group_id", "efficiency_day", "efficiency_week", "efficiency_month", "efficiency_total", "validators"}
+}
+func (VDBSummaryTableColumn) NewFromIndex(i int) VDBSummaryTableColumn {
+    return VDBSummaryTableColumn(i)
+}
 
 type VDBRewardsTableColumn int
-var VDBRewardsTableColumnSortNames = []string{"epoch", "duty"}
 const (
 	VDBRewardEpoch VDBRewardsTableColumn = iota
 	VDBRewardDuty                        // Sort by sum of percentages
 )
+func (c VDBRewardsTableColumn) GetColNames() []string {
+	return []string{"epoch", "duty"}
+}
+
+func (c VDBRewardsTableColumn) NewFromIndex(i int) VDBRewardsTableColumn {
+    return VDBRewardsTableColumn(i)
+}
 
 type VDBDutiesTableColumn int
-var VDBDutiesTableColumnSortNames = []string{"validator", "reward"}
 const (
 	VDBDutyValidator VDBDutiesTableColumn = iota
 	VDBDutyReward                        // Sort by sum of percentages
 )
+func (VDBDutiesTableColumn) GetColNames() []string {
+	return []string{"validator", "reward"}
+}
+func (VDBDutiesTableColumn) NewFromIndex(i int) VDBDutiesTableColumn {
+    return VDBDutiesTableColumn(i)
+}
 
 type VDBBlocksTableColumn int
-var VDBBlocksTableColumnSortNames = []string{"proposer", "group_id", "epoch", "slot", "block", "age", "status", "reward"}
 const (
 	VDBBlockProposer VDBBlocksTableColumn = iota
 	VDBBlockGroup
@@ -47,9 +72,14 @@ const (
 	VDBBlockStatus
 	VDBBlockProposerReward
 )
+func (VDBBlocksTableColumn) GetColNames() []string {
+	return []string{"proposer", "group_id", "epoch", "slot", "block", "age", "status", "reward"}
+}
+func (VDBBlocksTableColumn) NewFromIndex(i int) VDBBlocksTableColumn {
+    return VDBBlocksTableColumn(i)
+}
 
 type VDBWithdrawalsTableColumn int
-var VDBWithdrawalsTableColumnSortNames = []string{"epoch", "age", "index", "group_id", "recipient", "amount"}
 const (
 	VDBWithdrawalEpoch VDBWithdrawalsTableColumn = iota
 	VDBWithdrawalAge
@@ -58,7 +88,20 @@ const (
 	VDBWithdrawalRecipient
 	VDBWithdrawalAmount
 )
+func (c VDBWithdrawalsTableColumn) GetColNames() []string {
+	return []string{"epoch", "age", "index", "group_id", "recipient", "amount"}
+}
+func (VDBWithdrawalsTableColumn) NewFromIndex(i int) VDBWithdrawalsTableColumn {
+    return VDBWithdrawalsTableColumn(i)
+}
 
 
 // TODO
 type VDBValidatorsColumn int
+var VDBValidatorsColumnSortNames = []string{}
+func (c VDBValidatorsColumn) GetColNames() []string {
+	return VDBValidatorsColumnSortNames
+}
+func (VDBValidatorsColumn) NewFromIndex(i int) VDBValidatorsColumn {
+    return VDBValidatorsColumn(i)
+}
