a2a4b8ab5609d4d4ad2a8fd02dfd61755c6b9920
D13ce <stefan.benker@bitfly.at>
2024-02-27 13:30:26 +0100

    Add translations and improve styling

--- v2/frontend/components/playground/PlaygroundCharts.vue
+++ v2/frontend/components/playground/PlaygroundCharts.vue
@@ -12,7 +12,6 @@ import {
 } from 'echarts/components'
 import VChart from 'vue-echarts'
 
-// TODO: Remove unneeded components
 use([
   CanvasRenderer,
   LineChart,
@@ -23,6 +22,8 @@ use([
   GridComponent
 ])
 
+const { t: $t } = useI18n()
+
 // TODO: Extend data to way more data points
 const dataSetAll = [85, 85, 90, 67.5, 80, 45, 95]
 const dataSetHetzner = [80, 75, 85, 85, 80, 0, 90]
@@ -32,6 +33,8 @@ const dataSetOVH = [90, 95, 95, 50, 80, 90, 100]
 const labels = ['22.Aug\nEpoch 141393', '22.Aug\nEpoch 141393', '22.Aug\nEpoch 141393', '22.Aug\nEpoch 141393', '22.Aug\nEpoch 141393', '22.Aug\nEpoch 141393', '22.Aug\nEpoch 141393']
 
 const option = ref({
+  height: 400,
+
   textStyle: {
     fontFamily: 'Roboto',
     fontSize: 14,
@@ -42,7 +45,7 @@ const option = ref({
   color: ['#f0f0f0', '#e6194b', '#46f0f0', '#bcf60c', '#4363d8', '#ffe119', '#f032e6', '#3cb44b', '#911eb4', '#f58231', '#87ceeb', '#e6beff', '#40e0d0', '#fabebe', '#aaffc3', '#ffd8b1', '#fffac8', '#daa520', '#dda0dd', '#fa8072', '#d2b48c', '#6b8e23', '#a0522d', '#008080', '#9a6324', '#800000', '#808000', '#000075', '#808080', '#708090', '#ffdb58'],
 
   title: {
-    text: 'Efficiency', // TODO: Translation
+    text: $t('dashboard.validator.summary.chart.title'),
     left: 'center',
     textAlign: 'center',
     textStyle: {
@@ -55,9 +58,9 @@ const option = ref({
   xAxis: {
     type: 'category',
     data: labels,
+    boundaryGap: false,
 
     axisLabel: {
-      formatter: '{value} %',
       fontSize: 14, // TODO: Why is this needed? It should use the global textStyle
       lineHeight: 20
     },
@@ -70,7 +73,7 @@ const option = ref({
   },
 
   yAxis: {
-    name: 'Efficiency', // TODO: Translation
+    name: $t('dashboard.validator.summary.chart.yAxis'),
     nameLocation: 'center',
     nameTextStyle: { // TODO: retrieve from css?
       padding: [0, 0, 35, 0]
@@ -86,10 +89,15 @@ const option = ref({
     }
   },
 
-  // TODO: Styling and position
+  // TODO: Styling
   legend: {
     orient: 'horizontal',
-    bottom: 20
+    bottom: 50,
+    textStyle: {
+      color: '#f0f0f0',
+      fontSize: 14,
+      fontWeight: 'bold'
+    }
   },
 
   // TODO: Styling
@@ -98,7 +106,7 @@ const option = ref({
     trigger: 'axis'
   },
 
-  // TODO: Styling and position
+  // TODO: Styling
   dataZoom: [
     {
       type: 'inside',
@@ -133,6 +141,7 @@ const option = ref({
 
 <template>
   <div class="chart-container">
+    <!-- TODO: Somehow this is not reactive yet -->
     <VChart class="chart" :option="option" />
   </div>
 </template>
@@ -141,6 +150,6 @@ const option = ref({
 .chart-container {
   padding-top: var(--padding-large);
   width: 100%;
-  height: 840px; // TODO: Should be 640px
+  height: 625px;
 }
 </style>
