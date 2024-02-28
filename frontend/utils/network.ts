822a8978b7e3022ccf2463bdcfeb56f42f9a23ef
D13ce <stefan.benker@bitfly.at>
2024-02-28 15:09:56 +0100

    Improve axis labels

---
+++ frontend/utils/network.ts
@@ -0,0 +1,13 @@
+// TODO: Replace hardcoded Ethereum Mainnet values with real network information once network endpoint is available
+// This should als most likely become a composable later
+const tsForSlot0 = 1606820423
+const secondsPerSlot = 12
+const slotsPerEpoch = 32
+
+export function epochToTs (epoch: number): number | undefined {
+  if (epoch < 0) {
+    return undefined
+  }
+
+  return tsForSlot0 + ((epoch * slotsPerEpoch) * secondsPerSlot)
+}
