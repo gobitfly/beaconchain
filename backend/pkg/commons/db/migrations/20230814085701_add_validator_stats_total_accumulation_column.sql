a2a70c8d1b7ea4bdb76ee8765b9fe3c00391229d
stefan.fuhrmann <stefan.fuhrmann@bitfly.at>
2024-02-28 14:31:11 +0100

    (NOBIDS) removed all trailing ws

--- backend/pkg/commons/db/migrations/20230814085701_add_validator_stats_total_accumulation_column.sql
+++ backend/pkg/commons/db/migrations/20230814085701_add_validator_stats_total_accumulation_column.sql
@@ -3,9 +3,9 @@
 SELECT 'up SQL query with new column total_accumulation_exported for validator_stats_status';
 ALTER TABLE validator_stats_status ADD COLUMN IF NOT EXISTS total_accumulation_exported BOOLEAN NOT NULL DEFAULT FALSE;
 
-SELECT 'setting new validator_stats_status columns to true for already exported days'; 
+SELECT 'setting new validator_stats_status columns to true for already exported days';
 UPDATE validator_stats_status
-SET 
+SET
     total_accumulation_exported = true
 WHERE total_performance_exported = true;
 
