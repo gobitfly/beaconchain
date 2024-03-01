49db6d897ab79dd3d48af97073657f3601f6b6bc
D13ce <stefan.benker@bitfly.at>
2024-03-01 10:37:07 +0100

    Add creation flow to playground

---
+++ frontend/components/playground/PlaygroundDashboardCreation.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+
+import { type DisplayType } from '~/types/dashboard/creation'
+
+const displayType = ref<DisplayType>('modal')
+
+const changeDisplayType = (newDisplayType: DisplayType) => {
+  displayType.value = newDisplayType
+}
+</script>
+
+<template>
+  <div class="settings_container">
+    <h1>
+      Display Type Settings
+    </h1>
+    Current Display Type: {{ displayType }}
+    <div class="button_container">
+      <Button @click="changeDisplayType('modal')">
+        Display Type: Modal
+      </Button>
+      <Button @click="changeDisplayType('panel')">
+        Display Type: Panel
+      </Button>
+    </div>
+  </div>
+  <DashboardCreationController :display-type="displayType" />
+</template>
+
+<style lang="scss">
+  .settings_container {
+    padding: 10px;
+    border-bottom: 2px solid var(--primary-color);
+
+    .button_container {
+      display: flex;
+      padding: 10px;
+      gap: 10px;
+    }
+  }
+
+</style>
