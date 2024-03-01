f070e208e085e6eca8a0834d7eaf4cbd2afd4197
D13ce <stefan.benker@bitfly.at>
2024-03-01 11:04:12 +0100

    Roughly style creation mask

--- v2/frontend/components/dashboard/creation/DashboardCreationNetworkMask.vue
+++ v2/frontend/components/dashboard/creation/DashboardCreationNetworkMask.vue
@@ -3,11 +3,44 @@
 </script>
 
 <template>
-  <div>
-    Dashboard Creation Network Mask
+  <div class="mask_container">
+    <div class="element_container">
+      <div class="big_text">
+        Add a new dashboard
+      </div>
+      <div class="subtitle_text">
+        What network are your validators on?
+      </div>
+      <div class="network_button_container">
+        <Button>Ethereum</Button>
+        <Button>Gnosis</Button>
+      </div>
+      <div class="continue_button_container">
+        <Button>Continue</Button>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+  .mask_container{
+    width: 460px;
+    height: 248px;
 
+    .element_container{
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+
+      .network_button_container{
+        display: flex;
+        gap: 10px;
+      }
+
+      .continue_button_container{
+        display: flex;
+        justify-content: flex-end;
+      }
+    }
+  }
 </style>
--- v2/frontend/components/dashboard/creation/DashboardCreationTypeMask.vue
+++ v2/frontend/components/dashboard/creation/DashboardCreationTypeMask.vue
@@ -3,11 +3,40 @@
 </script>
 
 <template>
-  <div>
-    Dashboard Creation Type Mask
+  <div class="mask_container">
+    <div class="element_container">
+      <div class="big_text">
+        Add a new dashboard
+      </div>
+      <div class="subtitle_text">
+        What would you like to monitor?
+      </div>
+      <div class="row_container">
+        <Button>Accounts</Button>
+        <Button>Validators</Button>
+      </div>
+      <div class="row_container">
+        <InputText />
+        <Button>Continue</Button>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+  .mask_container{
+    width: 460px;
+    height: 248px;
 
+    .element_container{
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+
+      .row_container{
+        display: flex;
+        gap: 10px;
+      }
+    }
+  }
 </style>
