d69e9c03fce637c7135190a949dbff6ff8b8d363
Lucca <109136188+LuccaBitfly@users.noreply.github.com>
2024-03-05 12:23:09 +0100

    struct update (#66)

--- v2/backend/pkg/api/types/validator_dashboard.go
+++ v2/backend/pkg/api/types/validator_dashboard.go
@@ -140,13 +140,15 @@ type InternalGetValidatorDashboardDutiesResponse ApiPagingResponse[VDBEpochDutie
 // ------------------------------------------------------------
 // Blocks Tab
 type VDBBlocksTableRow struct {
-	Proposer uint64                     `json:"proposer"`
-	GroupId  uint64                     `json:"group_id"`
-	Epoch    uint64                     `json:"epoch"`
-	Slot     uint64                     `json:"slot"`
-	Block    uint64                     `json:"block"`
-	Status   string                     `json:"status" tstype:"'success' | 'missed' | 'orphaned' | 'scheduled'"`
-	Reward   ClElValue[decimal.Decimal] `json:"reward"`
+	Proposer        uint64                     `json:"proposer"`
+	GroupId         uint64                     `json:"group_id"`
+	Epoch           uint64                     `json:"epoch"`
+	Slot            uint64                     `json:"slot"`
+	Block           uint64                     `json:"block"`
+	Status          string                     `json:"status" tstype:"'success' | 'missed' | 'orphaned' | 'scheduled'"`
+	RewardRecipient Address                    `json:"reward_recipient"`
+	Reward          ClElValue[decimal.Decimal] `json:"reward"`
+	Graffiti        string                     `json:"graffiti"`
 }
 type InternalGetValidatorDashboardBlocksResponse ApiPagingResponse[VDBBlocksTableRow]
 
