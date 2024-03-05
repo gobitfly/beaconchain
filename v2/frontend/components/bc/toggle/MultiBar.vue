b9bfcf6fb402f7859d614a7db8d21f4ace75b8b3
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2024-03-05 10:54:20 +0100

    Nobids/toggle bar (#60)
    * multi toggle bar

---
+++ v2/frontend/components/bc/toggle/MultiBar.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import type { IconDefinition } from '@fortawesome/fontawesome-svg-core'
+import type { Component } from 'vue'
+
+interface Props {
+  icons: {
+    icon?: IconDefinition
+    component?: Component,
+    value: string
+  }[]
+}
+
+const props = defineProps<Props>()
+
+const selected = defineModel<string[]>({ required: true })
+
+const inital: Record<string, boolean> = {}
+const modelValues = ref<Record<string, boolean>>(props.icons.reduce((map, { value }) => {
+  map[value] = selected.value.includes(value)
+  return map
+}, inital))
+
+watch(modelValues, () => {
+  const list: string[] = []
+  Object.entries(modelValues.value).forEach(([key, value]) => {
+    if (value) {
+      list.push(key)
+    }
+  })
+  selected.value = list
+}, { deep: true })
+
+</script>
+
+<template>
+  <div class="bc-togglebar">
+    <BcToggleMultibarButton v-for="icon in props.icons" :key="icon.value" v-model="modelValues[icon.value]" :icon="icon.icon">
+      <template #icon>
+        <slot :name="icon.value">
+          <component :is="icon.component" />
+        </slot>
+      </template>
+    </BcToggleMultibarButton>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.bc-togglebar {
+  display: inline-flex;
+  gap: var(--padding);
+  padding: 7px 10px;
+
+  background-color: var(--container-background);
+  border: solid 1px var(--container-border-color);
+  border-radius: var(--border-radius);
+}
+</style>
