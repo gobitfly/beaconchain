69239cd981c75714c40044ff57e7e3dd9eaef38a
Stefan Pletka <stefan.pletka@bitfly.at>
2024-03-05 14:28:52 +0100

    Adjust slotViz method to new structure

--- backend/pkg/api/types/slot_viz.go
+++ backend/pkg/api/types/slot_viz.go
@@ -8,7 +8,11 @@ type VDBSlotVizDuty struct {
 }
 
 type VDBSlotVizTuple struct {
-	Validator  uint64 `json:"validator"`
+	Validator uint64 `json:"validator"`
+	// If the duty is a proposal & it's successful, the duty_object is the proposed block
+	// If the duty is a proposal & it failed/scheduled, the duty_object is the slot
+	// If the duty is a slashing & it's successful, the duty_object is the validator you slashed
+	// If the duty is a slashing & it failed, the duty_object is your validator that was slashed
 	DutyObject uint64 `json:"duty_object"`
 }
 
@@ -28,8 +32,8 @@ type VDBSlotVizSlot struct {
 	Status       string                                `json:"status" tstype:"'proposed' | 'missed' | 'scheduled' | 'orphaned'" faker:"oneof: proposed, missed, scheduled, orphaned"`
 	Proposal     *VDBSlotVizTuple                      `json:"proposal,omitempty"`
 	Attestations *VDBSlotVizStatus[VDBSlotVizDuty]     `json:"attestations,omitempty"`
-	Sync         *VDBSlotVizStatus[VDBSlotVizDuty]     `json:"sync,omitempty"`
-	Slashing     *VDBSlotVizStatus[VDBSlotVizSlashing] `json:"slashing,omitempty"`
+	Syncs        *VDBSlotVizStatus[VDBSlotVizDuty]     `json:"sync,omitempty"`
+	Slashings    *VDBSlotVizStatus[VDBSlotVizSlashing] `json:"slashing,omitempty"`
 }
 type SlotVizEpoch struct {
 	Epoch    uint64           `json:"epoch"`
