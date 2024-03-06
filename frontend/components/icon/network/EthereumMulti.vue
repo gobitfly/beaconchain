ca1ced6a0ae77d007792c2d8edda203c60601b28
D13ce <stefan.benker@bitfly.at>
2024-03-06 09:49:12 +0100

    Add mono- and multi colored network items

---
+++ frontend/components/icon/network/EthereumMulti.vue
@@ -0,0 +1,10 @@
+<template>
+  <svg width="33" height="55" viewBox="0 0 33 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path id="Vector" d="M16.0356 0L15.6849 1.2533V37.6203L16.0356 37.9885L32.0713 28.01L16.0356 0Z" fill="#343434" />
+    <path id="Vector_2" d="M16.0366 0L0 28.01L16.0366 37.9885V20.3369V0Z" fill="#8C8C8C" />
+    <path id="Vector_3" d="M16.038 41.1835L15.8401 41.4371V54.3917L16.038 54.9988L32.0833 31.2105L16.038 41.1835Z" fill="#3C3C3B" />
+    <path id="Vector_4" d="M16.0366 54.9988V41.1835L0 31.2105L16.0366 54.9988Z" fill="#8C8C8C" />
+    <path id="Vector_5" d="M16.0373 37.9901L32.073 28.0116L16.0373 20.3385V37.9901Z" fill="#141414" />
+    <path id="Vector_6" d="M0 28.0116L16.0366 37.9901V20.3385L0 28.0116Z" fill="#393939" />
+  </svg>
+</template>
