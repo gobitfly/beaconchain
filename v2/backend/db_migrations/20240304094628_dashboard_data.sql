ef2739962f9a5d018ac5bd6b2f8f2f988b086580
Manuel <5877862+manuelsc@users.noreply.github.com>
2024-03-07 13:05:45 +0100

    table names

--- v2/backend/db_migrations/20240304094628_dashboard_data.sql
+++ v2/backend/db_migrations/20240304094628_dashboard_data.sql
@@ -1,10 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 
-CREATE TABLE IF NOT EXISTS dashboard_data_epoch (
-    validatorindex int,
-    epoch int,
-    attestations_source_reward BIGINT,
+
+CREATE TABLE IF NOT EXISTS validator_dashboard_data_epoch (
+    validatorindex int NOT NULL,
+    epoch int NOT NULL,
+    attestations_source_reward BIGINT ,
     attestations_target_reward BIGINT,
     attestations_head_reward BIGINT,
     attestations_inactivity_reward BIGINT,
@@ -34,8 +35,8 @@ CREATE TABLE IF NOT EXISTS dashboard_data_epoch (
 ) PARTITION BY range (epoch);
 
 
-CREATE TABLE IF NOT EXISTS dashboard_data_rolling_24h (
-    validatorindex int,
+CREATE TABLE IF NOT EXISTS validator_dashboard_data_rolling_daily (
+    validatorindex int NOT NULL,
     attestations_source_reward BIGINT,
     attestations_target_reward BIGINT,
     attestations_head_reward BIGINT,
@@ -64,6 +65,165 @@ CREATE TABLE IF NOT EXISTS dashboard_data_rolling_24h (
     withdrawals_amount BIGINT,
     primary key (validatorindex)
 );
+
+
+-- CREATE TABLE IF NOT EXISTS validator_dashboard_data_hourly (
+--     validatorindex int NOT NULL,
+--     ts TIMESTAMP(0) DAY TO HOUR NOT NULL,
+--     attestations_source_reward BIGINT,
+--     attestations_target_reward BIGINT,
+--     attestations_head_reward BIGINT,
+--     attestations_inactivity_reward BIGINT,
+--     attestations_inclusion_reward BIGINT,
+--     attestations_reward BIGINT,
+--     attestations_ideal_source_reward BIGINT,
+--     attestations_ideal_target_reward BIGINT,
+--     attestations_ideal_head_reward BIGINT,
+--     attestations_ideal_inactivity_reward BIGINT,
+--     attestations_ideal_inclusion_reward BIGINT,
+--     attestations_ideal_reward BIGINT,
+--     blocks_scheduled int,
+--     blocks_proposed int,
+--     blocks_cl_reward BIGINT, -- gwei
+--     blocks_el_reward NUMERIC, -- wei
+--     sync_scheduled int,
+--     sync_executed int,
+--     sync_rewards BIGINT,
+--     slashed BOOLEAN,
+--     balance_start BIGINT,
+--     balance_end BIGINT,
+--     deposits_count int,
+--     deposits_amount BIGINT,
+--     withdrawals_count int,
+--     withdrawals_amount BIGINT,
+--     primary key (ts, validatorindex)
+-- );
+
+CREATE TABLE IF NOT EXISTS validator_dashboard_data_daily (
+    validatorindex int NOT NULL,
+    day date NOT NULL,
+    attestations_source_reward BIGINT,
+    attestations_target_reward BIGINT,
+    attestations_head_reward BIGINT,
+    attestations_inactivity_reward BIGINT,
+    attestations_inclusion_reward BIGINT,
+    attestations_reward BIGINT,
+    attestations_ideal_source_reward BIGINT,
+    attestations_ideal_target_reward BIGINT,
+    attestations_ideal_head_reward BIGINT,
+    attestations_ideal_inactivity_reward BIGINT,
+    attestations_ideal_inclusion_reward BIGINT,
+    attestations_ideal_reward BIGINT,
+    blocks_scheduled int,
+    blocks_proposed int,
+    blocks_cl_reward BIGINT, -- gwei
+    blocks_el_reward NUMERIC, -- wei
+    sync_scheduled int,
+    sync_executed int,
+    sync_rewards BIGINT,
+    slashed BOOLEAN,
+    balance_start BIGINT,
+    balance_end BIGINT,
+    deposits_count int,
+    deposits_amount BIGINT,
+    withdrawals_count int,
+    withdrawals_amount BIGINT,
+    primary key (day, validatorindex)
+);
+
+CREATE TABLE IF NOT EXISTS validator_dashboard_data_rolling_weekly (
+    validatorindex int NOT NULL,
+    attestations_source_reward BIGINT,
+    attestations_target_reward BIGINT,
+    attestations_head_reward BIGINT,
+    attestations_inactivity_reward BIGINT,
+    attestations_inclusion_reward BIGINT,
+    attestations_reward BIGINT,
+    attestations_ideal_source_reward BIGINT,
+    attestations_ideal_target_reward BIGINT,
+    attestations_ideal_head_reward BIGINT,
+    attestations_ideal_inactivity_reward BIGINT,
+    attestations_ideal_inclusion_reward BIGINT,
+    attestations_ideal_reward BIGINT,
+    blocks_scheduled int,
+    blocks_proposed int,
+    blocks_cl_reward BIGINT, -- gwei
+    blocks_el_reward NUMERIC, -- wei
+    sync_scheduled int,
+    sync_executed int,
+    sync_rewards BIGINT,
+    slashed BOOLEAN,
+    balance_start BIGINT,
+    balance_end BIGINT,
+    deposits_count int,
+    deposits_amount BIGINT,
+    withdrawals_count int,
+    withdrawals_amount BIGINT,
+    primary key (validatorindex)
+);
+
+CREATE TABLE IF NOT EXISTS validator_dashboard_data_rolling_monthly (
+    validatorindex int NOT NULL,
+    attestations_source_reward BIGINT,
+    attestations_target_reward BIGINT,
+    attestations_head_reward BIGINT,
+    attestations_inactivity_reward BIGINT,
+    attestations_inclusion_reward BIGINT,
+    attestations_reward BIGINT,
+    attestations_ideal_source_reward BIGINT,
+    attestations_ideal_target_reward BIGINT,
+    attestations_ideal_head_reward BIGINT,
+    attestations_ideal_inactivity_reward BIGINT,
+    attestations_ideal_inclusion_reward BIGINT,
+    attestations_ideal_reward BIGINT,
+    blocks_scheduled int,
+    blocks_proposed int,
+    blocks_cl_reward BIGINT, -- gwei
+    blocks_el_reward NUMERIC, -- wei
+    sync_scheduled int,
+    sync_executed int,
+    sync_rewards BIGINT,
+    slashed BOOLEAN,
+    balance_start BIGINT,
+    balance_end BIGINT,
+    deposits_count int,
+    deposits_amount BIGINT,
+    withdrawals_count int,
+    withdrawals_amount BIGINT,
+    primary key (validatorindex)
+);
+
+CREATE TABLE IF NOT EXISTS validator_dashboard_data_rolling_total (
+    validatorindex int NOT NULL,
+    attestations_source_reward BIGINT,
+    attestations_target_reward BIGINT,
+    attestations_head_reward BIGINT,
+    attestations_inactivity_reward BIGINT,
+    attestations_inclusion_reward BIGINT,
+    attestations_reward BIGINT,
+    attestations_ideal_source_reward BIGINT,
+    attestations_ideal_target_reward BIGINT,
+    attestations_ideal_head_reward BIGINT,
+    attestations_ideal_inactivity_reward BIGINT,
+    attestations_ideal_inclusion_reward BIGINT,
+    attestations_ideal_reward BIGINT,
+    blocks_scheduled int,
+    blocks_proposed int,
+    blocks_cl_reward BIGINT, -- gwei
+    blocks_el_reward NUMERIC, -- wei
+    sync_scheduled int,
+    sync_executed int,
+    sync_rewards BIGINT,
+    slashed BOOLEAN,
+    balance_start BIGINT,
+    balance_end BIGINT,
+    deposits_count int,
+    deposits_amount BIGINT,
+    withdrawals_count int,
+    withdrawals_amount BIGINT,
+    primary key (validatorindex)
+);
+
 
 
 -- +goose StatementEnd
