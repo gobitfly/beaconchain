fdb0294cadc23e54d13888f8350616299daef80d
Manuel <5877862+manuelsc@users.noreply.github.com>
2024-03-07 10:33:04 +0100

    add aggregate

--- backend/db_migrations/20240304094628_dashboard_data.sql
+++ backend/db_migrations/20240304094628_dashboard_data.sql
@@ -29,10 +29,42 @@ CREATE TABLE IF NOT EXISTS dashboard_data_epoch (
     deposits_count smallint,
     deposits_amount BIGINT,
     withdrawals_count smallint,
-    withdrawals_amount BIGINT
+    withdrawals_amount BIGINT,
+    primary key (validatorindex, epoch)
 ) PARTITION BY range (epoch);
 
-create index on dashboard_data_epoch (validatorindex, epoch);
+
+CREATE TABLE IF NOT EXISTS dashboard_data_rolling_24h (
+    validatorindex int,
+    attestations_source_reward BIGINT,
+    attestations_target_reward BIGINT,
+    attestations_head_reward BIGINT,
+    attestations_inactivity_reward BIGINT,
+    attestations_inclusion_reward BIGINT,
+    attestations_reward BIGINT,
+    attestations_ideal_source_reward BIGINT,
+    attestations_ideal_target_reward BIGINT,
+    attestations_ideal_head_reward BIGINT,
+    attestations_ideal_inactivity_reward BIGINT,
+    attestations_ideal_inclusion_reward BIGINT,
+    attestations_ideal_reward BIGINT,
+    blocks_scheduled int,
+    blocks_proposed int,
+    blocks_cl_reward BIGINT, -- gwei
+    blocks_el_reward NUMERIC, -- wei
+    sync_scheduled int,
+    sync_executed int,
+    sync_rewards BIGINT,
+    slashed BOOLEAN,
+    balance_start BIGINT,
+    balance_end BIGINT,
+    deposits_count int,
+    deposits_amount BIGINT,
+    withdrawals_count int,
+    withdrawals_amount BIGINT,
+    primary key (validatorindex)
+);
+
 
 -- +goose StatementEnd
 
