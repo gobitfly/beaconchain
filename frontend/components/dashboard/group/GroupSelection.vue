aa9436160182b0465b81ad3c40e563a9811eb7a6
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2024-03-07 13:02:30 +0100

    Bids 2821/dashboard validator management - group selection, dashboard loading (#78)
    * dropdown styles
    
    * group selection
    
    * load dashboard group
    
    ---------
    
    Co-authored-by: D13ce <stefan.benker@bitfly.at>

---
+++ frontend/components/dashboard/group/GroupSelection.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { DAHSHBOARDS_ALL_GROUPS_ID } from '~/types/dashboard'
+
+interface Props {
+  includeAll?: boolean,
+}
+const props = defineProps<Props>()
+
+const { overview } = storeToRefs(useValidatorDashboardOverviewStore())
+
+const list = computed(() => {
+  const groups = overview.value?.groups ?? []
+  if (props.includeAll) {
+    return [{ id: DAHSHBOARDS_ALL_GROUPS_ID, name: '' }].concat(groups)
+  }
+  return groups
+})
+
+const selected = defineModel<number>({ required: true })
+
+const selectedGroup = computed(() => {
+  return list.value.find(item => item.id === selected.value)
+})
+
+</script>
+
+<template>
+  <BcDropdown
+    v-model="selected"
+    :options="list"
+    option-value="id"
+    option-label="name"
+    :placeholder="$t('dashboard.group.selection.placeholder')"
+  >
+    <template v-if="selectedGroup" #value>
+      <span>{{ $t('dashboard.group.selection.group') }}:
+        <DashboardGroupLabel :group="selectedGroup" />
+      </span>
+    </template>
+
+    <template #option="slotProps">
+      <DashboardGroupLabel :group="slotProps" />
+    </template>
+  </BcDropdown>
+</template>
+
+<style lang="scss" scoped></style>
