5ac200385253ebf5892846e745479e1920053124
D13ce <stefan.benker@bitfly.at>
2024-03-14 08:17:52 +0100

    Mock all the data for now

--- v2/frontend/components/dashboard/DashboardHeader.vue
+++ v2/frontend/components/dashboard/DashboardHeader.vue
@@ -1,40 +1,42 @@
 <script lang="ts" setup>
 import { useUserDashboardStore } from '~/stores/dashboard/useUserDashboardStore'
+const { t: $t } = useI18n()
 const store = useUserDashboardStore()
 const { getDashboards } = store
 
-// const { dashboards } = storeToRefs(store)
+const { dashboards } = storeToRefs(store)
 await useAsyncData('validator_dashboards', () => getDashboards()) // TODO: This is called here and in DashboardValidatorManageValidators.vue. Should just be called once?
 
 const emit = defineEmits<{(e: 'showCreation'): void }>()
 
-const items = ref([
-  {
-    label: 'Selected Validators', // Caption of the button
-    items: [
-      {
-        label: 'Best Validators'
-      },
-      {
-        label: 'Worst Validators'
-      }
-    ]
-  },
-  {
-    label: 'Selected Accounts', // Caption of the button
-    items: [
-      {
-        label: 'Best Accounts'
-      },
-      {
-        label: 'Worst Accounts'
-      }
-    ]
-  },
-  {
-    label: 'Notifications'
+const items = computed(() => {
+  interface DashboardButton {
+    label: string;
+    items?: {
+        label: string;
+    }[];
   }
-])
+
+  const dashboardsButtons: DashboardButton[] = []
+
+  let items = dashboards.value?.validator_dashboards.map(({ name }) => ({ label: name }))
+  dashboardsButtons.push({
+    label: 'Validators',
+    items
+  })
+
+  items = dashboards.value?.account_dashboards.map(({ name }) => ({ label: name }))
+  dashboardsButtons.push({
+    label: 'Accounts',
+    items
+  })
+
+  dashboardsButtons.push({
+    label: $t('dashboard.notifications')
+  })
+
+  return dashboardsButtons
+})
 
 </script>
 
--- v2/frontend/composables/useCustomFetch.ts
+++ v2/frontend/composables/useCustomFetch.ts
@@ -46,7 +46,7 @@ const mapping: Record<string, MappingData> = {
   },
   [API_PATH.USER_DASHBOARDS]: {
     path: '/users/me/dashboards',
-    mock: false
+    mock: true
   },
   [API_PATH.DASHBOARD_CREATE_ACCOUNT]: {
     path: '/account-dashboards',
@@ -61,12 +61,12 @@ const mapping: Record<string, MappingData> = {
   [API_PATH.DASHBOARD_SUMMARY_DETAILS]: {
     path: '/validator-dashboards/{dashboardKey}/groups/{group_id}/summary',
     getPath: values => `/validator-dashboards/${values?.dashboardKey}/groups/${values?.groupId}/summary`,
-    mock: false
+    mock: true
   },
   [API_PATH.DASHBOARD_SUMMARY]: {
     path: '/validator-dashboards/{dashboardKey}/summary?',
     getPath: values => `/validator-dashboards/${values?.dashboardKey}/summary`,
-    mock: false
+    mock: true
   },
   [API_PATH.DASHBOARD_SUMMARY_CHART]: {
     path: '/validator-dashboards/{dashboardKey}/summary-chart?',
@@ -76,12 +76,12 @@ const mapping: Record<string, MappingData> = {
   [API_PATH.DASHBOARD_OVERVIEW]: {
     path: '/validator-dashboards/{dashboardKey}',
     getPath: values => `/validator-dashboards/${values?.dashboardKey}`,
-    mock: false
+    mock: true
   },
   [API_PATH.DASHBOARD_SLOTVIZ]: {
     path: '/validator-dashboards/{dashboardKey}/slot-viz',
     getPath: values => `/validator-dashboards/${values?.dashboardKey}/slot-viz`,
-    mock: false
+    mock: true
   },
   [API_PATH.LATEST_STATE]: {
     path: '/latestState',
