bf481804831c4b72f30a022689f20b2c369e0289
invis-bitfly <petty.palkovics.github@bitfly.at>
2024-03-18 13:56:18 +0100

    exporter, api: improve validator mapping performance, add pubkey=>index support

--- backend/pkg/api/services/service_validator_mapping.go
+++ backend/pkg/api/services/service_validator_mapping.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/gob"
+	"encoding/hex"
 	"fmt"
 	"sync"
 	"time"
@@ -15,7 +16,19 @@ import (
 	"github.com/pkg/errors"
 )
 
-var currentValidatorMapping *types.RedisCachedValidatorsMapping
+type ValidatorMapping struct {
+	ValidatorPubkeys  []string
+	ValidatorIndices  map[string]*uint64
+	ValidatorMetadata []*ValidatorMetadata
+}
+
+type ValidatorMetadata struct {
+	ActivationEpoch   types.Epoch
+	WithdrawableEpoch types.Epoch
+}
+
+var currentValidatorMapping *ValidatorMapping
+var lastValidatorIndex int
 
 var currentMappingMutex = &sync.RWMutex{}
 
@@ -31,6 +44,60 @@ func StartIndexMappingService() {
 	}
 }
 
+func initValidatorMapping(data *types.RedisCachedValidatorsMapping) error {
+	log.Infof("initializing validator mapping")
+	l := len(data.Mapping)
+
+	c := ValidatorMapping{}
+	c.ValidatorIndices = make(map[string]*uint64)
+	c.ValidatorPubkeys = make([]string, l)
+	c.ValidatorMetadata = make([]*ValidatorMetadata, l)
+
+	for i, v := range data.Mapping {
+		if i == l {
+			break
+		}
+
+		b := hex.EncodeToString(v.PublicKey)
+		j := uint64(i)
+
+		c.ValidatorPubkeys[i] = b
+		c.ValidatorIndices[b] = &j
+		c.ValidatorMetadata[i] = &ValidatorMetadata{
+			ActivationEpoch:   v.ActivationEpoch,
+			WithdrawableEpoch: v.WithdrawableEpoch,
+		}
+	}
+	currentValidatorMapping = &c
+	lastValidatorIndex = l - 1
+
+	return nil
+}
+
+func quickUpdateValidatorMapping(data *types.RedisCachedValidatorsMapping) error {
+	log.Infof("quick updating validator mapping")
+
+	for i, v := range data.Mapping {
+		if i > lastValidatorIndex {
+			b := hex.EncodeToString(v.PublicKey)
+			j := uint64(i)
+
+			currentValidatorMapping.ValidatorPubkeys = append(currentValidatorMapping.ValidatorPubkeys, b)
+			currentValidatorMapping.ValidatorIndices[b] = &j
+			currentValidatorMapping.ValidatorMetadata = append(currentValidatorMapping.ValidatorMetadata, &ValidatorMetadata{
+				ActivationEpoch:   v.ActivationEpoch,
+				WithdrawableEpoch: v.WithdrawableEpoch,
+			})
+
+			lastValidatorIndex = i
+			continue
+		}
+		currentValidatorMapping.ValidatorMetadata[i].ActivationEpoch = v.ActivationEpoch
+		currentValidatorMapping.ValidatorMetadata[i].WithdrawableEpoch = v.WithdrawableEpoch
+	}
+	return nil
+}
+
 func updateValidatorMapping() error {
 	var validatorMapping *types.RedisCachedValidatorsMapping
 
@@ -38,7 +105,7 @@ func updateValidatorMapping() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
 	defer cancel()
 	key := fmt.Sprintf("%d:%s", utils.Config.Chain.ClConfig.DepositChainID, "vm")
-	encoded, err := db.PersistentRedisDbClient.Get(ctx, key).Result()
+	encoded, err := db.PersistentRedisDbClient.Get(ctx, key).Bytes()
 	if err != nil {
 		return errors.Wrap(err, "failed to get compressed validator mapping from db")
 	}
@@ -46,9 +113,8 @@ func updateValidatorMapping() error {
 
 	// ungob
 	start = time.Now()
-	var buf bytes.Buffer
-	buf.Write([]byte(encoded))
-	dec := gob.NewDecoder(&buf)
+	buf := bytes.NewBuffer(encoded)
+	dec := gob.NewDecoder(buf)
 	err = dec.Decode(&validatorMapping)
 	if err != nil {
 		return errors.Wrap(err, "error decoding assignments data")
@@ -56,8 +122,59 @@ func updateValidatorMapping() error {
 	log.Debugf("decoding validator mapping from gob took %s", time.Since(start))
 
 	currentMappingMutex.Lock()
-	currentValidatorMapping = validatorMapping
+	start = time.Now()
+	if currentValidatorMapping == nil {
+		initValidatorMapping(validatorMapping)
+	} else {
+		quickUpdateValidatorMapping(validatorMapping)
+	}
+	log.Debugf("updated Validator Mapping, took %s", time.Since(start))
 	currentMappingMutex.Unlock()
 
 	return nil
+}
+
+// GetCurrentValidatorMapping returns the current validator mapping and a function to release the lock
+// Call release lock after you are done with accessing the data, otherwise it will block the validator mapping service from updating
+func GetCurrentValidatorMapping() (*ValidatorMapping, func(), error) {
+	currentMappingMutex.RLock()
+
+	if currentValidatorMapping == nil {
+		return nil, currentMappingMutex.RUnlock, errors.New("waiting for validator mapping to be initialized")
+	}
+
+	return currentValidatorMapping, currentMappingMutex.RUnlock, nil
+}
+
+func GetPubkeysOfValidatorIndexSlice(indices []uint64) ([]string, error) {
+	res := make([]string, len(indices))
+	mapping, releaseLock, err := GetCurrentValidatorMapping()
+	defer releaseLock()
+	if err != nil {
+		return nil, err
+	}
+	for i, index := range indices {
+		if index > uint64(lastValidatorIndex) {
+			return nil, fmt.Errorf("validator index outside of mapped range (%d is not within 0-%d)", index, lastValidatorIndex)
+		}
+		res[i] = mapping.ValidatorPubkeys[index]
+	}
+	return res, nil
+}
+
+func GetValidatorIndexOfPubkeySlice(pubkeys []string) ([]uint64, error) {
+	res := make([]uint64, len(pubkeys))
+	mapping, releaseLock, err := GetCurrentValidatorMapping()
+	defer releaseLock()
+	if err != nil {
+		return nil, err
+	}
+	for i, pubkey := range pubkeys {
+		p, ok := mapping.ValidatorIndices[pubkey]
+		if !ok {
+			return nil, fmt.Errorf("pubkey not found in validator mapping: %s", pubkey)
+		}
+		res[i] = *p
+	}
+	return res, nil
 }
