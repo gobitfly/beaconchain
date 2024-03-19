75f16e0e3419aaa809ae48918bb6a6cdc49fefae
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-03-19 13:39:30 +0100

    Toast webservice errors

--- frontend/composables/useCustomFetch.ts
+++ frontend/composables/useCustomFetch.ts
@@ -107,53 +107,63 @@ const mapping: Record<string, MappingData> = {
     mock: true
   }
 }
-
-export async function useCustomFetch<T> (pathName: PathName, options: NitroFetchOptions<string & {}> = { }, pathValues?: PathValues, query?: PathValues): Promise<T> {
-  // the access token stuff is only a blue-print and needs to be refined once we have api calls to test against
+export function useCustomFetch () {
   const refreshToken = useCookie('refreshToken')
   const accessToken = useCookie('accessToken')
+  const { showError } = useBcToast()
+  const { t: $t } = useI18n()
 
-  const map = mapping[pathName]
-  if (!map) {
-    throw new Error(`path ${pathName} not found`)
-  }
+  async function fetch<T> (pathName: PathName, options: NitroFetchOptions<string & {}> = { }, pathValues?: PathValues, query?: PathValues): Promise<T> {
+  // the access token stuff is only a blue-print and needs to be refined once we have api calls to test against
 
-  const url = useRequestURL()
-  const { public: { apiClient, legacyApiClient, xUserId, apiKey }, private: pConfig } = useRuntimeConfig()
-  const path = addQueryParams(map.mock ? `${pathName}.json` : map.getPath?.(pathValues) || map.path, query)
-  let baseURL = map.mock ? '../mock' : map.legacy ? legacyApiClient : apiClient
+    const map = mapping[pathName]
+    if (!map) {
+      throw new Error(`path ${pathName} not found`)
+    }
 
-  if (process.server) {
-    baseURL = map.mock ? `${url.protocol}${url.host}/mock` : map.legacy ? pConfig?.legacyApiServer : pConfig?.apiServer
-  }
+    const url = useRequestURL()
+    const { public: { apiClient, legacyApiClient, xUserId, apiKey }, private: pConfig } = useRuntimeConfig()
+    const path = addQueryParams(map.mock ? `${pathName}.json` : map.getPath?.(pathValues) || map.path, query)
+    let baseURL = map.mock ? '../mock' : map.legacy ? legacyApiClient : apiClient
 
-  const method = map.method || 'GET'
-  if (pathName === API_PATH.LOGIN) {
-    const res = await $fetch<LoginResponse>(path, { method, ...options, baseURL })
-    refreshToken.value = res.refresh_token
-    accessToken.value = res.access_token
-    return res as T
-  } else if (!map.noAuth) {
-    if (!accessToken.value && refreshToken.value) {
-      const res = await useCustomFetch<{ access_token: string }>(API_PATH.REFRESH_TOKEN, { body: { refresh_token: refreshToken.value } })
+    if (process.server) {
+      baseURL = map.mock ? `${url.protocol}${url.host}/mock` : map.legacy ? pConfig?.legacyApiServer : pConfig?.apiServer
+    }
+
+    const method = map.method || 'GET'
+    if (pathName === API_PATH.LOGIN) {
+      const res = await $fetch<LoginResponse>(path, { method, ...options, baseURL })
+      refreshToken.value = res.refresh_token
       accessToken.value = res.access_token
-    }
-
-    if (accessToken.value) {
-      options.headers = new Headers({})
-      options.headers.append('Authorization', `Bearer ${accessToken.value}`)
-    } else if (apiKey) {
-      options.headers = new Headers({})
-      options.headers.append('Authorization', `Bearer ${apiKey}`)
-    }
-
-    if (xUserId) {
-      if (!options.headers) {
-        options.headers = new Headers({ })
+      return res as T
+    } else if (!map.noAuth) {
+      if (!accessToken.value && refreshToken.value) {
+        const res = await fetch<{ access_token: string }>(API_PATH.REFRESH_TOKEN, { body: { refresh_token: refreshToken.value } })
+        accessToken.value = res.access_token
       }
-      (options.headers as Headers).append('X-User-Id', xUserId)
+
+      if (accessToken.value) {
+        options.headers = new Headers({})
+        options.headers.append('Authorization', `Bearer ${accessToken.value}`)
+      } else if (apiKey) {
+        options.headers = new Headers({})
+        options.headers.append('Authorization', `Bearer ${apiKey}`)
+      }
+
+      if (xUserId) {
+        if (!options.headers) {
+          options.headers = new Headers({ })
+        }
+        (options.headers as Headers).append('X-User-Id', xUserId)
+      }
+    }
+
+    try {
+      return await $fetch<T>(path, { method, ...options, baseURL })
+    } catch (e: any) {
+      showError({ group: e.statusCode, summary: $t('error.ws_error'), detail: `${baseURL}${path}` })
+      throw (e)
     }
   }
-
-  return await $fetch<T>(path, { method, ...options, baseURL })
+  return { fetch }
 }
--- frontend/stores/dashboard/useValidatorSlotVizStore.ts
+++ frontend/stores/dashboard/useValidatorSlotVizStore.ts
@@ -1,12 +1,12 @@
 import { defineStore } from 'pinia'
-import { useCustomFetch } from '~/composables/useCustomFetch'
 import type { SlotVizEpoch, InternalGetValidatorDashboardSlotVizResponse } from '~/types/api/slot_viz'
 import type { DashboardKey } from '~/types/dashboard'
 
 export const useValidatorSlotVizStore = defineStore('validator_slotViz', () => {
+  const { fetch } = useCustomFetch()
   const slotViz = ref<SlotVizEpoch[] | undefined | null>()
   async function getSlotViz (dashboardKey: DashboardKey | string) {
-    const res = await useCustomFetch<InternalGetValidatorDashboardSlotVizResponse>(API_PATH.DASHBOARD_SLOTVIZ, { headers: {} }, { dashboardKey })
+    const res = await fetch<InternalGetValidatorDashboardSlotVizResponse>(API_PATH.DASHBOARD_SLOTVIZ, { headers: {} }, { dashboardKey })
     slotViz.value = res.data
     return slotViz.value
   }
