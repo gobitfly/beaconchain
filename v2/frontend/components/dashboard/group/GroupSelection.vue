e4e788d6067264c3b74a633364017d09ff8f68dc
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2024-03-19 09:02:45 +0100

    Bids 2821/dashboard validator management modal (#109)
    * dashboard validator management modal
    
    ---------
    
    Co-authored-by: D13ce <stefan.benker@bitfly.at>

--- v2/frontend/components/dashboard/group/GroupSelection.vue
+++ v2/frontend/components/dashboard/group/GroupSelection.vue
@@ -6,6 +6,8 @@ interface Props {
 }
 const props = defineProps<Props>()
 
+const emit = defineEmits<{(e: 'setGroup', value: number): void}>()
+
 const { overview } = storeToRefs(useValidatorDashboardOverviewStore())
 
 const list = computed(() => {
@@ -31,6 +33,7 @@ const selectedGroup = computed(() => {
     option-value="id"
     option-label="name"
     :placeholder="$t('dashboard.group.selection.placeholder')"
+    @update:model-value="(value: number)=>emit('setGroup', value)"
   >
     <template v-if="selectedGroup" #value>
       <span>{{ $t('dashboard.group.selection.group') }}:
