6cf567212d702361b55c1795e5443a375d05720c
Lucca <109136188+LuccaBitfly@users.noreply.github.com>
2024-03-20 13:08:16 +0100

    use pointers as method receiver, not values (#129)

--- v2/backend/cmd/api/main.go
+++ v2/backend/cmd/api/main.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/gobitfly/beaconchain/pkg/api"
 	dataaccess "github.com/gobitfly/beaconchain/pkg/api/data_access"
-
 	"github.com/gobitfly/beaconchain/pkg/api/services"
+
 	"github.com/gobitfly/beaconchain/pkg/commons/db"
 	"github.com/gobitfly/beaconchain/pkg/commons/log"
 	"github.com/gobitfly/beaconchain/pkg/commons/types"
@@ -63,7 +63,7 @@ func main() {
 	log.Fatal(srv.ListenAndServe(), "Error while serving", 0)
 }
 
-func InitServices(das dataaccess.DataAccessService) dataaccess.DataAccessor {
+func InitServices(das *dataaccess.DataAccessService) dataaccess.DataAccessor {
 	db.ReaderDb = das.ReaderDb
 	db.WriterDb = das.WriterDb
 	db.PersistentRedisDbClient = das.PersistentRedisDbClient
