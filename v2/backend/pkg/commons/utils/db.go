e84a8cd062c0098459502c9ca9bc51290ba657e4
Manuel <5877862+manuelsc@users.noreply.github.com>
2024-03-20 09:09:51 +0100

    rework a bit and settling for a day partition width

--- v2/backend/pkg/commons/utils/db.go
+++ v2/backend/pkg/commons/utils/db.go
@@ -5,12 +5,19 @@ import (
 	"errors"
 
 	"github.com/gobitfly/beaconchain/pkg/commons/log"
+	"github.com/jackc/pgconn"
 	"github.com/jmoiron/sqlx"
 )
+
+var duplicateEntryError = &pgconn.PgError{Code: "23505"}
 
 func Rollback(tx *sqlx.Tx) {
 	err := tx.Rollback()
 	if err != nil && !errors.Is(err, sql.ErrTxDone) {
 		log.Error(err, "error rolling back transaction", 0)
 	}
+}
+
+func IsDuplicatedKeyError(err error) bool {
+	return errors.Is(err, duplicateEntryError)
 }
