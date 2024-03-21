bd8979409f523b28d34a692adea5708e77ba8405
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-03-21 16:16:46 +0100

    either there is a typo or there is not

--- frontend/components/bc/format/FormatNumber.vue
+++ frontend/components/bc/format/FormatNumber.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
 interface Props {
-  value?: number | string, // can ether be a number or a string representing a number
+  value?: number | string, // can either be a number or a string representing a number
   text?: string, // for already formatted numbers
   minDecimals?: number, // defaults to 0
   maxDecimals?: number, // defaults to 2
