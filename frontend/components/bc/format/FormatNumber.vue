f3c89729947d00eb8962f8425046994d1fb60bfa
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-03-21 16:13:49 +0100

    making a mini comment a little less mini

--- frontend/components/bc/format/FormatNumber.vue
+++ frontend/components/bc/format/FormatNumber.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
 interface Props {
-  value?: number | string, // has to be number
+  value?: number | string, // can ether be a number or a string representing a number
   text?: string, // for already formatted numbers
   minDecimals?: number, // defaults to 0
   maxDecimals?: number, // defaults to 2
