90d3b22aabbbc01a55ee111d15736b2cef438a53
D13ce <stefan.benker@bitfly.at>
2024-03-26 09:43:05 +0100

    Make store a data holder only and remove comments

--- v2/frontend/stores/dashboard/useValidatorDashboardOverviewStore.ts
+++ v2/frontend/stores/dashboard/useValidatorDashboardOverviewStore.ts
@@ -1,42 +1,36 @@
 import { defineStore } from 'pinia'
-import { warn } from 'vue'
 import type { VDBOverviewData, InternalGetValidatorDashboardResponse } from '~/types/api/validator_dashboard'
 import type { DashboardKey } from '~/types/dashboard'
 
-const validatorOverviewStore = defineStore('validator_overview', () => {
+// NOTE: The store itself is "as stupid as possible", it only holds data to share between server, client and all components
+const validatorOverviewStore = defineStore('validator_overview_store', () => {
   const data = ref<VDBOverviewData | undefined | null>()
-  warn('DIECE: validatorOverviewStore = defineStore')
-  const overview = readonly(data)
-
-  const setOverview = (d: VDBOverviewData | undefined | null) => {
-    warn('DIECE: setOverview() called')
-    data.value = d
-  }
-
-  return { overview, setOverview }
+  return { data }
 })
 
-// NOTE: this only works if called within a components script setup
-// but that's good enough since the data returned below is reactive
+// NOTE: this is a wrapper around the "stupid" store to
+// - make store data from the outside readonly
+// - provide a refresh function to update the data via an API call
+// calling useValidatorDashboardOverviewStore only works if called within a components script setup
+//  but that's good enough since the data returned below is reactive
 export function useValidatorDashboardOverviewStore () {
   const { fetch } = useCustomFetch()
-  const store = validatorOverviewStore()
-  const { setOverview } = store
-  const { overview } = storeToRefs(store) // NOTE: this is the REACTIVE (and read only) data
+  const { data } = storeToRefs(validatorOverviewStore()) // NOTE: this is the REACTIVE (and read only) data
+
+  const validatorDashboardOverview = readonly(data)
 
   // NOTE: function to UPDATE the data, use overview if you just want to access the data
   async function getOverview (key: DashboardKey) {
-    warn('DIECE: getOverview() called, key:', key)
-
     if (key === undefined) {
       return undefined
     }
 
     const res = await fetch<InternalGetValidatorDashboardResponse>(API_PATH.DASHBOARD_OVERVIEW, undefined, { dashboardKey: key })
-    setOverview(res.data)
-    return overview.value
+    data.value = res.data // NOTE: set data in store
+
+    return data.value
   }
 
   // TODO: maybe this should just be data and refresh and every component that uses that chooses a name to their liking?
-  return { validatorDashboardOverview: overview, refreshValidatorDashboardOverview: getOverview }
+  return { validatorDashboardOverview, refreshValidatorDashboardOverview: getOverview }
 }
