275258df2688c39481788eddf7a1cd0a9586539c
D13ce <stefan.benker@bitfly.at>
2024-03-27 14:10:31 +0100

    Remove notes

--- frontend/stores/dashboard/useValidatorDashboardOverviewStore.ts
+++ frontend/stores/dashboard/useValidatorDashboardOverviewStore.ts
@@ -2,27 +2,20 @@ import { defineStore } from 'pinia'
 import type { VDBOverviewData, InternalGetValidatorDashboardResponse } from '~/types/api/validator_dashboard'
 import type { DashboardKey } from '~/types/dashboard'
 
-// NOTE: The store itself is "as stupid as possible", it only holds data to share between server, client and all components
 const validatorOverviewStore = defineStore('validator_overview_store', () => {
   const data = ref<VDBOverviewData | undefined | null>()
   return { data }
 })
 
-// NOTE: this is a wrapper around the "stupid" store to
-// - make store data from the outside readonly
-// - provide a refresh function to update the data via an API call
-// calling useValidatorDashboardOverviewStore only works if called within a components script setup
-//  but that's good enough since the data returned below is reactive
 export function useValidatorDashboardOverviewStore () {
   const { fetch } = useCustomFetch()
-  const { data } = storeToRefs(validatorOverviewStore()) // NOTE: this is the REACTIVE (and read only) data
+  const { data } = storeToRefs(validatorOverviewStore())
 
-  const overview = computed(() => data.value) // NOTE: we use computed here to have a readonly access to the data only from the outside
+  const overview = computed(() => data.value)
 
-  // NOTE: function to UPDATE the data, use overview if you just want to access the data
   async function refreshOverview (key: DashboardKey) {
     const res = await fetch<InternalGetValidatorDashboardResponse>(API_PATH.DASHBOARD_OVERVIEW, undefined, { dashboardKey: key })
-    data.value = res.data // NOTE: set data in store
+    data.value = res.data
 
     return overview.value
   }
