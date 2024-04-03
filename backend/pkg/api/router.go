ac1b33e26c7fa47f3f0aad342dbe1316246e8d38
peter <1674920+peterbitfly@users.noreply.github.com>
2024-04-03 13:27:13 +0200

    fix update dashboard group name handler & add put to cors

--- backend/pkg/api/router.go
+++ backend/pkg/api/router.go
@@ -47,7 +47,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS, HEAD")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
