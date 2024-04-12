59eb99881ba6237edebc8aef88349fbab30df1db
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-04-12 09:33:04 +0200

    details details details

---
+++ frontend/components/block/table/BlockTableRewardItem.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { ClElValue } from '~/types/api/common'
+
+interface Props {
+  reward?: ClElValue<string>,
+  status?: 'success' | 'missed' | 'orphaned' | 'scheduled'
+}
+defineProps<Props>()
+
+</script>
+<template>
+  <BcTooltip v-if="status === 'success' && reward" class="combine-rewards">
+    <BcFormatValue :value="reward?.el" />
+    <BcFormatValue :value="reward?.cl" />
+    <template #tooltip>
+      <div>
+        <div class="tt-row">
+          <span>{{ $t('dashboard.validator.blocks.el_rewards') }}: </span>
+          <BcFormatValue :value="reward?.el" />
+        </div>
+        <div class="tt-row">
+          <span>{{ $t('dashboard.validator.blocks.cl_rewards') }}: </span>
+          <BcFormatValue :value="reward?.cl" />
+        </div>
+      </div>
+    </template>
+  </BcTooltip>
+  <span v-else>-</span>
+</template>
+<style lang="scss" scoped>
+
+.tt-row {
+  display: flex;
+  flex-wrap: nowrap;
+  white-space: nowrap;
+  gap: 3px;
+}
+
+.combine-rewards {
+  display: inline-flex;
+  flex-direction: column;
+
+  >div:last-child {
+    font-size: var(--small_text_font_size);
+  }
+}
+</style>
--- frontend/components/dashboard/table/DashboardTableBlocks.vue
+++ frontend/components/dashboard/table/DashboardTableBlocks.vue
@@ -24,10 +24,13 @@ const { overview } = useValidatorDashboardOverviewStore()
 const { width } = useWindowSize()
 const colsVisible = computed(() => {
   return {
-    duty: width.value > 1180,
-    rewards: width.value >= 860,
-    elRewards: width.value >= 740,
-    age: width.value >= 620
+    slot: width.value > 960,
+    age: width.value > 880,
+    recipient: width.value > 800,
+    status: width.value > 700,
+    mobileStatus: width.value < 1000,
+    rewards: width.value > 600,
+    groupSort: width.value > 400
   }
 })
 
@@ -128,6 +131,7 @@ const isRowExpandable = (row: VDBBlocksTableRow) => {
               field="group_id"
               body-class="group-id"
               header-class="group-id"
+              :sortable="colsVisible.groupSort"
               :header="$t('dashboard.validator.col.group')"
             >
               <template #body="slotProps">
@@ -136,7 +140,7 @@ const isRowExpandable = (row: VDBBlocksTableRow) => {
                 </span>
               </template>
             </Column>
-            <Column field="slot" :sortable="true" :header="$t('common.slot')">
+            <Column v-if="colsVisible.slot" field="slot" :sortable="true" :header="$t('common.slot')">
               <template #body="slotProps">
                 <NuxtLink :to="`/slot/${slotProps.data.slot}`" target="_blank" class="link" :no-prefetch="true">
                   <BcFormatNumber :value="slotProps.data.slot" />
@@ -158,48 +162,79 @@ const isRowExpandable = (row: VDBBlocksTableRow) => {
                 <BcFormatTimePassed class="time-passed" :value="slotProps.data.epoch" />
               </template>
             </Column>
-            <Column field="status" :header="$t('dashboard.validator.col.status')">
+            <Column
+              v-if="colsVisible.status"
+              field="status"
+              :sortable="!colsVisible.mobileStatus"
+              :header="$t('dashboard.validator.col.status')"
+              :body-class="{ 'status-mobile': colsVisible.mobileStatus }"
+            >
               <template #body="slotProps">
                 <!--TODO: use status render once merged-->
+                <span :class="{ 'status-mobile': colsVisible.mobileStatus }" />
                 {{ slotProps.data.status }}
               </template>
             </Column>
             <Column
               v-if="colsVisible.rewards"
               field="reward_cl"
+              :sortable="true"
               body-class="reward"
               header-class="reward"
               :header="$t('dashboard.validator.col.rewards')"
             >
               <template #body="slotProps">
-                <BcTooltip class="combine-rewards">
-                  <BcFormatValue :value="slotProps.data.reward?.el" :use-colors="true" :options="{ addPlus: true }" />
-                  <BcFormatValue :value="slotProps.data.reward?.cl" :use-colors="true" :options="{ addPlus: true }" />
-                  <template #tooltip>
-                    <div>
-                      <div class="tt-row">
-                        <span>{{ $t('dashboard.validator.blocks.el_rewards') }}: </span>
-                        <BcFormatValue
-                          :value="slotProps.data.reward?.el"
-                          :use-colors="true"
-                          :options="{ addPlus: true }"
-                        />
-                      </div>
-                      <div class="tt-row">
-                        <span>{{ $t('dashboard.validator.blocks.cl_rewards') }}: </span>
-                        <BcFormatValue
-                          :value="slotProps.data.reward?.cl"
-                          :use-colors="true"
-                          :options="{ addPlus: true }"
-                        />
-                      </div>
-                    </div>
-                  </template>
-                </BcTooltip>
+                <BlockTableRewardItem :reward="slotProps.data.reward" />
               </template>
             </Column>
             <template #expansion="slotProps">
-              todo {{ slotProps }}
+              <div class="expansion">
+                <div v-if="!colsVisible.slot" class="row">
+                  <div class="label">
+                    {{ $t('common.slot') }}:
+                  </div>
+                  <NuxtLink :to="`/slot/${slotProps.data.slot}`" target="_blank" class="link" :no-prefetch="true">
+                    <BcFormatNumber :value="slotProps.data.slot" />
+                  </NuxtLink>
+                </div>
+                <div class="row">
+                  <div class="label">
+                    {{ $t('common.epoch') }}:
+                  </div>
+                  <NuxtLink :to="`/epoch/${slotProps.data.block}`" target="_blank" class="link" :no-prefetch="true">
+                    <BcFormatNumber :value="slotProps.data.epoch" />
+                  </NuxtLink>
+                </div>
+                <div v-if="!colsVisible.age" class="row">
+                  <div class="label">
+                    <BcTableAgeHeader />
+                  </div>
+                  <BcFormatTimePassed class="time-passed" :value="slotProps.data.epoch" />
+                </div>
+                <div v-if="!colsVisible.status" class="row">
+                  <div class="label">
+                    {{ $t('dashboard.validator.col.status') }}:
+                  </div>
+                  <div class="value">
+                    <!--TODO: use status render once merged-->
+                    {{ slotProps.data.status }}
+                  </div>
+                </div>
+                <div v-if="!colsVisible.rewards" class="row">
+                  <div class="label">
+                    {{ $t('dashboard.validator.col.rewards') }}:
+                  </div>
+                  <BlockTableRewardItem :reward="slotProps.data.reward" />
+                </div>
+                <div class="row">
+                  <div class="label">
+                    {{ $t('block.col.graffiti') }}:
+                  </div>
+                  <div class="value">
+                    {{ slotProps.data.graffiti }}
+                  </div>
+                </div>
+              </div>
             </template>
           </BcTable>
         </ClientOnly>
@@ -217,34 +252,35 @@ const isRowExpandable = (row: VDBBlocksTableRow) => {
 @use "~/assets/css/utils.scss";
 
 :deep(.block-table) {
-  --col-width: 154px;
 
-  .epoch {
-    // TODO: use utils.set-all-width once merged
-    // @include utils.set-all-width(80px);
+  .group-id {
+    //TODO: @include utils.set-all-width(120px);
+    max-width: 120px;
+    width: 120px;
+    min-width: 120px;
+    @include utils.truncate-text;
   }
 
-  .group_id,
-  .reward {
-    // @include utils.set-all-width(120px);
+  @media (max-width: 399px) {
+    .group-id {
+      //TODO: @include utils.set-all-width(80px);
+      max-width: 80px;
+      width: 80px;
+      min-width: 80px;
+      @include utils.truncate-text;
+    }
+  }
+
+  .status-mobile {
+    //TODO: @include utils.set-all-width(40px);
+    max-width: 40px;
+    width: 40px;
+    min-width: 40px;
+    @include utils.truncate-text;
   }
 
   .time-passed {
     white-space: nowrap;
-  }
-
-  tr:not(.p-datatable-row-expansion) {
-    @media (max-width: 1300px) {
-      .duty {
-        // @include utils.set-all-width(300px);
-      }
-    }
-  }
-
-  tr:has(+.total-row) {
-    td {
-      border-bottom-color: var(--primary-color);
-    }
   }
 
   .future-row {
@@ -258,19 +294,30 @@ const isRowExpandable = (row: VDBBlocksTableRow) => {
   }
 }
 
-.tt-row {
+.expansion {
   display: flex;
-  flex-wrap: nowrap;
-  white-space: nowrap;
-  gap: 3px;
-}
-
-.combine-rewards {
-  display: inline-flex;
+  align-items: center;
   flex-direction: column;
+  gap: var(--padding-small);
+  padding: var(--padding) 41px;
+  background: var(--table-header-background);
+  font-size: var(--small_text_font_size);
 
-  >div:last-child {
-    font-size: var(--small_text_font_size);
+  .row {
+
+    display: flex;
+    align-items: center;
+
+    .label {
+      width: 90px;
+      font-weight: var(--small_text_bold_font_weight);
+      flex-shrink: 0;
+    }
+
+    .value {
+      text-wrap: wrap;
+      word-break: break-all;
+    }
   }
 }
 
