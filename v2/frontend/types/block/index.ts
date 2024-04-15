1adf035a450077a8e065e303305594d555b81d8e
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2024-04-15 12:15:39 +0200

    generic tag and block status tag (#197)
    * generic tag and block status tag

---
+++ v2/frontend/types/block/index.ts
@@ -0,0 +1,3 @@
+import type { VDBBlocksTableRow } from '../api/validator_dashboard'
+
+export type BlockStatus = VDBBlocksTableRow['status']
---
+++ v2/frontend/types/tag.ts
@@ -0,0 +1,4 @@
+export const TagColors = ['success', 'failed', 'orphaned', 'partial', 'light', 'dark'] as const
+export type TagColor = typeof TagColors[number]
+
+export type TagSize = 'default' | 'compact' | 'circle'
