a56793fdd38b40e1d55f0df628d3de7565f5ec4d
LUCCA DUKIC <109136188+LuccaBitfly@users.noreply.github.com>
2024-04-17 12:23:24 +0200

    configure cors for sessions

--- v2/backend/cmd/api/main.go
+++ v2/backend/cmd/api/main.go
@@ -51,7 +51,7 @@ func main() {
 	sessionManager := api.NewSessionManager(cfg.RedisCacheEndpoint, !cfg.Frontend.Debug)
 
 	router := api.NewApiRouter(dataAccessor, sessionManager)
-	router.Use(api.CorsMiddleware, api.GetAuthMiddleware(cfg.ApiKeySecret))
+	router.Use(api.GetCorsMiddleware(cfg.CorsAllowedHosts), api.GetAuthMiddleware(cfg.ApiKeySecret))
 
 	srv := &http.Server{
 		Handler:      router,
