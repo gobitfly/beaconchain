9e61c2afc27ac7825cb198222f465077064bec82
thib-wien <thibaut.barthelemy@bitfly.at>
2024-04-22 15:43:30 +0200

    (BIDS-2959) workaround for the shaking loading component

--- v2/frontend/components/bc/searchbar/CategorySelectors.vue
+++ v2/frontend/components/bc/searchbar/CategorySelectors.vue
@@ -15,81 +15,29 @@ const liveState = defineModel<CategoryFilter>({ required: true }) // each entry 
 const { t } = useI18n()
 
 function selectionHasChanged (category : Category, selected : boolean) {
-  liveState.value.set(category, selected)
+  liveState.value.set(category, selected) // the map element cannot save the infornmation from `v-model`, so we do it with .set()
   emit('change')
 }
 </script>
 
 <template>
   <div>
-    <span v-for="filter of liveState" :key="filter[0]">
-      <label class="filter-button">
-        <input
-          type="checkbox"
-          class="hidden-checkbox"
-          :true-value="true"
-          :false-value="false"
-          :checked="filter[1]"
-          :onchange="(e:any) => selectionHasChanged(filter[0], e.target.checked)"
-        >
-        <span class="face" :class="barStyle">
-          {{ t(...CategoryInfo[filter[0]].filterLabel) }}
-        </span>
-      </label>
-    </span>
+    <BcSearchbarMiniButton
+      v-for="filter of liveState"
+      :key="filter[0]"
+      v-model="filter[1]"
+      class="button"
+      :bar-style="barStyle"
+      @change="(activ : boolean) => selectionHasChanged(filter[0], activ)"
+    >
+      {{ t(...CategoryInfo[filter[0]].filterLabel) }}
+    </BcSearchbarMiniButton>
   </div>
 </template>
 
 <style lang="scss" scoped>
-@use '~/assets/css/main.scss';
-@use "~/assets/css/fonts.scss";
-
-.filter-button {
-  @include fonts.small_text_bold;
-
-  .face{
-    display: inline-block;
-    cursor: pointer;
-    border-radius: 10px;
-    height: 17px;
-    padding-top: 2.5px;
-    padding-left: 8px;
-    padding-right: 8px;
-    text-align: center;
-    margin-right: 6px;
-    transition: 0.2s;
-    margin-bottom: 8px;
-
-    &.gaudy {
-      color: var(--primary-contrast-color);
-      background-color: var(--searchbar-filter-unselected-gaudy);
-    }
-    &.discreet {
-      color: var(--light-black);
-      background-color: var(--light-grey);
-    }
-
-    &:hover {
-      background-color: var(--light-grey-3);
-    }
-    &:active {
-      background-color: var(--button-color-pressed);
-    }
-  }
-
-  .hidden-checkbox {
-    display: none;
-    width: 0;
-    height: 0;
-    &:checked + .face {
-      background-color: var(--button-color-active);
-      &:hover {
-        background-color: var(--button-color-hover);
-      }
-      &:active {
-        background-color: var(--button-color-pressed);
-      }
-    }
-  }
+.button {
+  margin-right: 6px;
+  margin-bottom: 8px;
 }
 </style>
