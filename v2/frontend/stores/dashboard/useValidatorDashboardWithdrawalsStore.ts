3d9c8d6f46fa1e844240333f06648cedf9191cbb
D13ce <stefan.benker@bitfly.at>
2024-04-25 15:06:36 +0200

    Add total withdrawals row

--- v2/frontend/stores/dashboard/useValidatorDashboardWithdrawalsStore.ts
+++ v2/frontend/stores/dashboard/useValidatorDashboardWithdrawalsStore.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import type { InternalGetValidatorDashboardWithdrawalsResponse } from '~/types/api/validator_dashboard'
+import type { InternalGetValidatorDashboardWithdrawalsResponse, InternalGetValidatorDashboardTotalWithdrawalsResponse } from '~/types/api/validator_dashboard'
 import type { DashboardKey } from '~/types/dashboard'
 import type { TableQueryParams } from '~/types/datatable'
 
@@ -10,27 +10,47 @@ const validatorDashboardWithdrawalsStore = defineStore('validator_dashboard_with
   return { data, query }
 })
 
+const validatorDashboardTotalWithdrawalStore = defineStore('validator_dashboard_total_withdrawal', () => {
+  const data = ref < InternalGetValidatorDashboardTotalWithdrawalsResponse>()
+
+  return { data }
+})
+
 export function useValidatorDashboardWithdrawalsStore () {
   const { fetch } = useCustomFetch()
-  const { data, query: storedQuery } = storeToRefs(validatorDashboardWithdrawalsStore())
+  const { data: withdrawalData, query: storedWithdrawalQuery } = storeToRefs(validatorDashboardWithdrawalsStore())
+  const { data: totalWithdrawalData } = storeToRefs(validatorDashboardTotalWithdrawalStore())
 
-  const withdrawals = computed(() => data.value)
-  const query = computed(() => storedQuery.value)
+  const withdrawals = computed(() => withdrawalData.value)
+  const query = computed(() => storedWithdrawalQuery.value)
+
+  const totalWithdrawals = computed(() => totalWithdrawalData.value)
 
   async function getWithdrawals (dashboardKey: DashboardKey, query?: TableQueryParams) {
     if (dashboardKey === undefined) {
       return undefined
     }
-    storedQuery.value = query
+    storedWithdrawalQuery.value = query
     const res = await fetch<InternalGetValidatorDashboardWithdrawalsResponse>(API_PATH.DASHBOARD_VALIDATOR_WITHDRAWALS, undefined, { dashboardKey }, query)
 
-    if (JSON.stringify(storedQuery.value) !== JSON.stringify(query)) {
+    if (JSON.stringify(storedWithdrawalQuery.value) !== JSON.stringify(query)) {
       return // in case some query params change while loading
     }
 
-    data.value = res
+    withdrawalData.value = res
     return res
   }
 
-  return { withdrawals, query, getWithdrawals }
+  async function getTotalWithdrawals (dashboardKey: DashboardKey) {
+    if (dashboardKey === undefined) {
+      return undefined
+    }
+
+    const res = await fetch<InternalGetValidatorDashboardTotalWithdrawalsResponse>(API_PATH.DASHBOARD_VALIDATOR_TOTAL_WITHDRAWALS, undefined, { dashboardKey })
+
+    totalWithdrawalData.value = res
+    return res
+  }
+
+  return { withdrawals, query, getWithdrawals, totalWithdrawals, getTotalWithdrawals }
 }
