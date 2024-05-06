b3b0bb487580a53d23ab158a97f27a8f8c592b65
D13ce <stefan.benker@bitfly.at>
2024-05-06 15:56:41 +0200

    Fix font size for expandable in rewards table

--- frontend/components/dashboard/table/DashboardTableRewardsDetails.vue
+++ frontend/components/dashboard/table/DashboardTableRewardsDetails.vue
@@ -244,17 +244,16 @@ const openDuties = () => {
   }
 
   .rewards-container {
-
     display: flex;
     flex-wrap: wrap;
     gap: var(--padding-xl);
+    font-size: var(--small_text_font_size);
 
     .rewards-group {
       display: flex;
       width: 360px;
 
       .col {
-
         >div,
         >span {
           height: 32px;
@@ -263,7 +262,7 @@ const openDuties = () => {
 
           &:last-child {
             border-top: solid 1px var(--container-border-color);
-            font-weight: var(--standard_text_medium_font_weight);
+            font-weight: var(--small_text_bold_font_weight);
           }
         }
 
@@ -305,7 +304,7 @@ const openDuties = () => {
 
         &:last-child {
           border-top: solid 1px var(--container-border-color);
-          font-weight: var(--standard_text_medium_font_weight);
+          font-weight: var(--small_text_bold_font_weight);
         }
       }
     }
@@ -313,7 +312,6 @@ const openDuties = () => {
 }
 
 @media screen and (max-width: 1180px) {
-
   .details-container {
     .small-screen-value {
       display: flex;
@@ -322,7 +320,6 @@ const openDuties = () => {
 }
 
 @media screen and (max-width: 900px) {
-
   .details-container {
     width: 400px;
     padding: var(--padding) var(--padding-large);
