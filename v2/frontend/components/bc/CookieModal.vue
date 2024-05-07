c38b74ce320d66cdb070ea2f2bd108750bcd40f2
D13ce <stefan.benker@bitfly.at>
2024-05-07 10:43:49 +0200

    Improve styling

--- v2/frontend/components/bc/CookieModal.vue
+++ v2/frontend/components/bc/CookieModal.vue
@@ -14,16 +14,63 @@ const visible = computed(() => cookiePreference.value === undefined)
 
 <template>
   <BcDialog v-model="visible">
-    {{ $t('cookies.text') }}
-    <Button @click="setCookiePreference('all')">
-      {{ $t('cookies.accept_all') }}
-    </Button>
-    <Button @click="setCookiePreference('functional')">
-      {{ $t('cookies.only_necessary') }}
-    </Button>
+    <div class="dialog-container">
+      <div class="text-container">
+        {{ $t('cookies.text') }}
+      </div>
+      <div class="button-container">
+        <Button @click="setCookiePreference('all')">
+          {{ $t('cookies.accept_all') }}
+        </Button>
+        <Button class="necessary-button" @click="setCookiePreference('functional')">
+          {{ $t('cookies.only_necessary') }}
+        </Button>
+      </div>
+    </div>
   </BcDialog>
 </template>
 
 <style lang="scss" scoped>
+@use "~/assets/css/fonts.scss";
+
+.dialog-container {
+  display: flex;
+  align-items: center;
+  gap: 35px;
+
+  @media (max-width: 670px) {
+    flex-direction: column;
+    gap: var(--padding);
+  }
+
+  .text-container {
+    // TODO: Required? Check once font size PR is merged
+    @include fonts.standard_text;
+    //line-height: 1.5; // TODO: Required?
+    width: 297px;
+
+    @media (max-width: 670px) {
+      width: auto;
+      max-width: 360px;
+    }
+  }
+
+  .button-container {
+    display: flex;
+    gap: 7px;
+    min-width: max-content;
+
+    @media (max-width: 670px) {
+      flex-direction: column;
+      gap: 9px;
+      width: 100%;
+    }
+
+    .necessary-button {
+      background-color: var(--button-color-dark-pattern);
+      border-color: var(--button-color-dark-pattern);
+    }
+  }
+}
 
 </style>
