eaae79142432ec3494e0b630ef6a7939957b29f6
D13ce <stefan.benker@bitfly.at>
2024-05-07 13:59:22 +0200

    Add link to modal text

--- frontend/utils/translation.ts
+++ frontend/utils/translation.ts
@@ -1,13 +1,20 @@
 import type { ComposerTranslation } from '@nuxtjs/i18n/dist/runtime/composables'
 
-export function formatMultiPartSpan (t:ComposerTranslation, key: string, classes:(string | undefined)[], options?: any) {
+export function formatMultiPartSpan (t:ComposerTranslation, key: string, classes:(string | undefined)[], links?:(string | undefined)[], options?: any) {
   const parts = classes.map((c, index) => {
     const value = t(`${key}[${index}]`, options, '')
     if (!value) {
       return undefined
     }
     const classString = c ? `class="${c}"` : ''
-    return `<span ${classString}>${value}</span>`
+
+    const spanElement = `<span ${classString}>${value}</span>`
+
+    if (links?.[index]) {
+      return `<a href="${links[index]}" target="_blank">${spanElement}</a>`
+    }
+
+    return spanElement
   }).filter(v => !!v)
 
   return `<span>${parts.join(' ')}</span>`
