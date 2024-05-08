44cc2a7cfb33e98e94b039bc9cb99f77bb506c65
D13ce <stefan.benker@bitfly.at>
2024-05-08 09:05:23 +0200

    Style header line

--- v2/frontend/pages/pricing.vue
+++ v2/frontend/pages/pricing.vue
@@ -4,13 +4,50 @@
 
 <template>
   <BcPageWrapper>
-    <h1>
-      Pricing
-    </h1>
-    Welcome to the pricing page! Please note, this is still under development.
+    <div class="page-container">
+      <div>
+        Beaconcha.in/API Keys Toggle
+      </div>
+      <div class="header-line">
+        <div class="premium">
+          Beaconcha.in Premium
+        </div>
+        <h1 class="monitoring">
+          Monitoring without limits on web and mobile.
+        </h1>
+      </div>
+      <div>
+        Monthly/Yearly Toggle
+      </div>
+    </div>
   </BcPageWrapper>
 </template>
 
 <style lang="scss" scoped>
+@use '~/assets/css/fonts.scss';
 
+.page-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .header-line {
+    display: flex;
+    flex-direction: column;
+    padding: 21px 0 21px 360px;
+    width: 100%;
+    border: 1px solid var(--container-border-color);
+    background: var(--container-background);
+
+    .premium {
+      @include fonts.subtitle_text;
+      color: var(--primary-color);
+    }
+
+    .monitoring {
+      margin-bottom: 0;
+    }
+  }
+}
 </style>
