80298a0117f406ee19afbf3d2a8071b05dfb1807
D13ce <stefan.benker@bitfly.at>
2024-05-08 08:42:59 +0200

    Add pricing page draft

---
+++ frontend/pages/pricing.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <BcPageWrapper>
+    <h1>
+      Pricing
+    </h1>
+    Welcome to the pricing page! Please note, this is still under development.
+  </BcPageWrapper>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
