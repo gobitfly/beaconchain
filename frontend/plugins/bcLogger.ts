2a7242d88a74410def0e7de1008f25870469a44f
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-05-08 09:38:40 +0200

    log to file during ssr

--- frontend/plugins/bcLogger.ts
+++ frontend/plugins/bcLogger.ts
@@ -1,10 +1,20 @@
+import * as fs from 'fs'
+import * as path from 'path'
 import { warn } from 'vue'
 
 export default defineNuxtPlugin((_nuxtApp) => {
+  const { public: { logFile } } = useRuntimeConfig()
   return {
     provide: {
       bcLogger: {
-        warn: (...rest:any) => warn('Warn', rest)
+        warn: (msg: string, ...rest:any) => {
+          const ts = new Date().toISOString()
+          if (process.server && logFile) {
+            const filePath = path.resolve(logFile)
+            fs.appendFileSync(filePath, `${ts}: ${msg} | ${JSON.stringify(rest)}\n`)
+          }
+          warn(`${ts}: `, ...rest)
+        }
       }
     }
   }
