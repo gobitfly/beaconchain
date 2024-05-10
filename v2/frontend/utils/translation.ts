aa61c49fbc8ae1492daceb5aa329dad556a513db
D13ce <stefan.benker@bitfly.at>
2024-05-10 09:26:45 +0200

    Remove link parameter from formatMultiPartSpan

--- v2/frontend/utils/translation.ts
+++ v2/frontend/utils/translation.ts
@@ -1,20 +1,13 @@
 import type { ComposerTranslation } from '@nuxtjs/i18n/dist/runtime/composables'
 
-export function formatMultiPartSpan (t: ComposerTranslation, key: string, classes:(string | undefined)[], links?:(string | undefined)[], options?: any) {
+export function formatMultiPartSpan (t: ComposerTranslation, key: string, classes: (string | undefined)[], options?: any) {
   const parts = classes.map((c, index) => {
     const value = t(`${key}[${index}]`, options, 'NOT_FOUND')
     if (!value || value === 'NOT_FOUND') {
       return undefined
     }
     const classString = c ? `class="${c}"` : ''
-
-    const spanElement = `<span ${classString}>${value}</span>`
-
-    if (links?.[index]) {
-      return `<a href="${links[index]}" target="_blank">${spanElement}</a>`
-    }
-
-    return spanElement
+    return `<span ${classString}>${value}</span>`
   }).filter(v => !!v)
 
   return `<span>${parts.join(' ')}</span>`
