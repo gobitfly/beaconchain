3dc099607de99674b12e22e83258dfeb13edd271
D13ce <stefan.benker@bitfly.at>
2024-05-15 11:58:45 +0200

    Draft PricingPremiumPlanBox

---
+++ v2/frontend/components/pricing/PricingPremiumPlanBox.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { type PremiumPlan } from '~/types/pricing'
+
+interface Props {
+  plan?: PremiumPlan,
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    [{{ props.plan?.Name }}]
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
