5b1431d5d5d0aa4ebb0772e1914a6d3dadfb0e6a
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-05-16 11:57:43 +0200

    no clicki click

--- frontend/components/bc/header/MainHeader.vue
+++ frontend/components/bc/header/MainHeader.vue
@@ -59,12 +59,12 @@ const userMenu = computed(() => {
           beaconcha.in
         </NuxtLink>
         <span v-if="latestState?.current_slot" class="info"><span>{{ $t('header.current_slot') }}</span>:
-          <NuxtLink :to="`/slot/${latestState.current_slot}`" :no-prefetch="true">
+          <NuxtLink :to="`/slot/${latestState.current_slot}`" :no-prefetch="true" :disabled="!showInDevelopment">
             <BcFormatNumber :value="latestState.current_slot" />
           </NuxtLink>
         </span>
         <span v-if="currentEpoch !== undefined" class="info"><span>{{ $t('header.current_epoch') }}</span>:
-          <NuxtLink :to="`/epoch/${currentEpoch}`" :no-prefetch="true">
+          <NuxtLink :to="`/epoch/${currentEpoch}`" :no-prefetch="true" :disabled="!showInDevelopment">
             <BcFormatNumber :value="currentEpoch" />
           </NuxtLink>
         </span>
