dd7858822d27c975bee78f889b5e56fa45091e80
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2024-05-17 07:39:43 +0200

    we are absolutly sure about that new format (#308)

---
+++ frontend/utils/dashboard/table.ts
@@ -0,0 +1,5 @@
+import type { ValueConvertOptions } from '~/types/value'
+
+export const formatRewardValueOption: ValueConvertOptions = {
+  addPlus: true, maxDecimalCount: 6, minUnit: 'MAIN', minDecimalCount: 0
+}
