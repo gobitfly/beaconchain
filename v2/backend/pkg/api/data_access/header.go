7dea69cdacd40cfaa8bccc75d6313bada3a76a54
Stefan Pletka <stefan.pletka@bitfly.at>
2024-05-22 11:34:57 +0200

    Marked methods as being worked on

--- v2/backend/pkg/api/data_access/header.go
+++ v2/backend/pkg/api/data_access/header.go
@@ -3,11 +3,11 @@ package dataaccess
 import t "github.com/gobitfly/beaconchain/pkg/api/types"
 
 func (d *DataAccessService) GetLatestSlot() (uint64, error) {
-	// TODO @recy21
+	// WORKING spletka
 	return d.dummy.GetLatestSlot()
 }
 
 func (d *DataAccessService) GetLatestExchangeRates() ([]t.EthConversionRate, error) {
-	// TODO @recy21
+	// WORKING spletka
 	return d.dummy.GetLatestExchangeRates()
 }
