4fb715bbb07bbad0a6eca3ac6d77da71adca41ab
D13ce <stefan.benker@bitfly.at>
2024-05-22 13:44:19 +0200

    Improve styling for footer

--- frontend/components/bc/footer/MainFooter.vue
+++ frontend/components/bc/footer/MainFooter.vue
@@ -3,60 +3,62 @@
 </script>
 
 <template>
-  <div id="main-footer">
-    <div id="links">
+  <div class="main-footer">
+    <div class="links">
       <BcFooterLinkList />
     </div>
-    <div id="very-last-line">
+    <div class="very-last-line">
       <BcFooterCopyrightAndVersion />&nbsp;|&nbsp;<BcFooterColorThemeToggle />
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-#main-footer {
+.main-footer {
+  width: 100%;
   display: flex;
   flex-direction: column;
-  left: 0;
-  right: 0;
+  gap: var(--padding-large);
+  padding-top: var(--padding-large);
+  margin-top: 64px;
+  background-color: var(--container-background);
+
   @media (min-width: 600px) { // large screen
     justify-content: center;
   }
+
   @media (max-width: 599.9px) { // mobile
     justify-content: flex-start;
-    width: 100%;
   }
-  >div{
-    padding-top: 4px;
-    padding-bottom: 13px;
-    padding-left: 10px;
-  }
-}
 
-#links {
-  display: flex;
-  @media (min-width: 600px) { // large screen
-    justify-content: center;
+  .links {
+    display: flex;
+
+    @media (min-width: 600px) { // large screen
+      justify-content: center;
+      flex-direction: row;
+      gap: 130px;
+    }
+
+    @media (max-width: 599.9px) { // mobile
+      justify-content: flex-start;
+      align-items: flex-start;
+      flex-direction: column;
+    }
+  }
+
+  .very-last-line {
+    display: flex;
     flex-direction: row;
-    gap: 130px;
-  }
-  @media (max-width: 599.9px) { // mobile
-    justify-content: flex-start;
-    align-items: flex-start;
-    flex-direction: column;
-    gap: 0px;
-  }
-}
+    padding-bottom: 13px;
 
-#very-last-line {
-  display: flex;
-  flex-direction: row;
-  margin-top: 10px;
-  @media (min-width: 600px) { // large screen
-    justify-content: center;
-  }
-  @media (max-width: 599.9px) { // mobile
-    justify-content: flex-start;
+    @media (min-width: 600px) { // large screen
+      justify-content: center;
+    }
+
+    @media (max-width: 599.9px) { // mobile
+      justify-content: flex-start;
+    }
   }
 }
 </style>
