ed154316225431997e983b1ccdc4f54388010349
thib-wien <thibaut.barthelemy@bitfly.at>
2024-05-23 15:54:43 +0200

    (BIDS-3084) rewriting of the CSS and reorganization of the template

--- frontend/components/bc/header/MainHeader.vue
+++ frontend/components/bc/header/MainHeader.vue
@@ -47,38 +47,38 @@ const userMenu = computed(() => {
     }
   ]
 })
-
 </script>
 
 <template>
-  <div class="header top">
-    <div class="content">
-      <div class="left-content">
-        <NuxtLink to="/" class="logo">
-          <IconBeaconchainLogo alt="Beaconcha.in logo" />
-          beaconcha.in
-        </NuxtLink>
-        <span v-if="latestState?.current_slot" class="info"><span>{{ $t('header.current_slot') }}</span>:
+  <div class="anchor">
+    <div class="top-background" />
+    <div class="rows">
+      <div class="grid-cell blockchain-info">
+        <span v-if="latestState?.current_slot"><span>{{ $t('header.current_slot') }}</span>:
           <NuxtLink :to="`/slot/${latestState.current_slot}`" :no-prefetch="true" :disabled="!showInDevelopment || null">
             <BcFormatNumber class="bold" :value="latestState.current_slot" />
           </NuxtLink>
         </span>
-        <span v-if="currentEpoch !== undefined" class="info"><span>{{ $t('header.current_epoch') }}</span>:
+        <span v-if="currentEpoch !== undefined"><span>{{ $t('header.current_epoch') }}</span>:
           <NuxtLink :to="`/epoch/${currentEpoch}`" :no-prefetch="true" :disabled="!showInDevelopment || null">
             <BcFormatNumber class="bold" :value="currentEpoch" />
           </NuxtLink>
         </span>
-        <span v-if="rate" class="info">
+        <span v-if="rate">
           <span>
-            <IconNetworkEthereum class="icon monochromatic" />ETH
+            <IconNetworkEthereum class="network-icon monochromatic" />ETH
           </span>:
           <span> {{ rate.symbol }}
             <BcFormatNumber class="bold" :value="rate.rate" :max-decimals="2" />
           </span>
         </span>
       </div>
-      <BcSearchbarGeneral v-if="showInDevelopment && !props.isHomePage" class="search" bar-style="discreet" />
-      <div class="right-content">
+
+      <div class="grid-cell search-bar">
+        <BcSearchbarGeneral v-if="showInDevelopment && !props.isHomePage" class="bar" :bar-style="width < 1023 ? 'gaudy' : 'discreet'" />
+      </div>
+
+      <div class="grid-cell controls">
         <BcCurrencySelection v-if="!isMobile" class="currency" />
         <div v-if="!isLoggedIn" class="logged-out">
           <NuxtLink to="/login">
@@ -105,173 +105,178 @@ const userMenu = computed(() => {
           <FontAwesomeIcon :icon="faBars" />
         </div>
       </div>
-    </div>
-  </div>
-  <div class="header bottom">
-    <div class="content">
-      <NuxtLink to="/dashboard" class="logo">
-        <IconBeaconchainLogo alt="Beaconcha.in logo" />
-        beaconcha.in
-      </NuxtLink>
-      <BcHeaderMegaMenu ref="megaMenu" />
-      <BcSearchbarGeneral v-if="showInDevelopment && !props.isHomePage" class="search" bar-style="discreet" />
+
+      <div class="grid-cell logo">
+        <NuxtLink to="/" class="logo-component">
+          <IconBeaconchainLogo alt="Beaconcha.in logo" />
+          beaconcha.in
+        </NuxtLink>
+      </div>
+
+      <div class="grid-cell mega-menu">
+        <BcHeaderMegaMenu ref="megaMenu" />
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.header {
-  width: 100%;
+.anchor {
+  position: relative;
   display: flex;
-  align-items: center;
+  width: 100%;
   justify-content: center;
-  color: var(--light-grey);
-
-  &.top {
+  border-bottom: 1px solid var(--container-border-color);
+  background-color: var(--container-background);
+  color: var(--container-color);
+  .top-background {
+    position: absolute;
+    width: 100%;
     height: var(--navbar-height);
     background-color: var(--dark-blue);
-
-    .user-menu-icon {
-      width: 19px;
-      height: 18px;
-      color: var(--light-grey);
-    }
-
-    .logo {
-      display: none;
-    }
-
-    @media (max-width: 1023px) {
-
-      .search,
-      .info {
-        display: none;
-      }
-
-      .logo {
-        display: flex;
-      }
-    }
-
-    @media (max-width: 469px) {
-      .currency {
-        display: none;
-      }
-    }
   }
 
-  &.bottom {
-    min-height: var(--navbar2-height);
-    background-color: var(--container-background);
-    color: var(--container-color);
-    border-bottom: 1px solid var(--container-border-color);
-
-    .search {
-      display: none;
-    }
-
+  .rows {
+    position: relative;
+    display: grid;
+    grid-template-columns: min-content min-content auto min-content;
+    grid-template-rows: var(--navbar-height) min-content;
     @media (max-width: 1023px) {
-      min-height: unset;
-
-      .content {
-        flex-direction: column;
-      }
-
-      .logo {
-        display: none;
-      }
-
-      .search {
-        display: flex;
-        width: 100%;
-        margin-top: var(--content-margin);
-        margin-bottom: var(--content-margin);
-      }
+      grid-template-columns: min-content auto min-content;
+      grid-template-rows: var(--navbar-height) min-content;
     }
-  }
-
-  .content {
     width: var(--content-width);
     margin-left: var(--content-margin);
     margin-right: var(--content-margin);
-    align-items: center;
-    display: flex;
-    justify-content: space-between;
-    margin-top: auto;
-    margin-bottom: auto;
-    vertical-align: middle;
-    align-items: center;
+    color: var(--light-grey);
+    @mixin bottom-cell($row) {
+      color: var(--container-color);
+      grid-row: $row;
+    }
     font-family: var(--main_header_font_family);
     font-size: var(--main_header_font_size);
     font-weight: var(--main_header_font_weight);
-
     .bold {
       font-weight: var(--main_header_bold_font_weight);
     }
-
-    .left-content {
+    .grid-cell {
+      position: relative;
       display: flex;
+      margin-top: auto;
+      margin-bottom: auto;
       align-items: center;
-      gap: var(--padding-large);
+      vertical-align: middle;
+      height: 100%;
+      gap: var(--padding);
+    }
 
-      .icon {
+    .blockchain-info {
+      @media (min-width: 1023.1px) {
+        grid-row: 1;
+        grid-column: 1;
+        grid-column-end: span 2;
+      }
+      @media (max-width: 1023px) {
+        display: none;
+      }
+      white-space: nowrap;
+      .network-icon {
         height: 14px;
         width: auto;
         margin-right: var(--padding-small);
       }
-
     }
 
-    .right-content {
-      display: flex;
-      align-items: center;
-      gap: var(--padding);
+    .search-bar {
+      grid-row: 1;
+      grid-column: 3;
+      @media (max-width: 1023px) {
+        @include bottom-cell(3);
+        grid-column: 1;
+        grid-column-end: span 3;
+      }
+      .bar {
+        position: relative;
+        margin-top: var(--content-margin);
+        margin-bottom: var(--content-margin);
+        @media (max-width: 1023px) {
+          width: 100%;
+        }
+      }
+    }
 
+    .controls {
+      grid-row: 1;
+      grid-column: 4;
+      @media (max-width: 1023px) {
+        grid-column: 3;
+      }
+      .currency {
+        @media (max-width: 469px) {
+          display: none;
+        }
+      }
       .logged-out {
         white-space: nowrap;
         display: flex;
         align-items: center;
         gap: var(--padding-small);
-
         .signup {
           padding: 8px;
         }
       }
-    }
-  }
-
-  .logo {
-    display: flex;
-    align-items: flex-end;
-    gap: var(--padding);
-    font-family: var(--logo_font_family);
-    font-size: var(--logo_font_size);
-    font-weight: var(--logo_font_weight);
-    letter-spacing: var(--logo_letter_spacing);
-    line-height: 20px;
-
-    @media (max-width: 1359px) {
-      font-size: var(--logo_small_font_size);
-      letter-spacing: var(--logo_small_letter_spacing);
-      gap: 6px;
-      align-items: center;
-
-      svg {
+      .user-menu-icon {
+        width: 19px;
         height: 18px;
-        margin-bottom: 7px;
+        color: var(--light-grey);
+      }
+      .burger {
+        cursor: pointer;
       }
     }
 
-  }
+    .logo {
+      grid-column: 1;
+      @media (min-width: 1023.1px) {
+        @include bottom-cell(2);
+      }
+      @media (max-width: 1023px) {
+        grid-row: 1;
+      }
+      .logo-component {
+        display: flex;
+        align-items: flex-end;
+        gap: var(--padding);
+        font-family: var(--logo_font_family);
+        font-size: var(--logo_font_size);
+        font-weight: var(--logo_font_weight);
+        letter-spacing: var(--logo_letter_spacing);
+        line-height: 20px;
+        @media (max-width: 1359px) {
+          font-size: var(--logo_small_font_size);
+          letter-spacing: var(--logo_small_letter_spacing);
+          gap: 6px;
+          align-items: center;
+          svg {
+            height: 18px;
+            margin-bottom: 7px;
+          }
+        }
+      }
+    }
 
-  .burger {
-    cursor: pointer;
+    .mega-menu {
+      @media (min-width: 1023.1px) {
+        grid-column: 2;
+        grid-column-end: span 3;
+        @include bottom-cell(2);
+      }
+      @media (max-width: 1023px) {
+        grid-row: 2;
+        grid-column: 1;
+        grid-column-end: span 3;
+      }
+    }
   }
-}
-
-.page {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 }
 </style>
