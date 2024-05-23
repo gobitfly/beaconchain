dbaca3307add66ff491ddff7bf2e8f54fc9951d7
D13ce <stefan.benker@bitfly.at>
2024-05-23 15:47:18 +0200

    Add ad_free support

--- frontend/composables/useCurrentAds.ts
+++ frontend/composables/useCurrentAds.ts
@@ -3,6 +3,7 @@ import type { AdConfiguration } from '~/types/adConfiguration'
 
 export function useCurrentAds () {
   const { adConfigs, refreshAdConfigs } = useAdConfigurationStore()
+  const { user } = useUserStore()
   const { path, name } = useRoute()
 
   const pathName = computed(() => name?.toString?.() || path)
@@ -11,8 +12,11 @@ export function useCurrentAds () {
     refreshAdConfigs(newName)
   }, { immediate: true })
 
-  // TODO: also validate if user is premium user and config is not for all
-  const ads = computed(() => {
+  const ads = computed<AdConfiguration[]>(() => {
+    if (user.value?.premium_perks.ad_free) {
+      return []
+    }
+
     const configs: AdConfiguration[] = adConfigs.value[pathName.value]?.filter(c => c.enabled) ?? []
     adConfigs.value.global?.forEach((config) => {
       if (config.enabled && !configs.find(c => c.jquery_selector === config.jquery_selector && c.insert_mode === config.insert_mode)) {
