f1dbe3ab164af69da50cdd178df1e206e13d2385
D13ce <stefan.benker@bitfly.at>
2024-05-28 11:07:33 +0200

    Refactor pricing page

--- v2/frontend/pages/pricing.vue
+++ v2/frontend/pages/pricing.vue
@@ -63,20 +63,7 @@ const scrollToAddons = () => {
           {{ $t('pricing.save_up_to', {percentage: savingPercentage}) }}
         </div>
       </div>
-      <div class="premium-products-container">
-        <div class="premium-products-row">
-          <template v-for="product in products?.premium_products" :key="product.product_id">
-            <PricingPremiumProductBox
-              v-if="product.price_per_year_eur > 0"
-              :product
-              :is-yearly="isYearly"
-            />
-          </template>
-        </div>
-        <div class="footnote">
-          {{ $t('pricing.excluding_vat') }}
-        </div>
-      </div>
+      <PricingPremiumProducts :is-yearly="isYearly" />
       <Button class="view-addons-button" @click="scrollToAddons()">
         {{ $t('pricing.view_addons') }}<FontAwesomeIcon :icon="faArrowDown" />
       </Button>
@@ -199,29 +186,6 @@ const scrollToAddons = () => {
     }
   }
 
-  .premium-products-container {
-    width: 100%;
-
-    .premium-products-row {
-      display: flex;
-      gap: 17px;
-      justify-content: space-between;
-      overflow-x: auto;
-      padding-bottom: 7px;
-    }
-
-    .footnote{
-      font-family: var(--roboto-family);
-      font-size: 14px;
-      font-weight: 400;
-      color: var(--text-color-discreet);
-      display: flex;
-      justify-content: flex-end;
-    }
-
-    margin-bottom: 38px;
-  }
-
   .view-addons-button {
     width: 215px;
     height: 45px;
@@ -280,14 +244,6 @@ const scrollToAddons = () => {
       .save-up-text {
         width: 60px;
         font-size: 12px;
-      }
-    }
-
-    .premium-products-container{
-      margin-bottom: 36px;
-
-      .footnote {
-        font-size: 8px;
       }
     }
 
