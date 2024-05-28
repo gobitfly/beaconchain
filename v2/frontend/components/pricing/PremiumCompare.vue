b2ef4de804ba251e5ec220a53d42d42f62b44d41
D13ce <stefan.benker@bitfly.at>
2024-05-28 12:43:21 +0200

    Refactor pricing (PremiumCompare)

---
+++ v2/frontend/components/pricing/PremiumCompare.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="compare-plans-container">
+    Compare Plans (coming soon)
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+.compare-plans-container {
+  width: 100%;
+  height: 500px;
+
+  background-color: var(--container-background);
+  border: 2px solid var(--container-border-color);
+  border-radius: 7px;
+  font-size: 50px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin-bottom: 43px;
+}
+</style>
