403a9265036c3b4410e8329e3133047bc9b803a8
D13ce <stefan.benker@bitfly.at>
2024-05-28 11:19:05 +0200

    Refactor pricing (HeaderLine)

---
+++ v2/frontend/components/pricing/HeaderLine.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="header-line-container">
+    <div class="header-line">
+      <div class="title">
+        {{ $t('pricing.premium') }}
+      </div>
+      <div class="subtitle">
+        {{ $t('pricing.subtitle') }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header-line-container {
+  width: 100vw;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  padding: 21px 0;
+  border: 1px solid var(--container-border-color);
+  background: var(--container-background);
+  margin-bottom: 55px;
+
+  .header-line {
+    width: var(--content-width);
+    display: flex;
+    flex-direction: column;
+
+    .title {
+      font-size: 18px;
+      color: var(--primary-color);
+      margin-bottom: var(--padding);
+    }
+
+    .subtitle {
+      font-size: 32px;
+    }
+  }
+}
+
+@media (max-width: 600px) {
+  .header-line-container{
+      padding-left: 5px;
+      margin-bottom: 30px;
+
+      .header-line .subtitle {
+        font-size: 20px;
+      }
+    }
+}
+</style>
--- v2/frontend/pages/pricing.vue
+++ v2/frontend/pages/pricing.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import { faArrowDown } from '@fortawesome/pro-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import TypeToggle from '~/components/pricing/TypeToggle.vue'
 
 const { t: $t } = useI18n()
 
@@ -31,17 +30,8 @@ const scrollToAddons = () => {
 <template>
   <BcPageWrapper>
     <div class="page-container">
-      <TypeToggle />
-      <div class="header-line-container">
-        <div class="header-line">
-          <div class="title">
-            {{ $t('pricing.premium') }}
-          </div>
-          <div class="subtitle">
-            {{ $t('pricing.subtitle') }}
-          </div>
-        </div>
-      </div>
+      <PricingTypeToggle />
+      <PricingHeaderLine />
       <div class="toggle-container">
         <BcToggle
           v-model="isYearly"
@@ -80,33 +70,6 @@ const scrollToAddons = () => {
   align-items: center;
   justify-content: center;
   padding-top: 25px;
-
-  .header-line-container {
-      width: 100vw;
-      box-sizing: border-box;
-      display: flex;
-      justify-content: center;
-      padding: 21px 0;
-      border: 1px solid var(--container-border-color);
-      background: var(--container-background);
-      margin-bottom: 55px;
-
-      .header-line {
-        width: var(--content-width);
-        display: flex;
-        flex-direction: column;
-
-        .title {
-          font-size: 18px;
-          color: var(--primary-color);
-          margin-bottom: var(--padding);
-        }
-
-        .subtitle {
-          font-size: 32px;
-        }
-    }
-  }
 
   .toggle-container {
     display: flex;
@@ -154,15 +117,6 @@ const scrollToAddons = () => {
   }
 
   @media (max-width: 600px) {
-    .header-line-container{
-      padding-left: 5px;
-      margin-bottom: 30px;
-
-      .header-line .subtitle {
-        font-size: 20px;
-      }
-    }
-
     .toggle-container {
       font-size: 16px;
       margin-bottom: 30px;
