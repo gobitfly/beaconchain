9e12b65e1537028a6e8373c39419338bdea12278
Manuel <5877862+manuelsc@users.noreply.github.com>
2024-06-04 16:13:16 +0200

    fix

--- v2/backend/pkg/exporter/modules/dashboard_data_w_rolling.go
+++ v2/backend/pkg/exporter/modules/dashboard_data_w_rolling.go
@@ -50,9 +50,9 @@ func (d *RollingAggregator) getCurrentRollingBounds(tx *sqlx.Tx, tableName strin
 	var bounds edb.EpochBounds
 	var err error
 	if tx == nil {
-		err = db.AlloyWriter.Get(&bounds, fmt.Sprintf(`SELECT max(epoch_start) as epoch_start, epoch_end as epoch_end FROM %s LIMIT 1`, tableName))
+		err = db.AlloyWriter.Get(&bounds, fmt.Sprintf(`SELECT max(epoch_start) as epoch_start, max(epoch_end) as epoch_end FROM %s`, tableName))
 	} else {
-		err = tx.Get(&bounds, fmt.Sprintf(`SELECT max(epoch_start) as epoch_start, epoch_end as epoch_end FROM %s LIMIT 1`, tableName))
+		err = tx.Get(&bounds, fmt.Sprintf(`SELECT max(epoch_start) as epoch_start, max(epoch_end) as epoch_end FROM %s`, tableName))
 	}
 	return bounds, err
 }
