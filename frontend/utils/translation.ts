2730988c03e9e63c1880cf78f7493e5f31d61e96
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-06-04 09:13:07 +0200

    some comments

--- frontend/utils/translation.ts
+++ frontend/utils/translation.ts
@@ -13,15 +13,18 @@ export function formatMultiPartSpan (t: ComposerTranslation, key: string, classe
   return `<span>${parts.join(' ')}</span>`
 }
 
+// Translation with default. Needed if we want to default to '' as the translation lib does not support to default to ''
 export function tD (t: ComposerTranslation, path : string, options?: any, d:string = '') : string {
   const translation = t(path, 'NOT_FOUND', { ...options, missingWarn: false })
   return (translation === 'NOT_FOUND') ? d : translation
 }
 
+// get a translation at a specific index, if it exists
 export function tOf (t: ComposerTranslation, path : string, index : number, options?: any) : string {
   return tD(t, `${path}[${index}]`, options)
 }
 
+// get all translations for an array
 export function tAll (t: ComposerTranslation, path : string, options?: any) : string[] {
   const list: string[] = []
   let index = 0
