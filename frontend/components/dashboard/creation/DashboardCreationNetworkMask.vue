0d1ae7054b5329be5e558547b3d5ff060bb00fda
thib-wien <thibaut.barthelemy@bitfly.at>
2024-06-05 14:09:31 +0200

    (BIDS-3067) brain bug fixed

--- frontend/components/dashboard/creation/DashboardCreationNetworkMask.vue
+++ frontend/components/dashboard/creation/DashboardCreationNetworkMask.vue
@@ -16,7 +16,7 @@ const buttonList = ValidatorDashboardNetworkList.map((chainId) => {
   return {
     value: String(chainId),
     text: ChainInfo[chainId].name,
-    disabled: !useRuntimeConfig().public.showInDevelopment && chainId !== currentNetwork.value, // TODO: simply set `true` for everything once dashboards can be created for all networks in `ValidatorDashboardNetworkList`
+    disabled: !useRuntimeConfig().public.showInDevelopment && chainId !== currentNetwork.value, // TODO: simply set `false` for everything once dashboards can be created for all networks in `ValidatorDashboardNetworkList`
     componentClass: 'dashboard-creation-button-network-icon'
   }
 })
