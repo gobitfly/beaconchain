0f923bc0150f81c91db3f8d29a08952bd5bafaa9
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-06-10 09:24:21 +0200

    commited to fast

--- frontend/components/dashboard/table/DashboardTableSummary.vue
+++ frontend/components/dashboard/table/DashboardTableSummary.vue
@@ -72,7 +72,7 @@ const getRowClass = (row: VDBSummaryTableRow) => {
   }
 }
 
-const searchPlaceholder = computed(() => $t(isPublic.value ? 'dashboard.validator.summary.search_placeholder_public' : 'dashboard.validator.summary.search_placeholder') : undefined)
+const searchPlaceholder = computed(() => $t(isPublic.value ? 'dashboard.validator.summary.search_placeholder_public' : 'dashboard.validator.summary.search_placeholder'))
 
 </script>
 <template>
