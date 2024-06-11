588b4327888272eda5b75e22a4d7c3dd0c0b547d
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2024-06-11 15:28:59 +0200

    label pending cl rewards on blocks table (#427)
    * label pending cl rewards on blocks table
    
    * Fix logic and styling
    
    ---------
    
    Co-authored-by: D13ce <stefan.benker@bitfly.at>

--- v2/frontend/components/block/table/BlockTableRewardItem.vue
+++ v2/frontend/components/block/table/BlockTableRewardItem.vue
@@ -11,7 +11,8 @@ defineProps<Props>()
 <template>
   <BcTooltip v-if="status === 'success' && reward" class="combine-rewards">
     <BcFormatValue :value="reward?.el" :no-tooltip="true" />
-    <BcFormatValue :value="reward?.cl" :no-tooltip="true" />
+    <BcFormatValue v-if="reward?.cl && reward.cl != '0'" :value="reward?.cl" :no-tooltip="true" />
+    <span v-else>{{ $t('dashboard.validator.blocks.cl_pending') }}</span>
     <template #tooltip>
       <div>
         <div class="tt-row">
@@ -20,7 +21,8 @@ defineProps<Props>()
         </div>
         <div class="tt-row">
           <span>{{ $t('dashboard.validator.blocks.cl_rewards') }}: </span>
-          <BcFormatValue :value="reward?.cl" :no-tooltip="true" :full-value="true" />
+          <BcFormatValue v-if="reward?.cl && reward.cl != '0'" :value="reward?.cl" :no-tooltip="true" :full-value="true" />
+          <span v-else>{{ $t('dashboard.validator.blocks.pending') }}</span>
         </div>
       </div>
     </template>
@@ -40,7 +42,7 @@ defineProps<Props>()
   display: inline-flex;
   flex-direction: column;
 
-  >div:last-child {
+  >div:last-child, >span:last-child {
     font-size: var(--small_text_font_size);
     color: var(--text-color-discreet);
   }
