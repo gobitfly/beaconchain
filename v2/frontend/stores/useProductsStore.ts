d1a48dc20b2118739ee186c5058e4c4c362943b2
D13ce <stefan.benker@bitfly.at>
2024-06-12 13:58:58 +0200

    Implement PremiumViaAppBanner

--- v2/frontend/stores/useProductsStore.ts
+++ v2/frontend/stores/useProductsStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import type { InternalGetProductSummaryResponse, ProductSummary } from '~/types/api/user'
 import { API_PATH } from '~/types/customFetch'
+import { ProductCategoryPremium } from '~/types/api/user'
 
 const productsStore = defineStore('products_store', () => {
   const data = ref <ProductSummary>()
@@ -11,11 +12,16 @@ const productsStore = defineStore('products_store', () => {
 export function useProductsStore () {
   const { fetch } = useCustomFetch()
   const { data } = storeToRefs(productsStore())
+  const { user } = useUserStore()
 
   const products = computed(() => data.value)
 
   const bestPremiumProduct = computed(() => {
     return data.value?.premium_products.reduce((max, product) => (product.price_per_year_eur > max.price_per_year_eur ? product : max), data.value.premium_products[0])
+  })
+
+  const currentPremiumSubscription = computed(() => {
+    return user.value?.subscriptions?.find(sub => sub.product_category === ProductCategoryPremium)
   })
 
   async function getProducts () {
@@ -29,5 +35,5 @@ export function useProductsStore () {
     return res
   }
 
-  return { products, getProducts, bestPremiumProduct }
+  return { products, getProducts, bestPremiumProduct, currentPremiumSubscription }
 }
