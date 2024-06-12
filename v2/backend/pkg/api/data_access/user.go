66d3bb6e46487422079d5ab6bbf61e1b9393bbf4
Patrick <guybrush@users.noreply.github.com>
2024-06-12 08:35:20 +0200

    api: update stripe logic (#441)

--- v2/backend/pkg/api/data_access/user.go
+++ v2/backend/pkg/api/data_access/user.go
@@ -26,7 +26,7 @@ func (d *DataAccessService) GetUserCredentialInfo(email string) (*t.UserCredenti
 		WITH
 			latest_and_greatest_sub AS (
 				SELECT user_id, product_id FROM users_app_subscriptions 
-				LEFT JOIN users ON users.id = user_id 
+				LEFT JOIN users ON users.id = user_id AND product_id IN ('orca.yearly', 'orca', 'dolphin.yearly', 'dolphin', 'guppy.yearly', 'guppy', 'whale', 'goldfish', 'plankton')
 				WHERE users.email = $1 AND active = true
 				ORDER BY CASE product_id
 					WHEN 'orca.yearly'    THEN  1
@@ -107,7 +107,7 @@ func (d *DataAccessService) GetUserInfo(userId uint64) (*t.UserInfo, error) {
 			to_timestamp((uss.payload->>'current_period_end')::bigint) AS end
 		FROM users_app_subscriptions uas
 		LEFT JOIN users_stripe_subscriptions uss ON uss.subscription_id = uas.subscription_id
-		WHERE uas.user_id = $1 AND uas.active = true
+		WHERE uas.user_id = $1 AND uas.active = true AND product_id IN ('orca.yearly', 'orca', 'dolphin.yearly', 'dolphin', 'guppy.yearly', 'guppy', 'whale', 'goldfish', 'plankton')
 		ORDER BY CASE uas.product_id
 			WHEN 'orca.yearly'    THEN  1
 			WHEN 'orca'           THEN  2
