430cc3ec2bddcbab812ae2f948b9cf1f36d5e33c
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2024-06-14 10:45:42 +0200

    use url hash to select tab (#468)
    * use url hash to select tab

---
+++ frontend/app/router.options.ts
@@ -0,0 +1,5 @@
+export default {
+  scrollBehavior (_to: any, _from: any, savedPosition: { left: number, top: number } | null) {
+    return { _to, ...savedPosition }
+  }
+}
---
+++ frontend/types/hashTabs.ts
@@ -0,0 +1,5 @@
+export type HashTab = {
+  index: number,
+  disabled?: boolean
+}
+export type HashTabs = Record<string, HashTab>
