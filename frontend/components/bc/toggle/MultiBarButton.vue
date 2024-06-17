d4e238a735b3b86d2ac03a1c3ee977e23fa8c64f
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-06-17 13:00:55 +0200

    also add hover delay to filter buttons

--- frontend/components/bc/toggle/MultiBarButton.vue
+++ frontend/components/bc/toggle/MultiBarButton.vue
@@ -19,7 +19,7 @@ const icon = computed(() => {
 </script>
 
 <template>
-  <BcTooltip :dont-open-permanently="true">
+  <BcTooltip :dont-open-permanently="true" :hover-delay="350">
     <template #tooltip>
       <div class="button-tooltip">
         <div>{{ selected ? $t('filter.enabled'): $t('filter.disabled') }}</div>
