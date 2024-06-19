ea4cc523d95eaf4af6e23f5139f5b1318a1c4ba7
thib-wien <thibaut.barthelemy@bitfly.at>
2024-06-19 10:10:17 +0200

    (BIDS-3131) nothing

--- frontend/components/playground/PlaygroundColorblind.vue
+++ frontend/components/playground/PlaygroundColorblind.vue
@@ -1,92 +1,145 @@
 <script lang="ts" setup>
-import type { Order } from '@stripe/stripe-js'
-
-type Point = number[]
-
-type RGB = number[]
-
-type SPI = { // simple color space supposedly close to human perception
-  s: number, // spectrum (similar to the notion of hue)
-  p: number, // purity (similar to the notion of saturation)
-  i: number // intensity (similar to the notion of value)
-}
-
 type Permutation = (0|1|2)[]
 
-function RGB24toRGB01 (rgb: RGB) : RGB {
-  return [
-    (rgb[0] / 255) ** 2.2,
-    (rgb[1] / 255) ** 2.2,
-    (rgb[2] / 255) ** 2.2
-  ]
-}
+class RGB {
+  chans: number[]
 
-function RGB01toRGB24 (rgb: RGB) : RGB {
-  return [
-    (rgb[0] ** 0.454545) * 255,
-    (rgb[1] ** 0.454545) * 255,
-    (rgb[2] ** 0.454545) * 255
-  ]
-}
+  protected format: 1 | 8
 
-function RGB01toSPI (rgb: RGB) : SPI {
-  const w = [0.3, 0.6, 0.1]
-  const [a, b, c] = order(rgb)
-  if (rgb[c] <= 0) {
-    // the highest channel is 0 so the color is black
-    return { s: 0, p: 0, i: 0 }
-  } else if (rgb[a] >= 1) {
-    // the lowest channel is 1 so the color is white
-    return { s: 0, p: 0, i: 1 }
-  }
-  const sumOfBounds = rgb[b] + rgb[c]  // sum of the two channels surrounding the color
-  let s : number
-  switch (a) { // a is the channel with the lowest value
-    case 0 : // the dominant channels are G and B
-      s = (1 + rgb[2] / sumOfBounds) / 3
-      break
-    case 1 : // the dominant channels are R and B
-      s = (2 + rgb[0] / sumOfBounds) / 3
-      break
-    case 2 : // the dominant channels are R and G
-      s = (0 + rgb[1] / sumOfBounds) / 3
-      break
-  }
-  const p = 1 - (2 * rgb[a]) / (rgb[b] + rgb[c])
-  const i = rgb[c] // this might look surprising but this value is the ratio between the absolute intensity (no matter how it is calculated, for example rgb[b] + rgb[c] or the sum of all channels or a weighted sum, etc) and the maximum absolute intensity that can be reached with s and p (so along their invariance line)
-  return { s, p, i }
-}
-
-function SPItoRGB01 (spi: SPI) : RGB {
-  if (spi.s <= 1 / 3) {
-    // the dominant channels are R and G
-    const factor = 1 / 
-    const G = 
-    return [
-      ,
-      ,
-    ]
-  } else
-    if (spi.s <= 2 / 3) {
-      // the dominant channels are G and B
-      const factor = 1 / 
-      const G = 
-      return [
-        ,
-        ,
-      ]
-    } else {
-      // the dominant channels are R and B
-      const factor = 1 / 
-      const G = 
-      return [
-        ,
-        ,
-      ]
+  constructor (from: RGB | undefined) {
+    if (!from) {
+      this.chans = [0, 0, 0]
+      this.format = 1
+      return
     }
+    this.chans = [...from.chans]
+    this.format = from.format
+  }
+
+  fillRGB1fromRGB8 (rgb8: number[] | RGB) {
+    if (!Array.isArray(rgb8)) {
+      rgb8 = rgb8.chans
+    }
+    this.chans[0] = (rgb8[0] / 255) ** 2.2
+    this.chans[1] = (rgb8[1] / 255) ** 2.2
+    this.chans[2] = (rgb8[2] / 255) ** 2.2
+    this.format = 1
+  }
+
+  fillRGB8fromRGB1 (rgb1: number[] | RGB) {
+    if (!Array.isArray(rgb1)) {
+      rgb1 = rgb1.chans
+    }
+    this.chans[0] = (rgb1[0] ** 0.454545) * 255
+    this.chans[1] = (rgb1[1] ** 0.454545) * 255
+    this.chans[2] = (rgb1[2] ** 0.454545) * 255
+    this.format = 8
+  }
+
+  limit () {
+    limit(this.chans, 0, (this.format === 1) ? 1 : 255)
+  }
 }
 
-function order (p : Point) : Permutation {
+/** color space supposedly close to human perception */
+class Eye {
+  /** Perceived wavelength indicating where the color is on the rainbow. Key values: 0 is pure red. 1/3 is pure green. 2/3 is pure blue. 1 is pure red again.
+   * Important: After changing the value of `w` or `p`, you must always set `i` or `j`. */
+  w: number
+  /** Perceived purity indicating how much light not contributing to the perceived wavelength is present.
+   * Important: After changing the value of `w` or `p`, you must always set `i` or `j`. */
+  p: number
+  /** Perceived intensity of the light, so not normalized (given `w` and `p`, the maximum perceived intensity that can be reached with `w` and `p` is often less than 1).
+   * Changing this value updates `j` accordingly. */
+  get i () : number { return this.I }
+  /** Normalized intensity of the light, any value between 0 and 1 is possible.
+   * Changing this value updates `i` accordingly. */
+  get j () : number { return this.J }
+  /** Maximum value that `i` can have under the constraint set by `w` and `p`.
+   * Updated when they change. */
+  get iMax () : number { return this.Imax }
+
+  /** Read `iMax` to know the maximum value that `i` can take for the current values of `w` and `p`. */
+  set i (val: number) {
+    this.I = val
+  }
+
+  set j (val: number) {
+    this.J = val
+  }
+
+  static channelIcontribution = [0.3, 0.6, 0.1]
+  private I: number
+  private J: number
+  private Imax: number
+
+  fillFromRGB1 (rgb1: RGB) {
+    const rgb = rgb1.chans
+    const [a, b, c] = order(rgb)
+    if (rgb[c] <= 0) {
+      // the highest channel is 0 so the color is black
+      this.w = this.p = this.I = this.J = this.Imax = 0
+      return
+    } else if (rgb[a] >= 1) {
+      // the lowest channel is 1 so the color is white
+      this.w = this.p = 0
+      this.I = this.J = this.Imax = 1
+      return
+    }
+    const sumOfDominants = rgb[b] + rgb[c] // sum of the two channels surrounding the color
+    switch (a) { // a is the channel with the lowest value
+      case 0 : // the dominant channels are G and B
+        this.w = (1 + rgb[2] / sumOfDominants) / 3
+        break
+      case 1 : // the dominant channels are R and B
+        this.w = (2 + rgb[0] / sumOfDominants) / 3
+        break
+      case 2 : // the dominant channels are R and G
+        this.w = (0 + rgb[1] / sumOfDominants) / 3
+        break
+    }
+    this.p = 1 - (2 * rgb[a]) / sumOfDominants
+    this.J = rgb[c] // due to our definitions of w and p, this value turns out to be the ratio between I (no matter how I is calculated) and the maximum I that could be reached with w and p constant
+    const [x, y, z] = order([Eye.channelIcontribution[0] * rgb[0], Eye.channelIcontribution[1] * rgb[1], Eye.channelIcontribution[2] * rgb[2]])
+    this.I = Eye.channelIcontribution[y] * rgb[y] + Eye.channelIcontribution[z] * rgb[z]
+    this.Imax = Eye.channelIcontribution[y] * rgb[y] / rgb[z] + Eye.channelIcontribution[z]
+  }
+
+  exportToRGB1 () : RGB {
+    if (spi.s <= 1 / 3) {
+      // the dominant channels are R and G
+      const factor = 1 / 
+      const G = 
+      return [
+        ,
+        ,
+      ]
+    } else
+      if (spi.s <= 2 / 3) {
+        // the dominant channels are G and B
+        const factor = 1 / 
+        const G = 
+        return [
+          ,
+          ,
+        ]
+      } else {
+        // the dominant channels are R and B
+        const factor = 1 / 
+        const G = 
+        return [
+          ,
+          ,
+        ]
+      }
+  }
+
+  constructor () {
+    this.w = this.p = this.I = this.J = this.Imax = 0
+  }
+}
+
+function order (p : number[]) : Permutation {
   let a = 0 as Permutation[number]
   let b = 1 as Permutation[number]
   const c = 2 as Permutation[number]
@@ -96,7 +149,7 @@ function order (p : Point) : Permutation {
   return [a, b, c]
 }
 
-function limit (p : Point, min: number, max: number) {
+function limit (p : number[], min: number, max: number) {
   for (let i = 0; i < 3; i++) {
     if (p[i] < min) { p[i] = min }
     if (p[i] > max) { p[i] = max }
