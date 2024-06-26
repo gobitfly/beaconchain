ef251c707da67a2c2ed8fbe620061034072fa207
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2024-06-26 16:09:40 +0200

    Bids 3075/validator subset modal v2 5 (#520)
    * summary v2.5 validator modal adaptations
    
    ---------
    
    Co-authored-by: LUCCA DUKIC <109136188+LuccaBitfly@users.noreply.github.com>

--- frontend/types/validator/index.ts
+++ frontend/types/validator/index.ts
@@ -1,3 +1,10 @@
-import type { VDBManageValidatorsTableRow } from '../api/validator_dashboard'
+import type { VDBManageValidatorsTableRow, VDBSummaryValidator, VDBSummaryValidatorsData } from '../api/validator_dashboard'
 
 export type ValidatorStatus = VDBManageValidatorsTableRow['status']
+
+export type ValidatorSubsetCategory = VDBSummaryValidatorsData['category'] | 'all'
+
+export type ValidatorSubset = {
+  category: ValidatorSubsetCategory,
+  validators: VDBSummaryValidator[]
+}
