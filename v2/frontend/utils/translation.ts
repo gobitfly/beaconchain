3eb4cf439ea8e7f82f922b645830c9b023a36f1c
thib-wien <thibaut.barthelemy@bitfly.at>
2024-06-27 17:49:26 +0200

    (BIDS-2859) translations, mini parser and rows implemented

--- v2/frontend/utils/translation.ts
+++ v2/frontend/utils/translation.ts
@@ -13,23 +13,32 @@ export function formatMultiPartSpan (t: ComposerTranslation, key: string, classe
   return `<span>${parts.join(' ')}</span>`
 }
 
-// Translation with default. Needed if we want to default to '' as the translation lib does not support to default to ''
+/** Translation with default. Needed if we want to default to '' as the translation lib does not support to default to '' */
 export function tD (t: ComposerTranslation, path : string, options?: any, d:string = '') : string {
+  if (typeof options === 'number') {
+    options = { plural: options }
+  }
   const translation = t(path, 'NOT_FOUND', { ...options, missingWarn: false })
   return (translation === 'NOT_FOUND') ? d : translation
 }
 
-// get a translation at a specific index, if it exists
+/** gets a translation at a specific index, if it exists */
 export function tOf (t: ComposerTranslation, path : string, index : number, options?: any) : string {
   return tD(t, `${path}[${index}]`, options)
 }
 
-// get all translations for an array
+/** returns an array of translations (with only one element if the translation data is a simple string) */
 export function tAll (t: ComposerTranslation, path : string, options?: any) : string[] {
   const list: string[] = []
-  let index = 0
-  for (let value = tOf(t, path, index); value; value = tOf(t, path, ++index, options)) {
-    list.push(value)
+  if (tD(t, path, options, 'NOT_FOUND') !== 'NOT_FOUND') {
+    // the data is a string, we push the translation
+    list.push(tD(t, path, options))
+  } else {
+    // the data is an array
+    let index = 0
+    for (let value = tOf(t, path, index, options); value; value = tOf(t, path, ++index, options)) {
+      list.push(value)
+    }
   }
   return list
 }
