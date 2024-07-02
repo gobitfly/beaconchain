8e5c3b7e09a5936b4b71054eb5563cebfc6ec2ee
thib-wien <thibaut.barthelemy@bitfly.at>
2024-07-02 16:17:02 +0200

    (BIDS-2859) stoppers fixed

--- frontend/composables/useRefPipe.ts
+++ frontend/composables/useRefPipe.ts
@@ -3,66 +3,64 @@ import { type WatchStopHandle } from 'vue'
 export interface ConverterCallBack<Tx, Ty> { (x: Tx) : Ty}
 
 export function useRefPipe () {
-  let stopBack: WatchStopHandle
-  let stopForth: WatchStopHandle
+  const stoppers: WatchStopHandle[] = []
 
   function bindPrimitiveRefs<T> (refA: Ref<T>, refB: Ref<T>) {
-    stopBack = watch(refA, (a: T) => {
+    stoppers.push(watch(refA, (a: T) => {
       if (a !== refB.value) {
         refB.value = a
       }
-    }, { immediate: true })
-    stopForth = watch(refB, (b: T) => {
+    }, { immediate: true }))
+    stoppers.push(watch(refB, (b: T) => {
       if (b !== refA.value) {
         refA.value = b
       }
-    }, { immediate: true })
+    }, { immediate: true }))
   }
 
   function bindPrimitiveRefsOfDifferentTypes<Ta, Tb> (refA: Ref<Ta>, refB: Ref<Tb>) {
-    stopBack = watch(refA, (a: Ta) => {
+    stoppers.push(watch(refA, (a: Ta) => {
       if (JSON.stringify(a) !== JSON.stringify(refB.value)) {
         refB.value = <Tb>(a as unknown)
       }
-    }, { immediate: true })
-    stopForth = watch(refB, (b: Tb) => {
+    }, { immediate: true }))
+    stoppers.push(watch(refB, (b: Tb) => {
       if (JSON.stringify(b) !== JSON.stringify(refA.value)) {
         refA.value = <Ta>(b as unknown)
       }
-    }, { immediate: true })
+    }, { immediate: true }))
   }
 
   function bindObjectRefs<T> (refA: Ref<T>, refB: Ref<T>) {
-    stopBack = watch(refA, (a: T) => {
+    stoppers.push(watch(refA, (a: T) => {
       if (JSON.stringify(a) !== JSON.stringify(refB.value)) {
         refB.value = a
       }
-    }, { immediate: true })
-    stopForth = watch(refB, (b: T) => {
+    }, { immediate: true }))
+    stoppers.push(watch(refB, (b: T) => {
       if (JSON.stringify(b) !== JSON.stringify(refA.value)) {
         refA.value = b
       }
-    }, { immediate: true })
+    }, { immediate: true }))
   }
 
   function bindArraysRefsOfDifferentTypes<Ta, Tb> (refA: Ref<Ta[]>, refB: Ref<Tb[]>, AtoB: ConverterCallBack<Ta, Tb>, BtoA: ConverterCallBack<Tb, Ta>) {
-    stopBack = watch(refA, (a: Ta[]) => {
+    stoppers.push(watch(refA, (a: Ta[]) => {
       const AasB = a.map(el => AtoB(el))
       if (JSON.stringify(AasB) !== JSON.stringify(refB.value)) {
         refB.value = AasB
       }
-    }, { immediate: true })
-    stopForth = watch(refB, (b: Tb[]) => {
+    }, { immediate: true }))
+    stoppers.push(watch(refB, (b: Tb[]) => {
       const BasA = b.map(el => BtoA(el))
       if (JSON.stringify(BasA) !== JSON.stringify(refA.value)) {
         refA.value = BasA
       }
-    }, { immediate: true })
+    }, { immediate: true }))
   }
 
   onUnmounted(() => {
-    stopBack?.()
-    stopForth?.()
+    stoppers.forEach(stopper => stopper())
   })
 
   return { bindPrimitiveRefs, bindObjectRefs, bindPrimitiveRefsOfDifferentTypes, bindArraysRefsOfDifferentTypes }
