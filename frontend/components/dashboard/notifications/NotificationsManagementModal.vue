009e26f9899ebb35272e5fb3d40ed76bae0aea7c
D13ce <stefan.benker@bitfly.at>
2024-07-02 15:09:11 +0200

    Add draft for manage notifications modal

---
+++ frontend/components/dashboard/notifications/NotificationsManagementModal.vue
@@ -0,0 +1,87 @@
+<script lang="ts" setup>
+import {
+  faCog,
+  faGaugeSimpleMax,
+  faMonitorWaveform,
+  faBolt,
+  faNetworkWired
+} from '@fortawesome/pro-solid-svg-icons'
+
+const { t: $t } = useI18n()
+
+const visible = defineModel<boolean>()
+</script>
+
+<template>
+  <BcDialog
+    v-model="visible"
+    :header="$t('dashboard.notifications.management.title')"
+    class="notifications-management-modal-container"
+  >
+    <TabView lazy class="notifications-management-tab-view">
+      <TabPanel>
+        <template #header>
+          <BcTabHeader :header="$t('dashboard.notifications.management.tabs.general')" :icon="faCog" />
+        </template>
+        General coming soon!
+      </TabPanel>
+      <TabPanel>
+        <template #header>
+          <BcTabHeader :header="$t('dashboard.notifications.management.tabs.dashboards')" :icon="faGaugeSimpleMax" />
+        </template>
+        Dashboards coming soon!
+      </TabPanel>
+      <TabPanel>
+        <template #header>
+          <BcTabHeader :header="$t('dashboard.notifications.management.tabs.machines')" :icon="faMonitorWaveform" />
+        </template>
+        Machines coming soon!
+      </TabPanel>
+      <TabPanel>
+        <template #header>
+          <BcTabHeader :header="$t('dashboard.notifications.management.tabs.clients')" :icon="faBolt" />
+        </template>
+        Clients coming soon!
+      </TabPanel>
+      <TabPanel>
+        <template #header>
+          <BcTabHeader :header="$t('dashboard.notifications.management.tabs.rocketpool')">
+            <template #icon>
+              <IconRocketPool />
+            </template>
+          </BcTabHeader>
+        </template>
+        Rocket Pool coming soon!
+      </TabPanel>
+      <TabPanel>
+        <template #header>
+          <BcTabHeader :header="$t('dashboard.notifications.management.tabs.network')" :icon="faNetworkWired" />
+        </template>
+        Network coming soon!
+      </TabPanel>
+    </TabView>
+    <Button class="done-button" label="Done" />
+  </BcDialog>
+</template>
+
+<style lang="scss" scoped>
+:global(.notifications-management-modal-container) {
+  width: 1400px;
+  height: 786px;
+}
+
+.done-button{
+  position: absolute;
+  bottom: calc(var(--padding-large) + var(--padding));
+  right: calc(var(--padding-large) + var(--padding));
+}
+
+:global(.notifications-management-tab-view >.p-tabview-panels) {
+  min-height: 652px;
+}
+
+.p-tabview {
+  margin-top: var(--padding-large);
+}
+
+</style>
