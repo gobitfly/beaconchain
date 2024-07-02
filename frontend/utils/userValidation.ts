2df6e0de0f904ab43e3d2bb6065d6105c3d5cda4
D13ce <stefan.benker@bitfly.at>
2024-07-02 10:48:30 +0200

    Improve and harmonize user settings validation

--- frontend/utils/userValidation.ts
+++ frontend/utils/userValidation.ts
@@ -6,30 +6,35 @@ export function setTranslator (translator: ComposerTranslation) {
   t = translator
 }
 
-export function validateAddress (value: string) : true|string {
-  if (!value) {
-    return t('login_and_register.no_email')
+export function validateEmailAddress (address: string, compareAddress?: string) : true | string {
+  if (!address) {
+    return t('validation.no_email')
   }
-  if (value.length > 100 || !REGEXP_VALID_EMAIL.test(value)) {
-    return t('login_and_register.invalid_email')
+  if (address.length > 100 || !REGEXP_VALID_EMAIL.test(address)) {
+    return t('validation.invalid_email')
   }
-  return true
-}
-
-export function validatePassword (value: string) : true|string {
-  if (!value) {
-    return t('login_and_register.no_password')
-  }
-  // TODO: ask for a complex password with special characters and son on?
-  if (value.length < 5 || value.length > 256) {
-    return t('login_and_register.invalid_password')
+  if (compareAddress && address !== compareAddress) {
+    return t('validation.emails_dont_match')
   }
   return true
 }
 
-export function validateAgreement (value : boolean) : true|string {
-  if (!value) {
-    return t('login_and_register.not_agreed')
+export function validatePassword (password: string, comparePassword?: string) : true | string {
+  if (!password) {
+    return t('validation.no_password')
+  }
+  if (password.length < 5 || password.length > 256) {
+    return t('validation.invalid_password')
+  }
+  if (comparePassword && password !== comparePassword) {
+    return t('validation.passwords_dont_match')
+  }
+  return true
+}
+
+export function validateAgreement (agreement : boolean) : true | string {
+  if (!agreement) {
+    return t('validation.not_agreed')
   }
   return true
 }
