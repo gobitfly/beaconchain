eabb58d50f5720f3232f2b95fa5773e8bfc4f777
thib-wien <thibaut.barthelemy@bitfly.at>
2024-07-08 12:25:00 +0200

    (BIDS-2859) now we can change easily the identifier of each setting for the API

--- frontend/types/subscriptionModal.ts
+++ frontend/types/subscriptionModal.ts
@@ -1,5 +1,48 @@
 import { ChainIDs } from './network'
 
+/** translates our the names of our object members to/from the names used by the API  // TODO: write here the identifiers actually used by the API */
+export enum SubscriptionJSONfields {
+  offlineValidator = 'offline_validator',
+  offline_validator = 'offlineValidator',
+
+  offlineGroup = 'offline_group',
+  offline_group = 'offlineGroup',
+
+  missedAttestations = 'missed_attestations',
+  missed_attestations = 'missedAttestations',
+
+  proposedBlock = 'proposed_block',
+  proposed_block = 'proposedBlock',
+
+  upcomingProposal = 'upcoming_proposal',
+  upcoming_proposal = 'upcomingProposal',
+
+  syncCommittee = 'sync_committee',
+  sync_committee = 'syncCommittee',
+
+  withdrawn = 'withdrawn',
+
+  slashed = 'slashed',
+
+  realTime = 'real_time',
+  real_time = 'realTime',
+
+  incoming = 'incoming',
+
+  outgoing = 'outgoing',
+
+  erc20 = 'erc20',
+
+  erc721 = 'erc721',
+
+  erc1155 = 'erc1155',
+
+  networks = 'networks',
+
+  ignoreSpam = 'ignore_spam',
+  ignore_spam = 'ignoreSpam'
+}
+
 /** for an option of type `number`: to mean that no value exists in the DB yet, set it to NaN; to mean that it is has a value but it is deactivated, set it to a negative value (for example 10% becomes -10) */
 export type ValidatorSubscriptionState = {
   offlineValidator: boolean,
@@ -22,26 +65,6 @@ export type AccountSubscriptionState = {
   erc1155: boolean,
   networks: ChainIDs[],
   ignoreSpam: boolean
-}
-
-/** for the communication with the API  // TODO: write here the identifiers actually used by the API */
-export const SubscriptionJSONfields: Record<keyof (ValidatorSubscriptionState&AccountSubscriptionState), string> = {
-  offlineValidator: 'offline_validator',
-  offlineGroup: 'offline_group',
-  missedAttestations: 'missed_attestations',
-  proposedBlock: 'proposed_block',
-  upcomingProposal: 'upcoming_proposal',
-  syncCommittee: 'sync_committee',
-  withdrawn: 'withdrawn',
-  slashed: 'slashed',
-  realTime: 'real_time',
-  incoming: 'incoming',
-  outgoing: 'outgoing',
-  erc20: 'erc20',
-  erc721: 'erc721',
-  erc1155: 'erc1155',
-  networks: 'networks',
-  ignoreSpam: 'ignore_spam'
 }
 
 /** for internal use */
