5ce9bc830880d86ce0d690ad1675b17e093c56ab
D13ce <stefan.benker@bitfly.at>
2024-07-09 15:00:18 +0200

    Add paired devices information to general tab

--- v2/frontend/types/notifications/general.ts
+++ v2/frontend/types/notifications/general.ts
@@ -4,9 +4,10 @@ import type { ApiDataResponse } from '~/types/api/common'
 // TODO: replace with real api types, once ready
 export interface NotificationsManagementGeneral {
   enabled_notifications: {
-    general_do_not_disturb_expire_ts: number /* int64 */
+    general_do_not_disturb_expire_ts: number
     general_email: boolean
     general_push: boolean
+    paired_devices_count: number
   }
 }
 
