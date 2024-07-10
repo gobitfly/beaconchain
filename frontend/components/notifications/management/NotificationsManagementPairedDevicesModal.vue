3743f2ddcf24d7a7b5b408706129af61748004b8
D13ce <stefan.benker@bitfly.at>
2024-07-10 08:39:08 +0200

    Implement draft for paired devices modal

---
+++ frontend/components/notifications/management/NotificationsManagementPairedDevicesModal.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+// TODO: Styling
+// TODO: Logic
+const visible = defineModel<boolean>()
+
+</script>
+
+<template>
+  <BcDialog
+    v-model="visible"
+    header="Paired Devices"
+    class="paired-devices-modal-container"
+  >
+    <Button class="done-button" :label="$t('navigation.done')" @click="visible = false" />
+  </BcDialog>
+</template>
+
+<style lang="scss" scoped>
+:global(.paired-devices-modal-container) {
+  max-width: 790px;
+}
+
+.done-button {
+  position: absolute;
+  bottom: calc(var(--padding-large) + var(--padding));
+  right: calc(var(--padding-large) + var(--padding));
+}
+</style>
