943f3806c4676c4d51a69251cdb72244e3b056bd
benji-bitfly <beniamin.hutyra@bitfly.at>
2024-07-15 15:11:41 +0200

    BIDS-3218: Assign the ssrSecret

--- frontend/composables/useCustomFetch.ts
+++ frontend/composables/useCustomFetch.ts
@@ -35,9 +35,10 @@ export function useCustomFetch () {
     const url = useRequestURL()
     const runtimeConfig = useRuntimeConfig()
     const showInDevelopment = Boolean(runtimeConfig.showInDevelopment)
-    const { public: { apiClient, legacyApiClient, apiKey, domain, logIp }, ssrSecret } = runtimeConfig
+    const { public: { apiClient, legacyApiClient, apiKey, domain, logIp }, private: pConfig } = runtimeConfig
     const path = map.mock ? `${pathName}.json` : map.getPath?.(pathValues) || map.path
     let baseURL = map.mock ? '../mock' : map.legacy ? legacyApiClient : apiClient
+    const ssrSecret = pConfig?.ssrSecret
 
     if (process.server) {
       baseURL = map.mock ? `${domain || url.origin.replace('http:', 'https:')}/mock` : map.legacy ? pConfig?.legacyApiServer : pConfig?.apiServer
