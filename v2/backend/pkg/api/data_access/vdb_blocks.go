9d5385ad0143f5a14056b87d6bafce07464fbb49
remoterami <ramirond@beaconcha.in>
2024-07-15 10:31:40 +0200

    adjusted to comments

--- v2/backend/pkg/api/data_access/vdb_blocks.go
+++ v2/backend/pkg/api/data_access/vdb_blocks.go
@@ -19,7 +19,7 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-func (d *DataAccessService) GetValidatorDashboardBlocks(ctx context.Context, dashboardId t.VDBId, cursor string, colSort t.Sort[enums.VDBBlocksColumn], search string, limit uint64, protocolModes []enums.ProtocolMode) ([]t.VDBBlocksTableRow, *t.Paging, error) {
+func (d *DataAccessService) GetValidatorDashboardBlocks(ctx context.Context, dashboardId t.VDBId, cursor string, colSort t.Sort[enums.VDBBlocksColumn], search string, limit uint64, protocolModes t.VDBProtocolModes) ([]t.VDBBlocksTableRow, *t.Paging, error) {
 	// @DATA-ACCESS incorporate protocolModes
 	var err error
 	var currentCursor t.BlocksCursor
