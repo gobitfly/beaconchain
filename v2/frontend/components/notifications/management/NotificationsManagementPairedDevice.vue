458d6cc81f38b4dec39c98345c4ae8cc1716ecd1
D13ce <stefan.benker@bitfly.at>
2024-07-16 12:58:01 +0200

    Add NotificationsManagementPairedDevice

---
+++ v2/frontend/components/notifications/management/NotificationsManagementPairedDevice.vue
@@ -0,0 +1,79 @@
+<script lang="ts" setup>
+// TODO: Translations
+// TODO: Get linked app "Beaconchain Dashboard" from NotificationSettingsPairedDevice
+// TODO: Get paired time stamp from NotificationSettingsPairedDevice
+import { faTrash } from '@fortawesome/pro-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { type NotificationSettingsPairedDevice } from '~/types/notifications/settings'
+
+interface Props {
+  device: NotificationSettingsPairedDevice
+}
+const props = defineProps<Props>()
+
+const notificationsToggle = ref(false)
+const accountAccessToggle = ref(false)
+
+</script>
+
+<template>
+  <div class="row-container">
+    <div class="device-row ">
+      <div class="device">
+        Device: {{ props.device.name || 'Unknown' }}
+      </div>
+      <Button secondary class="p-button-icon-only">
+        <FontAwesomeIcon :icon="faTrash" />
+      </Button>
+    </div>
+    <div class="linked-app-row">
+      Linked App: Beaconchain Dashboard
+    </div>
+    <div class="toggle-row">
+      <BcToggle v-model="notificationsToggle" />
+      Mobile Notifications
+    </div>
+    <div class="toggle-row">
+      <BcToggle v-model="accountAccessToggle" />
+      Grant account access (required)
+    </div>
+    <div class="paired-row">
+      Paired: 27 minutes ago
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '~/assets/css/fonts.scss';
+
+.row-container {
+  display: flex;
+  flex-direction: column;
+  gap: var(--padding);
+
+  .device-row {
+    @include fonts.subtitle_text;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .linked-app-row {
+    @include fonts.small_text;
+    margin-bottom: var(--padding);
+  }
+
+  .toggle-row {
+    display: flex;
+    align-items: center;
+    gap: var(--padding-large);
+  }
+
+  .paired-row {
+    @include fonts.small_text;
+    color: var(--text-color-discreet);
+    margin-top: var(--padding-small);
+  }
+}
+</style>
--- v2/frontend/components/notifications/management/NotificationsManagementPairedDevicesModal.vue
+++ v2/frontend/components/notifications/management/NotificationsManagementPairedDevicesModal.vue
@@ -1,28 +1,44 @@
 <script lang="ts" setup>
-// TODO: Styling
-// TODO: Logic
+import { useNotificationsManagementSettings } from '~/composables/notifications/useNotificationsManagementSettings'
+
+const { t: $t } = useI18n()
+
 const visible = defineModel<boolean>()
+const { generalSettings } = useNotificationsManagementSettings()
 
 </script>
 
 <template>
   <BcDialog
     v-model="visible"
-    header="Paired Devices"
     class="paired-devices-modal-container"
   >
-    <Button class="done-button" :label="$t('navigation.done')" @click="visible = false" />
+    <div class="container">
+      <h1>{{ $t('notifications.general.paired_devices.title') }}</h1>
+      <NotificationsManagementPairedDevice v-for="device in generalSettings?.paired_devices" :key="device.id" :device="device" />
+    </div>
+    <div class="button-row">
+      <Button :label="$t('navigation.done')" @click="visible = false" />
+    </div>
   </BcDialog>
 </template>
 
 <style lang="scss" scoped>
 :global(.paired-devices-modal-container) {
-  max-width: 790px;
+  width: 790px;
 }
 
-.done-button {
-  position: absolute;
-  bottom: calc(var(--padding-large) + var(--padding));
-  right: calc(var(--padding-large) + var(--padding));
+.container {
+  padding: var(--padding-large);
+
+  h1 {
+    margin-top: 0;
+  }
+}
+
+.button-row {
+  margin-top: var(--padding);
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
