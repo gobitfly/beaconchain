f3a81c5414d6e0b28af121119e96205e62b8681c
thib-wien <thibaut.barthelemy@bitfly.at>
2024-07-25 12:56:49 +0200

    (BIDS-3244) work in progress

--- frontend/components/bc/MiniParser.vue
+++ frontend/components/bc/MiniParser.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
-/** Component outputting HTML by interpreting simple tags found in the input.
+/** Component outputting HTML by interpreting simple markdown-like tags found in the input.
  *
  *  Usage:
  *
  *    Simplest case:
  *      <BcMiniParser :input="stringOrArrayOfStrings" />
- *    You can pass an object containing links:
- *      <BcMiniParser :input="stringOrArrayOfStrings" :links="{darklink: 'www.choco.com', milkyurl: 'index.html'}" />
- *    (see the example just below to understand the purpose of prop links)
- *    You can also pass an object containing numbers:
- *      <BcMiniParser :input="stringOrArrayOfStrings" :numbers="{easterDuration: 50}" />
- *    (see the example just below to understand the purpose of :links and :numbers)
+ *    You can pass an object containing variables whose values will be inserted:
+ *      <BcMiniParser :input="stringOrArrayOfStrings" :variables="{darkLink: 'www.choco.com', easterDuration: 50}" />
+ *    (see the example just below to see how to use the variables)
  *
  *  Example of input:
  *
@@ -18,8 +15,8 @@
  *    We will eat `chocolate` in 2 cases:
  *    - If your *validator* is _online_
  *    - During the $easterDuration days of Easter\*.
- *    It can be [very dark](darklink) or [with _milk_](milkyurl), we enjoy both.
- *    \*note that Christmas is also a good moment to do so
+ *    It can be [very dark]($darkLink) or with milk, we enjoy both.
+ *    \*note that Christmas is also _a good moment_ to do so
  *
  *  Language:
  *
@@ -30,8 +27,8 @@
  *      parts surrounded with _ will be surrounded with <i> tags
  *      parts surrounded with * will be surrounded with <b> tags
  *      parts surrounded with ` will be shown with a type-writter font and not parsed (formatting tags between ` and ` are ineffective)
- *      a link can be created by writing [a caption](and-a-url). The url can written directly or tell the name of a member of the object in props :links.
- *      numbers in the object passed in prop :numbers can be inserted by writing $objectKey. They will be formatted according to the local settings of the user.
+ *      a link can be created by writing [a caption](and-a-url). The url can written directly or come from an entry in the object in prop :variables.
+ *    When numbers are inserted from prop :variables, they are formatted according to the local settings of the user.
  *    Mixes are possible: italic inside bold or bold inside italic, code in italic or bold (if you surround ` and ` with the tags)...
  *
  *    If you need to display a character that is a tag, escape it with `\` and the parser will not interpret it.
@@ -59,12 +56,13 @@ enum Tag { H1 = '#', H2 = '##', H3 = '###', Item = '-', Italic = '_', Bold = '*'
 const OpeningTags = [Tag.Italic, Tag.Bold, Tag.Code, Tag.LinkStart]
 const ClosingTags: Record<string, Tag> = { [Tag.Italic]: Tag.Italic, [Tag.Bold]: Tag.Bold, [Tag.Code]: Tag.Code, [Tag.LinkStart]: Tag.LinkEnd }
 
-const props = defineProps<{ input: string|string[], links?: Record<string, string>, numbers?: Record<string, number> }>()
+const props = defineProps<{ input: string|string[], variables?: Record<string, string> }>()
 
 type VDOMnodes = Array<VNode|string>
 enum LineType { Useless, Blank, Title, List, Div }
 const ESC = '\u001B'
 const inputArray = { lines: [] as string[], pos: 0 as number }
+const variableNamesSortedByLength = !props.variables ? [] : Object.keys(props.variables).sort((a, b) => b.length - a.length)
 
 function parse (props: {input: string|string[]}) : VDOMnodes {
   if (!Array.isArray(props.input)) {
@@ -118,34 +116,34 @@ function parseText (text: string) : VDOMnodes {
     const { pos: openingTagPos, tag: openingTag } = findTag(text, 0)
     const { pos: closingTagPos } = findTag(text, openingTagPos + openingTag.length, ClosingTags[openingTag])
     if (openingTagPos < 0 || closingTagPos < 0) { // First case: no tag, we can copy the raw line. Second case: syntax error (either the closing tag has been forgotten or nested tags have their closure swapped)
-      output.push(...cleanTextAndInsertVariables(text)) // in both cases we output the text without parsing it
+      output.push(cleanText(text)) // in both cases we output the text without parsing it
       break
     }
     if (openingTagPos > 0) {
-      output.push(...cleanTextAndInsertVariables(text.slice(0, openingTagPos)))
+      output.push(cleanText(text.slice(0, openingTagPos)))
     }
     const middle = text.slice(openingTagPos + openingTag.length, closingTagPos)
     switch (openingTag) {
       case Tag.Italic : output.push(h('i', {}, parseText(middle))); break
       case Tag.Bold : output.push(h('b', {}, parseText(middle))); break
-      case Tag.Code : output.push(h('span', { style: 'font-family: monospace;' }, cleanTextAndInsertVariables(middle, true))); break
-      case Tag.LinkStart : output.push(...parseLink(middle)); break
+      case Tag.Code : output.push(h('span', { style: 'font-family: monospace;' }, cleanText(middle, true))); break
+      case Tag.LinkStart : output.push(parseLink(middle)); break
     }
     text = text.slice(closingTagPos + ClosingTags[openingTag].length)
   } while (text)
   return output
 }
 
-function parseLink (text: string) : VDOMnodes {
+function parseLink (text: string) : VNode|string {
   // note: param `text` is of the form  `caption of the link](urlRef`  (both ends have been removed by the calling function)
   const { pos: middleTagPos } = findTag(text, 0, Tag.LinkMid)
   if (middleTagPos <= 0) { // syntax error (the link has no caption) : we output the text without parsing it
-    return cleanTextAndInsertVariables(text)
+    return cleanText(text)
   }
   const urlRef = text.slice(middleTagPos + Tag.LinkMid.length)
-  const to = (props.links && urlRef in props.links) ? props.links[urlRef] : urlRef
+  const to = urlRef.startsWith(Tag.Variable) ? getVariableValue(urlRef) : urlRef
   const target = (to.includes('://') || to.startsWith('www.')) ? Target.External : Target.Internal // correct 99% of the time I suppose
-  return [h(BcLink, { to, target, class: 'link' }, () => parseText(text.slice(0, middleTagPos)))]
+  return h(BcLink, { to, target, class: 'link' }, () => parseText(text.slice(0, middleTagPos)))
 }
 
 /** @param wanted If given, finds the first valid occurence of this tag (this mode is used for closing tags). If omitted, finds the first opening tag that the parser recognises.
@@ -190,21 +188,20 @@ function removeLeadingSpacesAndReplaceEscapements (input: string) : string {
   return (output + input.slice(posIn)).trimStart()
 }
 
-function cleanTextAndInsertVariables (raw: string, showAsRaw = false) : VDOMnodes {
-  const output: VDOMnodes = []
-  if (showAsRaw) {
+function cleanText (raw: string, forceSpaces = false) : string {
+  if (forceSpaces) {
     raw = raw.replaceAll(' ', '\xA0')
-  } else {
-    for (const key in props.numbers) {
-      const sought = Tag.Variable + key
-      let pos = -sought.length
-      do {
-        pos = raw.indexOf(sought, pos + sought.length)
-        if (pos >= 1 && raw[pos - 1] === ESC) { continue }
-      } while (pos > 0)
-    }
   }
-  return output.map(part => (typeof part === 'string') ? part.replaceAll(ESC, '') : part)
+  return raw.replaceAll(ESC, '')
+}
+
+function getVariableValue (name: string) : any {
+  if (!props.variables) { return name }
+  const key = name.startsWith(Tag.Variable) ? name.slice(Tag.Variable.length) : name
+  for (const v of variableNamesSortedByLength) {
+    if (key.startsWith(v)) { return props.variables[v] }
+  }
+  return name
 }
 
 function getTitleType (pos: number) : { height: number, tag: Tag } | undefined {
