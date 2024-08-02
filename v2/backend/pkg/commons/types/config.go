2dad16389c1ea2243dd0a224beac21b191382667
Patrick <guybrush@users.noreply.github.com>
2024-08-02 13:53:01 +0200

    (NOBIDS) add config-switch to enable ratelimit (#662)

--- v2/backend/pkg/commons/types/config.go
+++ v2/backend/pkg/commons/types/config.go
@@ -212,6 +212,7 @@ type Config struct {
 			DiamondMonth  int `yaml:"diamondMonth" envconfig:"FRONTEND_RATELIMITS_DIAMOND_MONTH"`
 		} `yaml:"ratelimits"`
 		RatelimitUpdateInterval time.Duration `yaml:"ratelimitUpdateInterval" envconfig:"FRONTEND_RATELIMIT_UPDATE_INTERVAL"`
+		RatelimitEnabled        bool          `yaml:"ratelimitEnabled" envconfig:"FRONTEND_RATELIMIT_ENABLED"`
 		SessionSecret           string        `yaml:"sessionSecret" envconfig:"FRONTEND_SESSION_SECRET"`
 		SessionSameSiteNone     bool          `yaml:"sessionSameSiteNone" envconfig:"FRONTEND_SESSION_SAMESITE_NONE"`
 		SessionCookieDomain     string        `yaml:"sessionCookieDomain" envconfig:"FRONTEND_SESSION_COOKIE_DOMAIN"`
