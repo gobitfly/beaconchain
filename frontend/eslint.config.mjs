4d320879155288194bea5673570d35b6034ffbc4
MauserBitfly <125363940+MauserBitfly@users.noreply.github.com>
2024-08-09 12:56:17 +0200

    style(eslint): disable `vue/max-attributes-per-line`
    See: BIDS-128

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -59,6 +59,7 @@ export default withNuxt({
       'error',
       { allowAllPropertiesOnSameLine: true },
     ],
+    'vue/max-attributes-per-line': 'off',
     'vue/max-len': [
       'error',
       {
