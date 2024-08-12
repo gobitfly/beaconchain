bc5d0c61faffe6327563899a1dadfdfcfc92a4d0
Stefan Pletka <stefan.pletka@bitfly.at>
2024-08-12 11:44:31 +0200

    Updated migration file

--- backend/pkg/commons/db/migrations/postgres/TBD_is_archived.sql
+++ backend/pkg/commons/db/migrations/postgres/TBD_is_archived.sql
@@ -1,13 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
-
--- TODO
-
+SELECT 'up SQL query - add is_archived column';
+ALTER TABLE users_val_dashboards ADD COLUMN IF NOT EXISTS is_archived TEXT;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-
--- TODO
-
+SELECT 'down SQL query - drop is_archived column';
+ALTER TABLE users_val_dashboards DROP COLUMN IF EXISTS is_archived;
 -- +goose StatementEnd
