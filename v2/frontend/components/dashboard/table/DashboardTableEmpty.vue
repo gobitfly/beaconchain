9901dce82e963cc9c0d7f93165556004e811e8da
benji-bitfly <beniamin.hutyra@bitfly.at>
2024-08-16 15:43:36 +0200

    feat: Add new copmonent to display message whenever the table will display empty value. BEDS-162

---
+++ v2/frontend/components/dashboard/table/DashboardTableEmpty.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="empty delayed-fadein-animation">
+      <span class="big_text">{{ $t('dashboard.empty.empty_table') }}</span>
+    </div>
+  </template>
+  
+  <style lang="scss" scoped>
+  .empty {
+    width: 100%;
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: var(--text-color-disabled);
+  }
+  </style>
