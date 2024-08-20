06be8c281abace7e9f1d562ab28d54c07f8df008
peter <1674920+peterbitfly@users.noreply.github.com>
2024-08-20 15:51:01 +0200

    feat: update go to 1.23, update linter dependencies

--- v2/backend/cmd/notification_collector/main.go
+++ v2/backend/cmd/notification_collector/main.go
@@ -32,8 +32,8 @@ func main() {
 	flag.Parse()
 
 	if *versionFlag {
-		log.Infof(version.Version)
-		log.Infof(version.GoVersion)
+		log.Info(version.Version)
+		log.Info(version.GoVersion)
 		return
 	}
 
