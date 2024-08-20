663ae4d8d3a5ede08567730326c00c8313d9518b
MarcelBitfly <174338434+marcel-bitfly@users.noreply.github.com>
2024-08-20 16:17:09 +0200

    feat: add `balance` to `dashboardValidatorOverview`
    Also add new component for `iconButtons`
    
    See: BEDS-181

---
+++ v2/frontend/components/bc/BcButtonIcon.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  /**
+   *
+   * ♿️ screenreader text every button with just an icon should describe what it does
+   */
+  srText: string,
+}>()
+</script>
+
+<template>
+  <button class="bc-button-icon">
+    <span class="sr-only">{{ props.srText }}</span>
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+  .bc-button-icon {
+    background-color: unset;
+    border: unset;
+}
+</style>
