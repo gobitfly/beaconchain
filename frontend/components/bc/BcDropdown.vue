13c71d15e52944b10574223d454d2cb54138c7ea
Alexander Maushammer <alexander.maushammer@bitfly.at>
2024-08-22 09:49:41 +0200

    button, dialog, input, checkbox, table hover, notification network translation fix

--- frontend/components/bc/BcDropdown.vue
+++ frontend/components/bc/BcDropdown.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
+import type { SelectChangeEvent } from 'primevue/select'
+
 interface Props {
   panelClass?: string,
   variant?: 'default' | 'header' | 'table',
 }
 
 defineProps<Props>()
+
+const onChange = (event: SelectChangeEvent) => {
+  console.log('event', event)
+}
+const onValueChange = (value: any) => {
+  console.log('value change', value)
+}
 </script>
 
 <template>
   <Select
     :class="variant"
     :panel-class="[variant, panelClass]"
+    @change="onChange"
+    @update:model-value="onValueChange"
   >
     <template #dropdownicon>
       <IconChevron direction="bottom" />
@@ -25,7 +36,14 @@ defineProps<Props>()
       <slot
         name="option"
         v-bind="slotProps.option"
-      />
+      >
+        <span
+          v-if="slotProps.option.command"
+          class="p-select-option-label"
+          data-pc-section="optionlabel"
+          @click.stop.prevent="slotProps.option.command"
+        >{{ slotProps.option.label }}</span>
+      </slot>
     </template>
   </Select>
 </template>
