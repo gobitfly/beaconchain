dd3e084d52f7f08cc5596415b4e825f31b0d0e01
peter <1674920+peterbitfly@users.noreply.github.com>
2024-09-03 13:20:12 +0000

    feat(api): improve testing scaffolding

--- backend/pkg/api/handlers/handlers_test.go
+++ backend/pkg/api/handlers/handlers_test.go
@@ -9,8 +9,11 @@ import (
 	"net/http/cookiejar"
 	"net/http/httptest"
 	"os"
+	"os/exec"
 	"testing"
+	"time"
 
+	embeddedpostgres "github.com/fergusstrange/embedded-postgres"
 	"github.com/gobitfly/beaconchain/pkg/api"
 	dataaccess "github.com/gobitfly/beaconchain/pkg/api/data_access"
 	api_types "github.com/gobitfly/beaconchain/pkg/api/types"
@@ -18,11 +21,15 @@ import (
 	"github.com/gobitfly/beaconchain/pkg/commons/types"
 	"github.com/gobitfly/beaconchain/pkg/commons/utils"
 	"github.com/gobitfly/beaconchain/pkg/commons/version"
+	"github.com/jmoiron/sqlx"
+	"github.com/pressly/goose/v3"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var ts *testServer
 var dataAccessor dataaccess.DataAccessor
+var postgres *embeddedpostgres.EmbeddedPostgres
 
 type testServer struct {
 	*httptest.Server
@@ -69,17 +76,19 @@ func (ts *testServer) parseErrorResonse(t *testing.T, body string) api_types.Api
 
 func TestMain(m *testing.M) {
 	setup()
-	defer teardown()
+	code := m.Run()
+	teardown()
 
 	// wait till service initialization is completed (TODO: find a better way to do this)
 	// time.Sleep(30 * time.Second)
 
-	os.Exit(m.Run())
+	os.Exit(code)
 }
 
 func teardown() {
 	dataAccessor.Close()
 	ts.Close()
+	postgres.Stop()
 }
 
 func setup() {
@@ -87,11 +96,50 @@ func setup() {
 
 	flag.Parse()
 
+	_ = exec.Command("pkill", "-9", "postgres").Run()
+	postgres = embeddedpostgres.NewDatabase(embeddedpostgres.DefaultConfig().Username("postgres"))
+	err := postgres.Start()
+	if err != nil {
+		log.Fatal(err, "error starting embedded postgres", 0)
+	}
+
+	tempDb, err := sqlx.Connect("postgres", "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable")
+	if err != nil {
+		log.Fatal(err, "error connection to test db", 0)
+	}
+
+	if err := goose.Up(tempDb.DB, "../../../pkg/commons/db/migrations/postgres"); err != nil {
+		log.Fatal(err, "error running migrations", 0)
+	}
+
+	// insert dummy user for testing (email: admin@admin, password: admin)
+	pHash, _ := bcrypt.GenerateFromPassword([]byte("admin"), 10)
+	_, err = tempDb.Exec(`
+      INSERT INTO users (password, email, register_ts, api_key, email_confirmed)
+      VALUES ($1, $2, TO_TIMESTAMP($3), $4, $5)`,
+		string(pHash), "admin@admin.com", time.Now().Unix(), "admin", true,
+	)
+	if err != nil {
+		log.Fatal(err, "error inserting user", 0)
+	}
+
 	cfg := &types.Config{}
-	err := utils.ReadConfig(cfg, *configPath)
+	err = utils.ReadConfig(cfg, *configPath)
 	if err != nil {
 		log.Fatal(err, "error reading config file", 0)
 	}
+	cfg.Frontend.ReaderDatabase.Host = "localhost"
+	cfg.Frontend.ReaderDatabase.Port = "5432"
+	cfg.Frontend.ReaderDatabase.Name = "postgres"
+	cfg.Frontend.ReaderDatabase.Password = "postgres"
+	cfg.Frontend.ReaderDatabase.Username = "postgres"
+
+	cfg.Frontend.WriterDatabase.Host = "localhost"
+	cfg.Frontend.WriterDatabase.Port = "5432"
+	cfg.Frontend.WriterDatabase.Name = "postgres"
+	cfg.Frontend.WriterDatabase.Password = "postgres"
+	cfg.Frontend.WriterDatabase.Username = "postgres"
+
 	utils.Config = cfg
 
 	log.InfoWithFields(log.Fields{"config": *configPath, "version": version.Version, "commit": version.GitCommit, "chainName": utils.Config.Chain.ClConfig.ConfigName}, "starting")
@@ -100,8 +148,11 @@ func setup() {
 	router := api.NewApiRouter(dataAccessor, cfg)
 
 	ts = &testServer{httptest.NewTLSServer(router)}
-
-	jar, _ := cookiejar.New(nil)
+	log.Info(ts.URL)
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatal(err, "error creating cookie jar", 0)
+	}
 	ts.Server.Client().Jar = jar
 }
 
@@ -124,7 +175,7 @@ func TestInternalGetProductSummaryHandler(t *testing.T) {
 }
 
 func TestInternalGetLatestStateHandler(t *testing.T) {
-	code, _, body := ts.get(t, "//api/i/latest-state")
+	code, _, body := ts.get(t, "/api/i/latest-state")
 	assert.Equal(t, http.StatusOK, code)
 
 	respData := api_types.InternalGetLatestStateResponse{}
@@ -136,20 +187,6 @@ func TestInternalGetLatestStateHandler(t *testing.T) {
 	assert.NotEqual(t, uint64(0), respData.Data.FinalizedEpoch, "finalized epoch should not be 0")
 }
 
-func TestInternalPostAdConfigurationsHandler(t *testing.T) {
-	code, _, body := ts.get(t, "/api/i/ad-configurations")
-	assert.Equal(t, http.StatusUnauthorized, code)
-
-	resp := ts.parseErrorResonse(t, body)
-	assert.Equal(t, "unauthorized: not authenticated", resp.Error)
-
-	// login
-	code, _, body = ts.post(t, "/api/i/login", bytes.NewBuffer([]byte(`{"email": "admin@admin.com", "password": "admin"}`)))
-	assert.Equal(t, http.StatusNotFound, code)
-	resp = ts.parseErrorResonse(t, body)
-	assert.Equal(t, "not found: user not found", resp.Error)
-}
-
 func TestInternalLoginHandler(t *testing.T) {
 	// login with email in wrong format
 	code, _, body := ts.post(t, "/api/i/login", bytes.NewBuffer([]byte(`{"email": "admin", "password": "admin"}`)))
@@ -157,11 +194,33 @@ func TestInternalLoginHandler(t *testing.T) {
 	resp := ts.parseErrorResonse(t, body)
 	assert.Equal(t, "email: given value 'admin' has incorrect format", resp.Error, "unexpected error message")
 
-	// login with wrong user
-	code, _, body = ts.post(t, "/api/i/login", bytes.NewBufferString(`{"email": "admin@admin.com", "password": "admin"}`))
-	assert.Equal(t, http.StatusNotFound, code)
+	// login with correct user and wrong password
+	code, _, body = ts.post(t, "/api/i/login", bytes.NewBufferString(`{"email": "admin@admin.com", "password": "wrong"}`))
+	assert.Equal(t, http.StatusUnauthorized, code, "login should not be successful")
 	resp = ts.parseErrorResonse(t, body)
-	assert.Equal(t, "not found: user not found", resp.Error, "unexpected error message") // TODO: this should not return the same error as a user with a wrong password
+	assert.Equal(t, "unauthorized: invalid email or password", resp.Error, "unexpected error message")
+
+	// login with correct user and password
+	code, _, _ = ts.post(t, "/api/i/login", bytes.NewBufferString(`{"email": "admin@admin.com", "password": "admin"}`))
+	assert.Equal(t, http.StatusOK, code, "login should be successful")
+
+	// check if user is logged in and has a valid session
+	code, _, body = ts.get(t, "/api/i/users/me")
+	assert.Equal(t, http.StatusOK, code, "call to users/me should be successful")
+	meResponse := &api_types.InternalGetUserInfoResponse{}
+	if err := json.Unmarshal([]byte(body), meResponse); err != nil {
+		t.Fatal(err)
+	}
+	// check if email is censored
+	assert.Equal(t, meResponse.Data.Email, "a***n@a***n.com", "email should be a***n@a***n.com")
+
+	// check if logout works
+	code, _, _ = ts.post(t, "/api/i/logout", bytes.NewBufferString(``))
+	assert.Equal(t, http.StatusOK, code, "logout should be successful")
+
+	// check if user is logged out
+	code, _, _ = ts.get(t, "/api/i/users/me")
+	assert.Equal(t, http.StatusUnauthorized, code, "call to users/me should be unauthorized")
 }
 
 func TestInternalSearchHandler(t *testing.T) {
@@ -177,4 +236,30 @@ func TestInternalSearchHandler(t *testing.T) {
 	assert.NotEqual(t, 0, len(resp.Data), "response data should not be empty")
 	assert.NotNil(t, resp.Data[0].NumValue, "validator index should not be nil")
 	assert.Equal(t, uint64(5), *resp.Data[0].NumValue, "validator index should be 5")
+
+	// search for validator by pubkey
+	code, _, body = ts.post(t, "/api/i/search", bytes.NewBufferString(`{"input":"0x9699af2bad9826694a480cb523cbe545dc41db955356b3b0d4871f1cf3e4924ae4132fa8c374a0505ae2076d3d65b3e0","networks":[17000],"types":["validators_by_deposit_ens_name","validators_by_deposit_address","validators_by_withdrawal_ens_name","validators_by_withdrawal_address","validators_by_withdrawal_credential","validator_by_index","validator_by_public_key","validators_by_graffiti"]}`))
+	assert.Equal(t, 200, code)
+
+	resp = api_types.InternalPostSearchResponse{}
+	if err := json.Unmarshal([]byte(body), &resp); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotEqual(t, 0, len(resp.Data), "response data should not be empty")
+	assert.NotNil(t, resp.Data[0].NumValue, "validator index should not be nil")
+	assert.Equal(t, uint64(5), *resp.Data[0].NumValue, "validator index should be 5")
+
+	// search for validator by withdawal address
+	code, _, body = ts.post(t, "/api/i/search", bytes.NewBufferString(`{"input":"0x0e5dda855eb1de2a212cd1f62b2a3ee49d20c444","networks":[17000],"types":["validators_by_deposit_ens_name","validators_by_deposit_address","validators_by_withdrawal_ens_name","validators_by_withdrawal_address","validators_by_withdrawal_credential","validator_by_index","validator_by_public_key","validators_by_graffiti"]}`))
+	assert.Equal(t, 200, code)
+
+	resp = api_types.InternalPostSearchResponse{}
+	if err := json.Unmarshal([]byte(body), &resp); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotEqual(t, 0, len(resp.Data), "response data should not be empty")
+	assert.NotNil(t, resp.Data[0].NumValue, "validator index should not be nil")
+	assert.Greater(t, *resp.Data[0].NumValue, uint64(0), "returned number of validators should be greater than 0")
 }
