489e2d275bac0c71e57a21d55f811dac6f28137a
peter <1674920+peterbitfly@users.noreply.github.com>
2024-09-17 09:42:59 +0000

    feat(notifications): add dashboard id and group id fields

--- v2/backend/pkg/commons/types/frontend.go
+++ v2/backend/pkg/commons/types/frontend.go
@@ -14,7 +14,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/gobitfly/beaconchain/pkg/commons/log"
-	"github.com/gobitfly/beaconchain/pkg/consapi/types"
 	"github.com/lib/pq"
 	"github.com/pkg/errors"
 	"golang.org/x/text/cases"
@@ -279,16 +278,18 @@ type Notification interface {
 }
 
 type NotificationBaseImpl struct {
-	LatestState     string
-	SubscriptionID  uint64
-	EventName       EventName
-	Epoch           uint64
-	Info            string
-	Title           string
-	EventFilter     string
-	EmailAttachment *EmailAttachment
-	InfoMarkdown    string
-	UserID          UserId
+	LatestState      string
+	SubscriptionID   uint64
+	EventName        EventName
+	Epoch            uint64
+	Info             string
+	Title            string
+	EventFilter      string
+	EmailAttachment  *EmailAttachment
+	InfoMarkdown     string
+	UserID           UserId
+	DashboardId      *int64
+	DashboardGroupId *int64
 }
 
 func (n NotificationBaseImpl) GetLatestState() string {
@@ -332,39 +333,6 @@ func (n NotificationBaseImpl) GetUserId() UserId {
 }
 
 // func UnMarschal
-
-type Subscription struct {
-	ID          *uint64    `db:"id,omitempty"`
-	UserID      *UserId    `db:"user_id,omitempty"`
-	EventName   EventName  `db:"event_name"`
-	EventFilter string     `db:"event_filter"`
-	LastSent    *time.Time `db:"last_sent_ts"`
-	LastEpoch   *uint64    `db:"last_sent_epoch"`
-	// Channels        pq.StringArray `db:"channels"`
-	CreatedTime    time.Time `db:"created_ts"`
-	CreatedEpoch   uint64    `db:"created_epoch"`
-	EventThreshold float64   `db:"event_threshold"`
-	// State          sql.NullString `db:"internal_state" swaggertype:"string"`
-	GroupId     *int64
-	DashboardId *int64
-}
-
-type UserId uint64
-type DashboardId uint64
-type DashboardGroupId uint64
-type ValidatorDashboardConfig struct {
-	DashboardsByUserId map[UserId]map[DashboardId]*ValidatorDashboard
-}
-
-type ValidatorDashboard struct {
-	Name   string `db:"name"`
-	Groups map[DashboardGroupId]*ValidatorDashboardGroup
-}
-
-type ValidatorDashboardGroup struct {
-	Name       string `db:"name"`
-	Validators []types.ValidatorIndex
-}
 
 type TaggedValidators struct {
 	UserID             uint64 `db:"user_id"`
