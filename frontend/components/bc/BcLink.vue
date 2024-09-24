d7396f5333dcd843d0f63015993d1976c92e217f
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2024-09-24 15:55:43 +0200

    refactor(BcLink): make `prop to` required

--- frontend/components/bc/BcLink.vue
+++ frontend/components/bc/BcLink.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import type { RouteLocationRaw } from 'vue-router'
+import type { NuxtLinkProps } from '#app'
+
+defineProps<{
+  to: NuxtLinkProps['to'],
+}>()
+</script>
 
 <template>
-  <NuxtLink :no-prefetch="true">
+  <NuxtLink
+    :to
+    :no-prefetch="true"
+  >
     <template #default>
       <slot />
     </template>
