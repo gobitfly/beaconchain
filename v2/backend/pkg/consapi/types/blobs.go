4add973c13e60c89da68eb094ae2a302da910ff1
Patrick <guybrush@users.noreply.github.com>
2024-09-24 12:38:16 +0200

    refactor(blobindexer): allow multiple indexers running concurrently, handle pruned node (#857)
    BEDS-451

--- v2/backend/pkg/consapi/types/blobs.go
+++ v2/backend/pkg/consapi/types/blobs.go
@@ -4,13 +4,20 @@ import "github.com/ethereum/go-ethereum/common/hexutil"
 
 type StandardBlobSidecarsResponse struct {
 	Data []struct {
-		BlockRoot       hexutil.Bytes `json:"block_root"`
-		Index           uint64        `json:"index,string"`
-		Slot            uint64        `json:"slot,string"`
-		BlockParentRoot hexutil.Bytes `json:"block_parent_root"`
-		ProposerIndex   uint64        `json:"proposer_index,string"`
-		KzgCommitment   hexutil.Bytes `json:"kzg_commitment"`
-		KzgProof        hexutil.Bytes `json:"kzg_proof"`
-		Blob            hexutil.Bytes `json:"blob"`
+		Index             uint64        `json:"index,string"`
+		Blob              hexutil.Bytes `json:"blob"`
+		KzgCommitment     hexutil.Bytes `json:"kzg_commitment"`
+		KzgProof          hexutil.Bytes `json:"kzg_proof"`
+		SignedBlockHeader struct {
+			Message struct {
+				Slot          uint64        `json:"slot,string"`
+				ProposerIndex uint64        `json:"proposer_index,string"`
+				ParentRoot    hexutil.Bytes `json:"parent_root"`
+				StateRoot     hexutil.Bytes `json:"state_root"`
+				BodyRoot      hexutil.Bytes `json:"body_root"`
+			} `json:"message"`
+			Signature hexutil.Bytes `json:"signature"`
+		} `json:"signed_block_header"`
+		KzgCommitmentInclusionProof []hexutil.Bytes `json:"kzg_commitment_inclusion_proof"`
 	}
 }
