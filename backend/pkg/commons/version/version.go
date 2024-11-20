3c8d8aa4742b53fcbc87210adad362453909b45b
Patrick Pfeiffer <306324+guybrush@users.noreply.github.com>
2024-11-20 09:57:49 +0100

    fix(api): fix version-middleware

--- backend/pkg/commons/version/version.go
+++ backend/pkg/commons/version/version.go
@@ -16,7 +16,7 @@ var (
 
 func HttpMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r.Header.Set("X-Beaconchain-Version", Version)
+		w.Header().Set("X-Beaconchain-Version", Version)
 		next.ServeHTTP(w, r)
 	})
 }
