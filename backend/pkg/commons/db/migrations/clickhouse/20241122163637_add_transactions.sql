6139925d721b7c817459d55acb355e57a07fe7dc
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2024-11-28 17:23:24 +0000

    refactor: updated clickhouse tx schema

--- backend/pkg/commons/db/migrations/clickhouse/20241122163637_add_transactions.sql
+++ backend/pkg/commons/db/migrations/clickhouse/20241122163637_add_transactions.sql
@@ -1,15 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE transactions_ethereum (
-    tx_index UInt32 CODEC(T64, ZSTD(3)),
+CREATE TABLE IF NOT EXISTS transactions_ethereum (
+    tx_index UInt64 CODEC(T64, ZSTD(3)),
     tx_hash String,
     block_number UInt64 CODEC(T64, ZSTD(3)),           
     from_address String,
     to_address String,
     type LowCardinality(String) CODEC(ZSTD(3)),  
     method String CODEC(ZSTD(3)), 
-    value Int128,
-    nonce UInt32,
+    value UInt64,
+    nonce UInt64,
     status Enum('failed' = 0, 'success' = 1, 'partialy failed' = 2) CODEC(ZSTD(3)), 
     timestamp DateTime,
     gas Int64 CODEC(T64, ZSTD(3)),
@@ -21,27 +21,37 @@ CREATE TABLE transactions_ethereum (
     blob_gas_price Nullable(Int64) CODEC(ZSTD(3)),
     blob_gas_used Nullable(Int64) CODEC(T64, ZSTD(3)),
     access_list Array(Nullable(String)),
-    input_data Array(Nullable(UInt8)),
-    contract_created Nullable(String),
-    logs Array(Nullable(UInt8)),
-    logs_bloom Array(Nullable(UInt8)),
-    internal_data Nested(
-        from_address Nullable(String),
-        to_address Nullable(String),
-        type Nullable(String),
-        value Nullable(String),
-        path Nullable(String),
-        gas_limit Nullable(Int64),
-        error_msg Nullable(String)
-    ),
+    input_data Array(Nullable(UInt8)), -- []byte
+    is_contract_creation Boolean,
+    logs Array(Nullable(String)),
+    logs_bloom Array(Nullable(UInt8)),  -- []byte
     INDEX idx_to_address (to_address) TYPE bloom_filter(0.5) GRANULARITY 1,
     INDEX idx_method (method) TYPE bloom_filter(0.5) GRANULARITY 1
 ) ENGINE = MergeTree()
 ORDER BY (toStartOfWeek(timestamp), from_address, block_number, tx_index)
-PRIMARY KEY (toStartOfMonth(timestamp), from_address, block_number, tx_index)
+PRIMARY KEY (toStartOfWeek(timestamp), from_address, block_number, tx_index)
 PARTITION BY toStartOfQuarter(timestamp)
 SETTINGS index_granularity = 8192
 -- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS internal_tx_ethereum (
+    parent_hash String,
+    block_number UInt64 CODEC(T64, ZSTD(3)),
+    from_address String,
+    to_address String,
+    type String,
+    value String,
+    path String,
+    timestamp DateTime,
+    error_msg Nullable(String)
+)ENGINE = MergeTree()
+ORDER BY (toStartOfWeek(timestamp), parent_hash, from_address, block_number, value)
+PRIMARY KEY (toStartOfWeek(timestamp), parent_hash, from_address, block_number, value)
+PARTITION BY toStartOfMonth(timestamp) 
+SETTINGS index_granularity = 8192
+-- +goose StatementEnd
+
 
 -- +goose StatementBegin
 CREATE TABLE erc20_ethereum (
@@ -52,8 +62,8 @@ CREATE TABLE erc20_ethereum (
     token_address String,
     value Int128,
     log_index UInt32,
-    log_type Nullable(String),
-    transaction_log_index Nullable(UInt32) CODEC(T64, ZSTD(3)),
+    log_type String,
+    transaction_log_index UInt32 CODEC(T64, ZSTD(3)),
     removed Boolean,
     timestamp DateTime,
 
@@ -118,6 +128,9 @@ SETTINGS index_granularity = 8192
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE transactions_ethereum IF EXISTS
+-- +goose StatementEnd
+-- +goose StatementBegin
+DROP TABLE internal_tx_ethereum IF EXISTS
 -- +goose StatementEnd
 -- +goose StatementBegin
 DROP TABLE erc20_ethereum IF EXISTS
