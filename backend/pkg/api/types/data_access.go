1553554be4266c23682f9e1595e01aab1c6db270
remoterami <142154971+remoterami@users.noreply.github.com>
2024-11-29 11:03:41 +0100

    moved types to data access file

--- backend/pkg/api/types/data_access.go
+++ backend/pkg/api/types/data_access.go
@@ -2,10 +2,13 @@ package types
 
 import (
 	"database/sql"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gobitfly/beaconchain/pkg/api/enums"
 	t "github.com/gobitfly/beaconchain/pkg/commons/types"
+	"github.com/gobitfly/beaconchain/pkg/commons/log"
 	"github.com/gobitfly/beaconchain/pkg/consapi/types"
 	"github.com/gobitfly/beaconchain/pkg/monitoring/constants"
 	"github.com/shopspring/decimal"
@@ -324,3 +327,175 @@ const CtxUserIdKey CtxKey = "user_id"
 const CtxIsMockedKey CtxKey = "is_mocked"
 const CtxMockSeedKey CtxKey = "mock_seed"
 const CtxDashboardIdKey CtxKey = "dashboard_id"
+
+// -------------------------
+// Search structs
+
+// -- General
+type SearchType int
+
+// all possible search types
+const (
+	SearchTypeInteger SearchType = iota
+	SearchTypeName
+	SearchTypeEthereumAddress
+	SearchTypeWithdrawalCredential
+	SearchTypeEnsName
+	SearchTypeGraffiti
+	SearchTypeCursor
+	SearchTypeEmail
+	SearchTypePassword
+	SearchTypeEmailUserToken
+	SearchTypeJsonContentType
+	// Validator Dashboard
+	SearchTypeValidatorDashboardPublicId
+	SearchTypeValidatorPublicKeyWithPrefix
+	SearchTypeValidatorPublicKey
+)
+
+type Searchable interface {
+	GetSearches() []SearchType // optional: implement in embedding structs to limit regex pattern matches
+	SetSearchValue(s string)   // optional: implement for custom behavior
+	SetSearchType(st SearchType, b bool)
+	IsEnabled() bool
+	HasAnyMatches() bool
+}
+
+// not to be used directly, only for embedding
+type basicSearch struct {
+	types map[SearchType]bool
+	value string
+}
+
+func (bs *basicSearch) SetSearchValue(s string) {
+	if bs == nil {
+		log.Warnf("BasicSearch is nil, can't apply search: %s", s)
+		return
+	}
+	bs.value = s
+}
+
+func (bs *basicSearch) SetSearchType(st SearchType, b bool) {
+	if bs == nil {
+		bs = &basicSearch{}
+	}
+	bs.types[st] = b
+}
+
+func (bs *basicSearch) IsEnabled() bool {
+	return bs != nil && bs.value != ""
+}
+
+func (bs *basicSearch) HasAnyMatches() bool {
+	for _, v := range bs.types {
+		if v {
+			return true
+		}
+	}
+	return false
+}
+
+func (bs *basicSearch) GetSearches() []SearchType {
+	return []SearchType{
+		SearchTypeName,
+		SearchTypeInteger,
+		SearchTypeEthereumAddress,
+		SearchTypeWithdrawalCredential,
+		SearchTypeEnsName,
+		SearchTypeGraffiti,
+		SearchTypeCursor,
+		SearchTypeEmail,
+		SearchTypePassword,
+		SearchTypeEmailUserToken,
+		SearchTypeJsonContentType,
+		SearchTypeValidatorDashboardPublicId,
+		SearchTypeValidatorPublicKeyWithPrefix,
+		SearchTypeValidatorPublicKey,
+	}
+}
+
+type baseSearchResult struct {
+	Enabled bool
+}
+
+type SearchNumber struct {
+	baseSearchResult
+	Value uint64
+}
+
+type SearchString struct {
+	baseSearchResult
+	Value string
+}
+
+func (bs *basicSearch) AsNumber(st SearchType) SearchNumber {
+	if !bs.IsEnabled() {
+		log.Warn("tried accessing invalid search", 1)
+		return SearchNumber{}
+	}
+
+	if !bs.types[st] {
+		return SearchNumber{}
+	}
+
+	switch st {
+	case SearchTypeInteger:
+		number, err := strconv.ParseUint(bs.value, 10, 64)
+		if err != nil {
+			log.Error(err, "error converting search value, check regex parsing", 0)
+			return SearchNumber{}
+		}
+		return SearchNumber{baseSearchResult{true}, number}
+	}
+	return SearchNumber{}
+}
+
+func (bs *basicSearch) AsString(st SearchType) SearchString {
+	if !bs.IsEnabled() {
+		log.Warn("tried accessing invalid search", 1)
+		return SearchString{}
+	}
+
+	// apply custom conversion by type (e.g. prefix search term with 0x)
+	switch st {
+	case SearchTypeValidatorPublicKeyWithPrefix:
+		return SearchString{baseSearchResult{true}, strings.ToLower(bs.value)}
+	default:
+		return SearchString{baseSearchResult{true}, bs.value}
+	}
+}
+
+// -- Commonly used
+type SearchTableByIndexPubkeyGroup struct {
+	*basicSearch
+	// conditionals
+	DashboardId VDBId
+}
+
+func (s SearchTableByIndexPubkeyGroup) Index() SearchNumber {
+	return s.AsNumber(SearchTypeInteger)
+}
+
+func (s SearchTableByIndexPubkeyGroup) Pubkey() SearchString {
+	return s.AsString(SearchTypeValidatorPublicKeyWithPrefix)
+}
+
+func (s SearchTableByIndexPubkeyGroup) Group() SearchString {
+	if s.DashboardId.AggregateGroups || s.DashboardId.Validators != nil {
+		return SearchString{baseSearchResult{false}, ""}
+	}
+	return s.AsString(SearchTypeName)
+}
+
+func (s SearchTableByIndexPubkeyGroup) GetSearches() []SearchType {
+	return []SearchType{
+		SearchTypeInteger,
+		SearchTypeName,
+		SearchTypeValidatorPublicKeyWithPrefix,
+	}
+}
+
+// custom to filter out certain group searches
+func (s SearchTableByIndexPubkeyGroup) HasAnyMatches() bool {
+	return s.Group().Enabled || s.basicSearch.HasAnyMatches()
+}
