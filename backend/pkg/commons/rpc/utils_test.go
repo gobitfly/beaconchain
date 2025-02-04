dae278c23d01f88458d21b01be13cdc5fdce37a5
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2025-02-04 18:50:27 +0000

    test: add rpc/utils tests

---
+++ backend/pkg/commons/rpc/utils_test.go
@@ -0,0 +1,239 @@
+package rpc
+
+import (
+	"bytes"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	gethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/gobitfly/beaconchain/pkg/commons/types"
+)
+
+var (
+	aliceAddress = common.HexToAddress("0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5")
+	bobAddress   = common.HexToAddress("0x388C818CA8B9251b393131C08a736A67ccB19297")
+)
+
+func TestGetReceiver(t *testing.T) {
+	tests := []struct {
+		name     string
+		tx       *gethtypes.Transaction
+		expected []byte
+	}{
+		{
+			name:     "transaction with recipient",
+			tx:       gethtypes.NewTransaction(0, aliceAddress, big.NewInt(100), 100000, big.NewInt(10), []byte{}),
+			expected: aliceAddress.Bytes(),
+		},
+		{
+			name:     "contract creation transaction",
+			tx:       gethtypes.NewContractCreation(0, big.NewInt(100), 100000, big.NewInt(10), []byte{}),
+			expected: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := getReceiver(tt.tx)
+			if !bytes.Equal(result, tt.expected) {
+				t.Errorf("got %v, want %v", result, tt.expected)
+			}
+		})
+	}
+}
+
+func TestGetBlockWithdrawals(t *testing.T) {
+	tests := []struct {
+		name           string
+		blkWithdrawals gethtypes.Withdrawals
+		expected       []*types.Eth1Withdrawal
+	}{
+		{
+			name: "block with one withdrawal",
+			blkWithdrawals: gethtypes.Withdrawals{
+				{
+					Index:     1,
+					Validator: 2,
+					Address:   aliceAddress,
+					Amount:    100,
+				},
+			},
+			expected: []*types.Eth1Withdrawal{
+				{
+					Index:          1,
+					ValidatorIndex: 2,
+					Address:        aliceAddress.Bytes(),
+					Amount:         big.NewInt(100).Bytes(),
+				},
+			},
+		},
+		{
+			name: "block with two withdrawals",
+			blkWithdrawals: gethtypes.Withdrawals{
+				{
+					Index:     1,
+					Validator: 2,
+					Address:   aliceAddress,
+					Amount:    100,
+				},
+				{
+					Index:     2,
+					Validator: 3,
+					Address:   bobAddress,
+					Amount:    200,
+				},
+			},
+			expected: []*types.Eth1Withdrawal{
+				{
+					Index:          1,
+					ValidatorIndex: 2,
+					Address:        aliceAddress.Bytes(),
+					Amount:         big.NewInt(100).Bytes(),
+				},
+				{
+					Index:          2,
+					ValidatorIndex: 3,
+					Address:        bobAddress.Bytes(),
+					Amount:         big.NewInt(200).Bytes(),
+				},
+			},
+		},
+		{
+			name:           "block with no withdrawals",
+			blkWithdrawals: gethtypes.Withdrawals{},
+			expected:       nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := getBlockWithdrawals(tt.blkWithdrawals)
+			if len(result) != len(tt.expected) {
+				t.Fatalf("got %v withdrawals, want %v withdrawals", len(result), len(tt.expected))
+			}
+			for i, withdrawal := range result {
+				if withdrawal.Index != tt.expected[i].Index {
+					t.Errorf("got Index %v, want %v", withdrawal.Index, tt.expected[i].Index)
+				}
+				if withdrawal.ValidatorIndex != tt.expected[i].ValidatorIndex {
+					t.Errorf("got ValidatorIndex %v, want %v", withdrawal.ValidatorIndex, tt.expected[i].ValidatorIndex)
+				}
+				if !bytes.Equal(withdrawal.Address, tt.expected[i].Address) {
+					t.Errorf("got Address %v, want %v", withdrawal.Address, tt.expected[i].Address)
+				}
+				if !bytes.Equal(withdrawal.Amount, tt.expected[i].Amount) {
+					t.Errorf("got Amount %v, want %v", withdrawal.Amount, tt.expected[i].Amount)
+				}
+			}
+		})
+	}
+}
+
+func TestGetLogsFromReceipts(t *testing.T) {
+	tests := []struct {
+		name     string
+		logs     []*gethtypes.Log
+		expected []*types.Eth1Log
+	}{
+		{
+			name: "receipt with one log",
+			logs: []*gethtypes.Log{
+				{
+					Address: aliceAddress,
+					Topics:  []common.Hash{common.HexToHash("0x123"), common.HexToHash("0x234")},
+					Data:    []byte("data"),
+					Removed: false,
+				},
+			},
+			expected: []*types.Eth1Log{
+				{
+					Address: aliceAddress.Bytes(),
+					Topics:  [][]byte{common.HexToHash("0x123").Bytes(), common.HexToHash("0x234").Bytes()},
+					Data:    []byte("data"),
+					Removed: false,
+				},
+			},
+		},
+		{
+			name: "receipt with two logs",
+			logs: []*gethtypes.Log{
+				{
+					Address: aliceAddress,
+					Topics:  []common.Hash{common.HexToHash("0x123"), common.HexToHash("0x234")},
+					Data:    []byte("data"),
+					Removed: false,
+				},
+				{
+					Address: bobAddress,
+					Topics:  []common.Hash{common.HexToHash("0x123"), common.HexToHash("0x234")},
+					Data:    []byte("data"),
+					Removed: false,
+				},
+			},
+			expected: []*types.Eth1Log{
+				{
+					Address: aliceAddress.Bytes(),
+					Topics:  [][]byte{common.HexToHash("0x123").Bytes(), common.HexToHash("0x234").Bytes()},
+					Data:    []byte("data"),
+					Removed: false,
+				},
+				{
+					Address: bobAddress.Bytes(),
+					Topics:  [][]byte{common.HexToHash("0x123").Bytes(), common.HexToHash("0x234").Bytes()},
+					Data:    []byte("data"),
+					Removed: false,
+				},
+			},
+		},
+		{
+			name:     "receipt with no logs",
+			logs:     []*gethtypes.Log{},
+			expected: nil,
+		},
+		{
+			name: "receipt with logs but no topics",
+			logs: []*gethtypes.Log{
+				{
+					Address: aliceAddress,
+					Topics:  []common.Hash{},
+					Data:    []byte("data"),
+					Removed: false,
+				},
+			},
+			expected: []*types.Eth1Log{
+				{
+					Address: aliceAddress.Bytes(),
+					Topics:  [][]byte{},
+					Data:    []byte("data"),
+					Removed: false,
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := getLogsFromReceipts(tt.logs)
+			if len(result) != len(tt.expected) {
+				t.Fatalf("got %v logs, want %v logs", len(result), len(tt.expected))
+			}
+			for i, log := range result {
+				if !bytes.Equal(log.Address, tt.expected[i].Address) {
+					t.Errorf("got Address %v, want %v", log.Address, tt.expected[i].Address)
+				}
+				if !bytes.Equal(log.Data, tt.expected[i].Data) {
+					t.Errorf("got Data %v, want %v", log.Data, tt.expected[i].Data)
+				}
+				if log.Removed != tt.expected[i].Removed {
+					t.Errorf("got Removed %v, want %v", log.Removed, tt.expected[i].Removed)
+				}
+				for j, topic := range log.Topics {
+					if !bytes.Equal(topic, tt.expected[i].Topics[j]) {
+						t.Errorf("got Topic %v, want %v", topic, tt.expected[i].Topics[j])
+					}
+				}
+			}
+		})
+	}
+}
