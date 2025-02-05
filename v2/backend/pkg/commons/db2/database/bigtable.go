41e95e9bc7d85202207ff67553d9c750f96900d4
Tangui Clairet <181825613+Tangui-Bitfly@users.noreply.github.com>
2025-02-05 14:54:16 +0100

    chore(ci): add cache

--- v2/backend/pkg/commons/db2/database/bigtable.go
+++ v2/backend/pkg/commons/db2/database/bigtable.go
@@ -89,7 +89,6 @@ type BigTable struct {
 }
 
 func NewBigTableWithClient(ctx context.Context, client *bigtable.Client, adminClient *bigtable.AdminClient, tablesAndFamilies map[string][]string) (*BigTable, error) {
-	// Initialize the Bigtable table and column family
 	if err := initTable(ctx, adminClient, tablesAndFamilies); err != nil {
 		return nil, err
 	}
@@ -237,7 +236,6 @@ func (b BigTable) Read(table, prefix string) ([]Row, error) {
 }
 
 func (b BigTable) GetRow(table, key string) (*Row, error) {
-	// Open the transfer table for reading
 	tbl := b.client.Open(table)
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
