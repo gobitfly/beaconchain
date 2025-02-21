2dcd91b5a18c391ea50fead5497cfb78f774294d
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2025-02-21 12:16:38 +0000

    refactor: update pubkey_tags.go

--- backend/pkg/exporter/modules/pubkey_tags.go
+++ backend/pkg/exporter/modules/pubkey_tags.go
@@ -1,51 +1,57 @@
 package modules
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gobitfly/beaconchain/pkg/commons/db"
 	"github.com/gobitfly/beaconchain/pkg/commons/log"
 	"github.com/gobitfly/beaconchain/pkg/commons/metrics"
 	"github.com/gobitfly/beaconchain/pkg/monitoring/constants"
-	monitoringServices "github.com/gobitfly/beaconchain/pkg/monitoring/services"
+	"github.com/gobitfly/beaconchain/pkg/monitoring/services"
 )
 
 func UpdatePubkeyTag() {
 	log.Infof("Started Pubkey Tags Updater")
-	delay := time.Minute * 10
 	for {
-		start := time.Now()
-		r := monitoringServices.NewStatusReport(constants.Event_ExporterLegacyPubkeyTags, delay, time.Second*12)
-		r(constants.Running, nil)
-		tx, err := db.WriterDb.Beginx()
+		err := updatePubkeyTagOnce()
 		if err != nil {
-			log.Error(err, "Error connecting to DB", 0)
-			r(constants.Failure, map[string]string{"error": err.Error()})
-			// return err
+			log.Error(err, "error updating pubkey tags", 0)
 		}
-		_, err = tx.Exec(`INSERT INTO validator_tags (publickey, tag)
-		SELECT publickey, FORMAT('pool:%s', sps.name) tag
-		FROM eth1_deposits
-		inner join stake_pools_stats as sps on ENCODE(from_address::bytea, 'hex')=sps.address
-		WHERE sps.name NOT LIKE '%Rocketpool -%'
-		ON CONFLICT (publickey, tag) DO NOTHING;`)
-		if err != nil {
-			log.Error(err, "error updating validator_tags", 0)
-			r(constants.Failure, map[string]string{"error": err.Error()})
-			// return err
-		}
-
-		err = tx.Commit()
-		if err != nil {
-			log.Error(err, "error committing transaction", 0)
-			r(constants.Failure, map[string]string{"error": err.Error()})
-		}
-		_ = tx.Rollback()
-
-		log.Infof("Updating Pubkey Tags took %v sec.", time.Since(start).Seconds())
-		r(constants.Success, map[string]string{"took": time.Since(start).String(), "took_raw": time.Since(start).String()})
-		metrics.TaskDuration.WithLabelValues("validator_pubkey_tag_updater").Observe(time.Since(start).Seconds())
-
-		time.Sleep(delay)
+		time.Sleep(constants.Duration10Mins)
 	}
+}
+
+func updatePubkeyTagOnce() error {
+	startTime := time.Now()
+	statusReport := createPubkeyTagStatusReport()
+	statusReport(constants.Running, nil)
+
+	err := db.UpdatePubkeyTags()
+	if err != nil {
+		handlePubkeyTagsError(err, statusReport)
+		return err
+	}
+
+	log.Infof("Updating Pubkey Tags took %v sec.", time.Since(startTime).Seconds())
+	handlePubkeyTagSuccess(startTime, statusReport)
+	metrics.TaskDuration.WithLabelValues("validator_pubkey_tag_updater").Observe(time.Since(startTime).Seconds())
+
+	return nil
+}
+
+func createPubkeyTagStatusReport() func(status constants.StatusType, metadata map[string]string) {
+	return services.NewStatusReport(constants.Event_ExporterLegacyPubkeyTags, constants.Duration10Mins, time.Second*12)
+}
+
+func handlePubkeyTagsError(err error, statusReport func(status constants.StatusType, metadata map[string]string)) {
+	log.Error(err, "error updating validator_tags", 0)
+	statusReport(constants.Failure, map[string]string{"error": err.Error()})
+}
+
+func handlePubkeyTagSuccess(startTime time.Time, statusReport func(status constants.StatusType, metadata map[string]string)) {
+	statusReport(constants.Success, map[string]string{
+		"took":     time.Since(startTime).String(),
+		"took_raw": fmt.Sprintf("%v", time.Since(startTime).Milliseconds()),
+	})
 }
