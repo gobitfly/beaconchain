2e7dc3855bc60d9860abf002bc783b7c97f736e5
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2025-02-25 15:50:39 +0000

    test: add rocketpool DAO proposals test

---
+++ backend/pkg/exporter/modules/rocketpool_DAO_proposals_test.go
@@ -0,0 +1,171 @@
+package modules
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"regexp"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/gobitfly/beaconchain/pkg/commons/db"
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+	"github.com/rocket-pool/rocketpool-go/rocketpool"
+)
+
+func TestSaveDAOProposals(t *testing.T) {
+	tests := []struct {
+		name          string
+		daoTProposal  []*RocketpoolDAOProposal
+		mockSaveError error
+		expectedError bool
+	}{
+		{
+			name:          "successful save",
+			daoTProposal:  daoProposals,
+			expectedError: false,
+		},
+		{
+			name:          "SaveRocketPoolDAOProposals error",
+			daoTProposal:  daoProposals,
+			mockSaveError: errors.New("error"),
+			expectedError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dbMock, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("failed to create sqlmock: %v", err)
+			}
+			defer dbMock.Close()
+
+			sqlxDB := sqlx.NewDb(dbMock, "sqlmock")
+			db.WriterDb = sqlxDB
+
+			rp := &RocketpoolExporter{
+				API: &rocketpool.RocketPool{
+					RocketStorageContract: &rocketpool.Contract{
+						Address: func() *common.Address {
+							addr := common.HexToAddress("0x001")
+							return &addr
+						}(),
+					},
+				},
+				DAOProposalsByID: map[uint64]*RocketpoolDAOProposal{
+					1: &daoProposal,
+				},
+			}
+
+			nArgs := 18
+			valueStringsTpl := createValueStringsTemplate(nArgs)
+			valueStrings, valueArgs := rp.prepareDAOProposalBatch(tt.daoTProposal, valueStringsTpl, nArgs)
+
+			saveDAOProposalsQuery := fmt.Sprintf(saveDAOProposals, strings.Join(valueStrings, ","))
+			var driverArgs = make([]driver.Value, len(valueArgs))
+			for i, v := range valueArgs {
+				driverArgs[i] = driver.Value(v)
+			}
+
+			// mock SaveRocketPoolDAOProposals query
+			if tt.mockSaveError != nil {
+				mock.ExpectBegin()
+				mock.ExpectExec(regexp.QuoteMeta(saveDAOProposalsQuery)).WillReturnError(tt.mockSaveError)
+				mock.ExpectRollback()
+			} else {
+				mock.ExpectBegin()
+				mock.ExpectExec(regexp.QuoteMeta(saveDAOProposalsQuery)).WithArgs(driverArgs...).WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit()
+			}
+
+			err = rp.SaveDAOProposals()
+
+			if !tt.expectedError {
+				if err != nil {
+					t.Errorf("unexpected error: %v", err)
+				}
+			}
+			if tt.expectedError {
+				if err == nil {
+					t.Errorf("expected error got nil")
+				}
+			}
+
+		})
+	}
+}
+
+var daoProposals = []*RocketpoolDAOProposal{&daoProposal}
+var daoProposal = RocketpoolDAOProposal{
+	ID:              1,
+	DAO:             "dao",
+	ProposerAddress: []byte("0x001"),
+	Message:         "test-msg",
+	CreatedTime:     time.Unix(1, 0),
+	StartTime:       time.Unix(2, 0),
+	EndTime:         time.Unix(3, 0),
+	ExpiryTime:      time.Unix(4, 0),
+	VotesRequired:   1,
+	VotesFor:        1,
+	VotesAgainst:    1,
+	MemberVoted:     true,
+	MemberSupported: true,
+	IsCancelled:     false,
+	IsExecuted:      true,
+	Payload:         []byte("payload"),
+	State:           "state",
+	MemberVotes: []RocketpoolDAOProposalMemberVotes{
+		{
+			Address:    []byte("0x001"),
+			Voted:      true,
+			ProposalID: 1,
+			Supported:  true,
+		},
+	},
+}
+
+var (
+	saveDAOProposals = `INSERT INTO rocketpool_dao_proposals (
+				rocketpool_storage_address, 
+				id, 
+				dao, 
+				proposer_address,
+				message, 
+				created_time, 
+				start_time, 
+				end_time, 
+				expiry_time, 
+				votes_required, 
+				votes_for, 
+				votes_against, 
+				member_voted, 
+				member_supported, 
+				is_cancelled, 
+				is_executed, 
+				payload, 
+				state
+			) 
+			VALUES %s 
+			ON CONFLICT (rocketpool_storage_address, id) DO UPDATE SET 
+				dao = excluded.dao, 
+				proposer_address = excluded.proposer_address, 
+				message = excluded.message, 
+				created_time = excluded.created_time, 
+				start_time = excluded.start_time, 
+				end_time = excluded.end_time, 
+				expiry_time = excluded.expiry_time, 
+				votes_required = excluded.votes_required, 
+				votes_for = excluded.votes_for, 
+				votes_against = excluded.votes_against, 
+				member_voted = excluded.member_voted, 
+				member_supported = excluded.member_supported, 
+				is_cancelled = excluded.is_cancelled, 
+				is_executed = excluded.is_executed, 
+				payload = excluded.payload, 
+				state = excluded.state
+			`
+)
