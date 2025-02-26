b00f0dcc6613eca4a6c5929b437f3f74fceec432
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2025-02-26 12:16:29 +0000

    refactor: update genesis deposit

--- backend/pkg/exporter/modules/genesis_deposit.go
+++ backend/pkg/exporter/modules/genesis_deposit.go
@@ -1,23 +1,19 @@
 package modules
 
 import (
-	"database/sql"
-	"errors"
+	"fmt"
 	"time"
 
 	"github.com/gobitfly/beaconchain/pkg/commons/db"
 	"github.com/gobitfly/beaconchain/pkg/commons/log"
 	"github.com/gobitfly/beaconchain/pkg/commons/rpc"
+	"github.com/gobitfly/beaconchain/pkg/consapi/types"
 )
 
-func genesisDepositsExporter(client rpc.Client) {
+func genesisDepositsExporter(client rpc.Client, dbs *db.ConsensusDB) {
 	for {
-		// check if the beaconchain has started
-		var latestEpoch uint64
-		err := db.WriterDb.Get(&latestEpoch, "SELECT COALESCE(MAX(epoch), 0) FROM epochs")
+		latestEpoch, err := getLatestEpoch(dbs)
 		if err != nil {
-			log.Error(err, "error retrieving latest epoch from the database", 0)
-			time.Sleep(time.Second * 10)
 			continue
 		}
 
@@ -26,17 +22,12 @@ func genesisDepositsExporter(client rpc.Client) {
 			continue
 		}
 
-		// check if genesis-deposits have already been exported
-		var genesisDepositsCount uint64
-		err = db.WriterDb.Get(&genesisDepositsCount, "SELECT COUNT(*) FROM blocks_deposits WHERE block_slot=0")
+		genesisDepositCount, err := getGenesisDepositCount(dbs)
 		if err != nil {
-			log.Error(err, "error retrieving genesis-deposits-count when exporting genesis-deposits", 0)
-			time.Sleep(time.Minute)
 			continue
 		}
 
-		// if genesis-deposits have already been exported exit this go-routine
-		if genesisDepositsCount > 0 {
+		if genesisDepositCount > 0 {
 			return
 		}
 
@@ -47,71 +38,9 @@ func genesisDepositsExporter(client rpc.Client) {
 			continue
 		}
 
-		tx, err := db.WriterDb.Beginx()
+		err = exportGenesisDeposits(genesisValidators, dbs)
 		if err != nil {
-			log.Error(err, "error beginning db-tx when exporting genesis-deposits", 0)
-			time.Sleep(time.Minute)
-			continue
-		}
-
-		log.Infof("exporting deposit data for %v genesis validators", len(genesisValidators.Data))
-		for i, validator := range genesisValidators.Data {
-			if i%1000 == 0 {
-				log.Infof("exporting deposit data for genesis validator %v (%v/%v)", validator.Index, i, len(genesisValidators.Data))
-			}
-			_, err = tx.Exec(`INSERT INTO blocks_deposits (block_slot, block_root, block_index, publickey, withdrawalcredentials, amount, signature)
-			VALUES (0, '\x01', $1, $2, $3, $4, $5) ON CONFLICT DO NOTHING`,
-				validator.Index, validator.Validator.Pubkey, validator.Validator.WithdrawalCredentials, validator.Balance, []byte{0x0},
-			)
-			if err != nil {
-				err := tx.Rollback()
-				if err != nil {
-					log.Error(err, "error rolling back transaction", 0)
-				}
-				log.Error(err, "error exporting genesis-deposits: %v", 0)
-				time.Sleep(time.Minute)
-				continue
-			}
-		}
-
-		// hydrate the eth1 deposit signature for all genesis validators that have a corresponding eth1 deposit
-		_, err = tx.Exec(`
-			UPDATE blocks_deposits
-			SET signature = a.signature
-			FROM (
-				SELECT DISTINCT ON(publickey) publickey, signature
-				FROM eth1_deposits
-				WHERE valid_signature = true) AS a
-			WHERE block_slot = 0 AND blocks_deposits.publickey = a.publickey AND blocks_deposits.signature = '\x'`)
-		if err != nil {
-			err := tx.Rollback()
-			if err != nil && !errors.Is(err, sql.ErrTxDone) {
-				log.Error(err, "error rolling back transaction", 0)
-			}
-			log.Error(err, "error hydrating eth1 data into genesis-deposits", 0)
-			time.Sleep(time.Minute)
-			continue
-		}
-
-		// update deposits-count
-		_, err = tx.Exec("UPDATE blocks SET depositscount = $1 WHERE slot = 0", len(genesisValidators.Data))
-		if err != nil {
-			err := tx.Rollback()
-			if err != nil && !errors.Is(err, sql.ErrTxDone) {
-				log.Error(err, "error rolling back transaction", 0)
-			}
-			log.Error(err, "error updating deposit count for the genesis slot", 0)
-			time.Sleep(time.Minute)
-			continue
-		}
-
-		err = tx.Commit()
-		if err != nil {
-			err := tx.Rollback()
-			if err != nil && !errors.Is(err, sql.ErrTxDone) {
-				log.Error(err, "error rolling back transaction", 0)
-			}
-			log.Error(err, "error committing db-tx when exporting genesis-deposits", 0)
+			log.Error(err, "error exporting genesis-deposits: %v", 0)
 			time.Sleep(time.Minute)
 			continue
 		}
@@ -119,4 +48,50 @@ func genesisDepositsExporter(client rpc.Client) {
 		log.Infof("exported genesis-deposits for %v genesis-validators", len(genesisValidators.Data))
 		return
 	}
+}
+
+func getLatestEpoch(db db.ConsensusDBI) (uint64, error) {
+	latestEpoch, err := db.GetLatestEpoch()
+	if err != nil {
+		log.Error(err, "error retrieving latest epoch from the database", 0)
+		time.Sleep(time.Second * 10)
+		return 0, err
+	}
+
+	return latestEpoch, nil
+}
+func getGenesisDepositCount(db db.ConsensusDBI) (uint64, error) {
+	genesisDepositsCount, err := db.GetDepositsCountForBlockSlot()
+	if err != nil {
+		log.Error(err, "error retrieving genesis-deposits-count when exporting genesis-deposits", 0)
+		time.Sleep(time.Minute)
+		return 0, err
+	}
+	return genesisDepositsCount, nil
+}
+
+func exportGenesisDeposits(genesisValidators *types.StandardValidatorsResponse, db db.ConsensusDBI) error {
+	log.Infof("exporting deposit data for %v genesis validators", len(genesisValidators.Data))
+
+	for i, validator := range genesisValidators.Data {
+		if i%1000 == 0 {
+			log.Infof("exporting deposit data for genesis validator %v (%v/%v)", validator.Index, i, len(genesisValidators.Data))
+		}
+		err := db.SaveBlockDeposits(validator.Index, validator.Validator.Pubkey, validator.Validator.WithdrawalCredentials, validator.Balance)
+		if err != nil {
+			return fmt.Errorf("error exporting genesis-deposits: %v", err)
+		}
+	}
+
+	err := db.UpdateBlockDepositsSignature()
+	if err != nil {
+		return fmt.Errorf("error hydrating eth1 data into genesis-deposits: %v", err)
+	}
+
+	err = db.UpdateBlockDepositCount(len(genesisValidators.Data))
+	if err != nil {
+		return fmt.Errorf("error updating deposit count for the genesis slot: %v", err)
+	}
+
+	return nil
 }
