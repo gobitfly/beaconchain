cd90a3ea925caa2c1dc6a9321a29de55f159bdce
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2025-02-26 13:54:09 +0000

    refactor: mv ModuleLog to module_log.go

--- backend/pkg/exporter/modules/base.go
+++ backend/pkg/exporter/modules/base.go
@@ -18,7 +18,6 @@ import (
 	"github.com/gobitfly/beaconchain/pkg/monitoring/constants"
 	"github.com/gobitfly/beaconchain/pkg/monitoring/services"
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -213,64 +212,4 @@ func GetModuleContext() (ModuleContext, error) {
 type ModuleContext struct {
 	CL         consapi.Client
 	ConsClient *rpc.LighthouseClient
-}
-
-type ModuleLog struct {
-	module ModuleInterface
-}
-
-func (m ModuleLog) Info(message string) {
-	log.InfoWithFields(log.Fields{"module": m.module.GetName()}, message)
-}
-
-func (m ModuleLog) Infof(format string, args ...interface{}) {
-	log.InfoWithFields(log.Fields{"module": m.module.GetName()}, fmt.Sprintf(format, args...))
-}
-
-func (m ModuleLog) Debug(message string) {
-	if logrus.IsLevelEnabled(logrus.DebugLevel) {
-		log.DebugWithFields(log.Fields{"module": m.module.GetName()}, message)
-	}
-}
-
-func (m ModuleLog) Debugf(format string, args ...interface{}) {
-	if logrus.IsLevelEnabled(logrus.DebugLevel) {
-		log.DebugWithFields(log.Fields{"module": m.module.GetName()}, fmt.Sprintf(format, args...))
-	}
-}
-
-func (m ModuleLog) Trace(message string) {
-	if logrus.IsLevelEnabled(logrus.TraceLevel) {
-		log.TraceWithFields(log.Fields{"module": m.module.GetName()}, message)
-	}
-}
-
-func (m ModuleLog) Tracef(format string, args ...interface{}) {
-	if logrus.IsLevelEnabled(logrus.TraceLevel) {
-		log.TraceWithFields(log.Fields{"module": m.module.GetName()}, fmt.Sprintf(format, args...))
-	}
-}
-
-func (m ModuleLog) InfoWithFields(additionalInfos log.Fields, msg string) {
-	additionalInfos["module"] = m.module.GetName()
-	log.InfoWithFields(additionalInfos, msg)
-}
-
-func (m ModuleLog) Error(err error, errorMsg interface{}, callerSkip int, additionalInfos ...log.Fields) {
-	additionalInfos = append(additionalInfos, log.Fields{"module": m.module.GetName()})
-	log.Error(err, errorMsg, callerSkip, additionalInfos...)
-}
-
-func (m ModuleLog) Warn(err error, errorMsg interface{}, callerSkip int, additionalInfos ...log.Fields) {
-	additionalInfos = append(additionalInfos, log.Fields{"module": m.module.GetName()})
-	log.WarnWithStackTrace(err, errorMsg, callerSkip, additionalInfos...)
-}
-
-func (m ModuleLog) Warnf(format string, args ...interface{}) {
-	log.WarnWithFields(log.Fields{"module": m.module.GetName()}, fmt.Sprintf(format, args...))
-}
-
-func (m ModuleLog) Fatal(err error, errorMsg interface{}, callerSkip int, additionalInfos ...log.Fields) {
-	additionalInfos = append(additionalInfos, log.Fields{"module": m.module.GetName()})
-	log.Fatal(err, errorMsg, callerSkip, additionalInfos...)
 }
