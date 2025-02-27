09aec21d970d02cfe7b79d84a7c8d5411324c068
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2025-02-27 12:32:18 +0000

    test: udpated genesis deposit tests

--- backend/pkg/exporter/modules/genesis_deposit_test.go
+++ backend/pkg/exporter/modules/genesis_deposit_test.go
@@ -1,7 +1,9 @@
 package modules
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/gobitfly/beaconchain/pkg/commons/db/mocks"
 	rpcmocks "github.com/gobitfly/beaconchain/pkg/commons/rpc/mocks"
@@ -9,37 +11,49 @@ import (
 	"github.com/pkg/errors"
 )
 
-func TestProcessGenesisDeposits(t *testing.T) {
+func TestGenesisDepositsExporter_Export(t *testing.T) {
 	tests := []struct {
-		name                     string
-		mockEpochResponse        uint64
-		mockDepositCountResponse uint64
-		mockValidatorStateError  error
-		expectedShouldSleep      bool
-		expectedError            bool
+		name                        string
+		mockEpochResponse           uint64
+		mockDepositCountResponse    uint64
+		mockBlockDepositCountUpdate int
+		mockValidatorStateError     error
+		expectedError               bool
 	}{
 		{
-			name:                     "valid export",
-			mockEpochResponse:        1,
-			mockDepositCountResponse: 0,
-			expectedShouldSleep:      false,
+			name:                        "valid export",
+			mockEpochResponse:           1,
+			mockDepositCountResponse:    0,
+			mockBlockDepositCountUpdate: 1,
+			expectedError:               false,
 		},
 		{
-			name:                    "GetValidatorState error",
-			mockValidatorStateError: errors.New("error"),
-			expectedShouldSleep:     true,
-			expectedError:           true,
+			name:                        "GetValidatorState error",
+			mockDepositCountResponse:    2,
+			mockBlockDepositCountUpdate: 1,
+			mockValidatorStateError:     errors.New("error"),
+			expectedError:               true,
 		},
+	}
+
+	mockConsDBClient := new(mocks.ConsensusDBI)
+	mockRPCClient := new(rpcmocks.ValidatorClient)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	exporter := genesisDepositsExporter{
+		client: mockRPCClient,
+		db:     mockConsDBClient,
+		offset: 0,
+		ctx:    ctx,
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			mockRPCClient := new(rpcmocks.ValidatorClient)
-			mockRPCClient.On("GetValidatorState", uint64(0)).Return(genesisValidators, tt.mockValidatorStateError)
-
-			mockConsDBClient := new(mocks.ConsensusDBI)
-			mockConsDBClient.On("GetLatestEpoch").Return(tt.mockEpochResponse, nil)
-			mockConsDBClient.On("GetDepositsCountForBlockSlot").Return(tt.mockDepositCountResponse, nil)
+			// mock expected calls
+			mockConsDBClient.On("GetLatestEpoch").Return(uint64(1), nil)
+			mockConsDBClient.On("GetDepositsCountForBlockSlot").Return(uint64(0), nil)
+			mockRPCClient.On("GetValidatorState", uint64(0)).Return(genesisValidators, nil)
 			mockConsDBClient.On("SaveBlockDeposits",
 				genesisValidators.Data[0].Index,
 				[]byte(genesisValidators.Data[0].Validator.Pubkey),
@@ -47,24 +61,24 @@ func TestProcessGenesisDeposits(t *testing.T) {
 				genesisValidators.Data[0].Balance,
 			).Return(nil)
 			mockConsDBClient.On("UpdateBlockDepositsSignature").Return(nil)
-			mockConsDBClient.On("UpdateBlockDepositCount", int(1)).Return(nil)
+			mockConsDBClient.On("UpdateBlockDepositCount", tt.mockBlockDepositCountUpdate).Return(nil)
 
-			shouldSleep, err := processGenesisDeposits(mockRPCClient, mockConsDBClient)
+			exporter.Export()
 
-			if err != nil {
-				if tt.expectedError {
-					return
-				}
-				t.Errorf("expected no error, got: %v", err)
-			}
-			if tt.expectedError {
-				t.Error("expected error, got nil")
-			}
-			if shouldSleep != tt.expectedShouldSleep {
-				t.Errorf("expected shouldSleep: %v, got: %v", tt.expectedShouldSleep, shouldSleep)
-			}
+			mockConsDBClient.AssertCalled(t, "GetLatestEpoch")
+			mockConsDBClient.AssertCalled(t, "GetDepositsCountForBlockSlot")
+			mockRPCClient.AssertCalled(t, "GetValidatorState", uint64(0))
+			mockConsDBClient.AssertCalled(t, "SaveBlockDeposits",
+				genesisValidators.Data[0].Index,
+				[]byte(genesisValidators.Data[0].Validator.Pubkey),
+				[]byte(genesisValidators.Data[0].Validator.WithdrawalCredentials),
+				genesisValidators.Data[0].Balance,
+			)
+			mockConsDBClient.AssertCalled(t, "UpdateBlockDepositsSignature")
+			mockConsDBClient.AssertCalled(t, "UpdateBlockDepositCount", tt.mockBlockDepositCountUpdate)
 		})
 	}
+
 }
 
 var genesisValidators = &types.StandardValidatorsResponse{
