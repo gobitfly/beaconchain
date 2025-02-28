f1103439f405b67a468e4eb7e6c501abbabf5efc
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2025-02-28 10:53:27 +0000

    refactor: add ctx cancellation in update loop

--- backend/pkg/exporter/modules/pubkey_tags.go
+++ backend/pkg/exporter/modules/pubkey_tags.go
@@ -30,25 +30,31 @@ func newPubkeyTagsUpdater(ctx context.Context, db db.ConsensusDBI) pubkeyTagsUpd
 func (p *pubkeyTagsUpdater) Update() {
 	log.Infof("Started Pubkey Tags Updater")
 	for {
-		startTime := time.Now()
-		statusReport := services.NewStatusReport(constants.Event_ExporterLegacyPubkeyTags, utils.Config.DeploymentType, p.delay, time.Second*12)
-		statusReport(constants.Running, nil)
+		select {
+		case <-p.ctx.Done():
+			log.Info("update loop cancelled")
+			return
+		default:
+			startTime := time.Now()
+			statusReport := services.NewStatusReport(constants.Event_ExporterLegacyPubkeyTags, utils.Config.DeploymentType, p.delay, time.Second*12)
+			statusReport(constants.Running, nil)
 
-		err := p.db.UpdatePubkeyTags()
-		if err != nil {
-			log.Error(err, "error updating validator_tags", 0)
-			statusReport(constants.Failure, map[string]string{"error": err.Error()})
+			err := p.db.UpdatePubkeyTags()
+			if err != nil {
+				log.Error(err, "error updating validator_tags", 0)
+				statusReport(constants.Failure, map[string]string{"error": err.Error()})
+			}
+
+			log.Infof("Updating Pubkey Tags took %v sec.", time.Since(startTime).Seconds())
+
+			statusReport(constants.Success, map[string]string{
+				"took":     time.Since(startTime).String(),
+				"took_raw": fmt.Sprintf("%v", time.Since(startTime).Milliseconds()),
+			})
+
+			metrics.TaskDuration.WithLabelValues("validator_pubkey_tag_updater").Observe(time.Since(startTime).Seconds())
+
+			time.Sleep(p.delay)
 		}
-
-		log.Infof("Updating Pubkey Tags took %v sec.", time.Since(startTime).Seconds())
-
-		statusReport(constants.Success, map[string]string{
-			"took":     time.Since(startTime).String(),
-			"took_raw": fmt.Sprintf("%v", time.Since(startTime).Milliseconds()),
-		})
-
-		metrics.TaskDuration.WithLabelValues("validator_pubkey_tag_updater").Observe(time.Since(startTime).Seconds())
-
-		time.Sleep(p.delay)
 	}
 }
