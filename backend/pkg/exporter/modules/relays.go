945154d61c0cb72e7f9464532cb4bf2b3929dbb7
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2025-02-28 12:25:36 +0000

    refactor: updated relays.go handlers

--- backend/pkg/exporter/modules/relays.go
+++ backend/pkg/exporter/modules/relays.go
@@ -1,7 +1,7 @@
 package modules
 
 import (
-	"database/sql"
+	"context"
 	"encoding/json"
 	"fmt"
 	"math"
@@ -27,34 +27,48 @@ type BidTrace struct {
 	Value                types.WeiString `json:"value"`
 }
 
-func mevBoostRelaysExporter() {
-	var relays []types.Relay
-	for {
-		// we retrieve the relays from the db each loop to prevent having to restart the exporter for changes
-		relays = nil
-		err := db.ReaderDb.Select(&relays, `select tag_id, endpoint, public_link, is_censoring, is_ethical, export_failure_count, last_export_try_ts, last_export_success_ts from relays`)
-		wg := &sync.WaitGroup{}
-		mux := &sync.Mutex{}
-		if err == nil {
-			for _, relay := range relays {
-				if shouldTryToExportRelay(relay) {
-					// create relay logger
-					wg.Add(1)
-					go singleRelayExport(relay, wg, mux)
-				}
-			}
-		} else if err != sql.ErrNoRows {
-			log.Error(err, "failed to retrieve relays from db", 0)
-		}
-		wg.Wait()
-		time.Sleep(time.Minute)
+type relaysExporter struct {
+	db db.ConsensusDBI
+
+	delay time.Duration
+	ctx   context.Context
+}
+
+func newRelaysExporter(ctx context.Context, db db.ConsensusDBI) relaysExporter {
+	return relaysExporter{
+		db:    db,
+		delay: time.Minute,
+		ctx:   ctx,
 	}
 }
 
-func singleRelayExport(r types.Relay, wg *sync.WaitGroup, mux *sync.Mutex) {
-	defer wg.Done()
+func (rs *relaysExporter) MEVBoostRelaysExporter() {
+	for {
+		// we retrieve the relays from the db each loop to prevent having to restart the exporter for changes
+		relays, err := rs.db.GetRelays()
+		var wg sync.WaitGroup
+		if err == nil {
+			for _, relay := range relays {
+				if !shouldTryToExportRelay(relay) {
+					continue
+				}
+				wg.Add(1)
+				go func(r types.Relay) {
+					defer wg.Done()
+					rs.singleRelayExport(r)
+				}(relay)
+			}
+		} else {
+			log.Error(err, "failed to retrieve relays from db", 0)
+		}
 
-	err := exportRelayBlocks(r)
+		wg.Wait()
+		time.Sleep(rs.delay)
+	}
+}
+
+func (rs *relaysExporter) singleRelayExport(r types.Relay) {
+	err := rs.exportRelayBlocks(r)
 	if err != nil {
 		errMsg := fmt.Errorf("failed to export blocks for relay: %v", err)
 		if shouldLogExportAsError(r) {
@@ -63,22 +77,15 @@ func singleRelayExport(r types.Relay, wg *sync.WaitGroup, mux *sync.Mutex) {
 			log.WarnWithFields(log.Fields{"relay": r.ID}, errMsg.Error())
 		}
 
-		// Only increase the export_failure_count if we haven't already reached the maximum wait time
+		// only increase the export_failure_count if we haven't already reached the maximum wait time
 		_, isMaxWaitTime := waitTimeToExportRelay(r)
-		mux.Lock()
+
 		if isMaxWaitTime {
-			_, err = db.WriterDb.Exec(`
-			UPDATE relays SET
-				last_export_try_ts = (NOW() AT TIME ZONE 'utc')
-			WHERE tag_id = $1 AND endpoint = $2`, r.ID, r.Endpoint)
+			err = rs.db.UpdateRelayLastExportTry(r.ID, r.Endpoint)
 		} else {
-			_, err = db.WriterDb.Exec(`
-			UPDATE relays SET
-				export_failure_count = $1,
-				last_export_try_ts = (NOW() AT TIME ZONE 'utc')
-			WHERE tag_id = $2 AND endpoint = $3`, r.ExportFailureCount+1, r.ID, r.Endpoint)
+			err = rs.db.UpdateRelayExportFailureCount(r.ExportFailureCount, r.ID, r.Endpoint)
 		}
-		mux.Unlock()
+
 		if err != nil {
 			log.Error(err, "could not update failed relay export", 0, map[string]interface{}{"relay": r.ID})
 		}
@@ -86,32 +93,25 @@ func singleRelayExport(r types.Relay, wg *sync.WaitGroup, mux *sync.Mutex) {
 		return
 	}
 
-	mux.Lock()
-	_, err = db.WriterDb.Exec(`
-			UPDATE relays SET
-				export_failure_count = 0,
-				last_export_try_ts = (NOW() AT TIME ZONE 'utc'),
-				last_export_success_ts = (NOW() AT TIME ZONE 'utc')
-			WHERE tag_id = $1 AND endpoint = $2`, r.ID, r.Endpoint)
-	mux.Unlock()
+	err = rs.db.UpdateRelays(r.ID, r.Endpoint)
 	if err != nil {
-		log.Error(err, "could not update successful relay eport", 0, map[string]interface{}{"relay": r.ID})
+		log.Error(err, "could not update successful relay export", 0, map[string]interface{}{"relay": r.ID})
 	}
 
-	log.Infof("finished syncing payloads from relay")
+	log.Infof("finished syncing payloads from relay %v", r.ID)
 }
 
 func fetchDeliveredPayloads(r types.Relay, offset uint64) ([]BidTrace, error) {
-	var payloads []BidTrace
 	url := fmt.Sprintf("%s/relay/v1/data/bidtraces/proposer_payload_delivered?limit=100", r.Endpoint)
 	if offset != 0 {
 		url += fmt.Sprintf("&cursor=%v", offset)
 	}
+
 	client := &http.Client{
 		Timeout: time.Second * 30,
 	}
-	resp, err := client.Get(url)
 
+	resp, err := client.Get(url)
 	if err != nil {
 		log.Error(err, "error retrieving delivered payloads", 0, map[string]interface{}{"relay": r.ID, "offset": offset, "url": url})
 		return nil, fmt.Errorf("error retrieving delivered payloads for relay: %v, offset: %v, url: %v: %w", r.ID, offset, url, err)
@@ -119,6 +119,7 @@ func fetchDeliveredPayloads(r types.Relay, offset uint64) ([]BidTrace, error) {
 
 	defer resp.Body.Close()
 
+	var payloads []BidTrace
 	err = json.NewDecoder(resp.Body).Decode(&payloads)
 	if err != nil {
 		return nil, fmt.Errorf("error decoding json for delivered payloads for relay: %v, offset: %v, url: %v: %w", r.ID, offset, url, err)
@@ -127,123 +128,101 @@ func fetchDeliveredPayloads(r types.Relay, offset uint64) ([]BidTrace, error) {
 	return payloads, nil
 }
 
-func exportRelayBlocks(r types.Relay) error {
+func (rs *relaysExporter) exportRelayBlocks(r types.Relay) error {
 	// retrieve the oldest tag usage so we know when to stop processing payloads from the head
-	var lastUsage types.RelayBlock
-	err := db.ReaderDb.Get(&lastUsage, `SELECT tag_id, block_slot, block_root, exec_block_hash, value, builder_pubkey, proposer_pubkey, proposer_fee_recipient FROM relays_blocks WHERE tag_id=$1 ORDER BY block_slot DESC LIMIT 1`, r.ID)
+	lastUsage, err := rs.db.GetLastRelayBlock(r.ID)
 	if err != nil {
 		log.Error(err, "failed to retrieve last relay block from db, assuming none set", 0, map[string]interface{}{"relay": r.ID})
 	}
 
-	err = retrieveAndInsertPayloadsFromRelay(r, lastUsage.BlockSlot, 0)
+	err = rs.retrieveAndInsertPayloadsFromRelay(r, lastUsage.BlockSlot, 0)
 	if err != nil {
 		return err
 	}
 
 	// to make sure we dont have an incomplete table, check if there are any payloads before our first tag usage
-	var firstUsage types.RelayBlock
-	err = db.ReaderDb.Get(&firstUsage, `SELECT tag_id, block_slot, block_root, exec_block_hash, value, builder_pubkey, proposer_pubkey, proposer_fee_recipient FROM relays_blocks WHERE tag_id=$1 ORDER BY block_slot ASC LIMIT 1`, r.ID)
+	firstUsage, err := rs.db.GetFirstRelayBlock(r.ID)
 	if err != nil {
 		log.Error(err, "failed to retrieve first relay block from db, assuming none set", 0, map[string]interface{}{"relay": r.ID})
 	}
-	if firstUsage.BlockSlot == 0 {
-		return nil
-	}
-	err = retrieveAndInsertPayloadsFromRelay(r, 0, firstUsage.BlockSlot)
-	if err != nil {
-		log.Error(err, "failed to retrieve and insert possibly missing payloads", 0, map[string]interface{}{"relay": r.ID})
-		return err
+
+	if firstUsage.BlockSlot != 0 {
+		err = rs.retrieveAndInsertPayloadsFromRelay(r, 0, firstUsage.BlockSlot)
+		if err != nil {
+			log.Error(err, "failed to retrieve and insert possibly missing payloads", 0, map[string]interface{}{"relay": r.ID})
+			return err
+		}
 	}
 
 	return nil
 }
 
-func retrieveAndInsertPayloadsFromRelay(r types.Relay, low_bound uint64, high_bound uint64) error {
-	tx, err := db.WriterDb.Beginx()
-	if err != nil {
-		log.Error(err, "failed to start db transaction", 0)
-		return err
-	}
-	defer utils.Rollback(tx)
+func (rs *relaysExporter) retrieveAndInsertPayloadsFromRelay(r types.Relay, lowBound, highBound uint64) error {
+	minSlot := calculateMinSlot(lowBound)
+	offset := highBound
 
-	var min_slot uint64
-	if low_bound > 10 {
-		min_slot = low_bound - 10
-	}
-
-	offset := high_bound
 	for {
-		resp, err := fetchDeliveredPayloads(r, offset)
+		payloads, err := fetchDeliveredPayloads(r, offset)
 		if err != nil {
 			return fmt.Errorf("error calling fetchDeliveredPayloads with offset: %v for relay: %v: %w", offset, r.ID, err)
 		}
 
-		if resp == nil {
+		if len(payloads) == 0 {
 			log.Error(fmt.Errorf("got no payloads"), "", 0, map[string]interface{}{"relay": r.ID})
 			break
 		}
 
-		for _, payload := range resp {
+		for _, payload := range payloads {
 			// first insert the tag into the blocks_tags table
-			_, err = tx.Exec(`
-				insert into blocks_tags
-				select blocks.slot, blocks.blockroot, $1
-				from blocks
-				where
-					blocks.slot = $2 and
-					blocks.exec_block_hash = $3
-				ON CONFLICT DO NOTHING`, r.ID, payload.Slot, utils.MustParseHex(payload.BlockHash))
+			err := rs.db.SaveBlocksTags(r.ID,
+				payload.Slot,
+				utils.MustParseHex(payload.BlockHash))
 			if err != nil {
 				log.Error(fmt.Errorf("failed to insert payload into blocks_tags table"), "", 0, map[string]interface{}{"relay": r.ID})
 				return err
 			}
-			_, err = tx.Exec(`
-				insert into relays_blocks
-				(
-					tag_id,
-					block_slot,
-					block_root,
-					exec_block_hash,
-					value,
-					builder_pubkey,
-					proposer_pubkey,
-					proposer_fee_recipient
-				)
-				select
-					$1,	blocks.slot, blocks.blockroot, blocks.exec_block_hash, $4, $5, $6, $7
-				from blocks
-				where
-					blocks.slot = $2 and
-					blocks.exec_block_hash = $3
-				ON CONFLICT (block_slot, block_root, tag_id) DO NOTHING`,
-				r.ID, payload.Slot, utils.MustParseHex(payload.BlockHash),
-				payload.Value, utils.MustParseHex(payload.BuilderPubkey),
+
+			err = rs.db.SaveBlocksRelays(r.ID,
+				payload.Slot,
+				payload.Value,
+				utils.MustParseHex(payload.BlockHash),
+				utils.MustParseHex(payload.BuilderPubkey),
 				utils.MustParseHex(payload.ProposerPubkey),
 				utils.MustParseHex(payload.ProposerFeeRecipient))
 			if err != nil {
 				log.Error(fmt.Errorf("failed to insert payload into relays_blocks table"), "", 0, map[string]interface{}{"relay": r.ID})
 				return err
 			}
+
 		}
 
-		if len(resp) == 0 || resp[len(resp)-1].Slot < min_slot {
+		if payloads[len(payloads)-1].Slot < minSlot {
 			// last payload we received is bellow than our calculated min_slot
 			break
 		}
 
-		if len(resp) < 100 {
+		if len(payloads) < 100 {
 			// if the response is less than 100 payloads, we assume that we have reached the end and break
 			break
 		}
-		if resp[len(resp)-1].Slot == offset {
+
+		if payloads[len(payloads)-1].Slot == offset {
 			return fmt.Errorf("relay doesn't follow spec, last returned slot matches offset (sort order ascending instead of descending)")
 		}
 
 		// sleep for a bit to not kill the relay
-		offset = resp[len(resp)-1].Slot
-		time.Sleep(time.Second * 1)
+		offset = payloads[len(payloads)-1].Slot
+		time.Sleep(time.Second)
 	}
-	return tx.Commit()
+
+	return nil
+}
+
+func calculateMinSlot(lowBound uint64) uint64 {
+	if lowBound > 10 {
+		return lowBound - 10
+	}
+	return 0
 }
 
 func shouldTryToExportRelay(r types.Relay) bool {
@@ -262,11 +241,10 @@ func waitTimeToExportRelay(r types.Relay) (waitTime time.Duration, isMaxWaitTime
 		waitTime = maxWaitTimeForRelayExport
 		isMaxWaitTime = true
 	}
-	return
+	return waitTime, isMaxWaitTime
 }
 
 func shouldLogExportAsError(r types.Relay) bool {
 	maxWaitTimeForRelayExportError := utils.Month
-
 	return time.Since(r.LastExportSuccessTs) >= maxWaitTimeForRelayExportError
 }
