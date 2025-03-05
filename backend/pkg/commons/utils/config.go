5eddfc388b6d13f0ef3ef9c3123dace7d171a670
remoterami <142154971+remoterami@users.noreply.github.com>
2025-03-05 15:10:27 +0100

    fix: added pectra configs
    (cherry picked from commit a17f8f8f59f4a2db90eacd14e1592ae28bf40f59)
    (cherry picked from commit 2450ba3aa4752c0cc6cf37a92ba674a3c7f14130)

--- backend/pkg/commons/utils/config.go
+++ backend/pkg/commons/utils/config.go
@@ -438,10 +438,6 @@ func setCLConfig(cfg *types.Config) error {
 			log.Warnf("ElectraForkEpoch not set, defaulting to maxForkEpoch")
 			jr.Data.ElectraForkEpoch = &MaxForkEpoch
 		}
-		if jr.Data.ElectraForkEpoch == nil {
-			log.Warnf("ElectraForkEpoch not set, defaulting to maxForkEpoch")
-			jr.Data.ElectraForkEpoch = &MaxForkEpoch
-		}
 
 		chainCfg := types.ClChainConfig{
 			PresetBase:                              jr.Data.PresetBase,
