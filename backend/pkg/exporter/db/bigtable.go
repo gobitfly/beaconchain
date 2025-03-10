bbbffb05a7f651fdf067ecaa009dea9bcb4fdd55
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2025-03-10 13:22:37 +0000

    refactor: updated bigtable repo calls

--- backend/pkg/exporter/db/bigtable.go
+++ backend/pkg/exporter/db/bigtable.go
@@ -22,18 +22,18 @@ func NewSlotExporterBT(btClient *db.Bigtable) *SlotExporterBT {
 	}
 }
 
-func (r *SlotExporterBT) GetValidatorBalanceHistory(validators []uint64, startEpoch uint64, endEpoch uint64) (map[uint64][]*types.ValidatorBalance, error) {
-	return db.BigtableClient.GetValidatorBalanceHistory(validators, startEpoch, endEpoch)
+func (s *SlotExporterBT) GetValidatorBalanceHistory(validators []uint64, startEpoch uint64, endEpoch uint64) (map[uint64][]*types.ValidatorBalance, error) {
+	return s.btClient.GetValidatorBalanceHistory(validators, startEpoch, endEpoch)
 }
 
-func (r *SlotExporterBT) SaveAttestationDuties(attDuties map[types.Slot]map[types.ValidatorIndex][]types.Slot) error {
-	return db.BigtableClient.SaveAttestationDuties(attDuties)
+func (s *SlotExporterBT) SaveAttestationDuties(attDuties map[types.Slot]map[types.ValidatorIndex][]types.Slot) error {
+	return s.btClient.SaveAttestationDuties(attDuties)
 }
 
-func (r *SlotExporterBT) SaveSyncCommitteeDuties(syncDuties map[types.Slot]map[types.ValidatorIndex]bool) error {
-	return db.BigtableClient.SaveSyncCommitteeDuties(syncDuties)
+func (s *SlotExporterBT) SaveSyncCommitteeDuties(syncDuties map[types.Slot]map[types.ValidatorIndex]bool) error {
+	return s.btClient.SaveSyncCommitteeDuties(syncDuties)
 }
 
-func (r *SlotExporterBT) SaveValidatorBalances(epoch uint64, validators []*types.Validator) error {
-	return db.BigtableClient.SaveValidatorBalances(epoch, validators)
+func (s *SlotExporterBT) SaveValidatorBalances(epoch uint64, validators []*types.Validator) error {
+	return s.btClient.SaveValidatorBalances(epoch, validators)
 }
