e24a7ca6aa1f7bfe9bba188c8c358f9981d5e9ad
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-03-11 11:41:25 +0100

    docs(useCurrency): fix `default maxFractionDigits` for `crypto currencys`

--- v2/frontend/composables/useCurrency.ts
+++ v2/frontend/composables/useCurrency.ts
@@ -103,7 +103,7 @@ export const useCurrency = () => {
    * maximumFractionDigits and minimumFractionDigits depending on targetCurrency
    * @param {number} [options.maximumFractionDigits] - Defaults:
    *   - fiat: 2 (4 if hasHigherPrecision=true)
-   *   - crypto: 9 (18 if hasHigherPrecision=true)
+   *   - crypto: 6 (18 if hasHigherPrecision=true)
    * @returns {string} The formatted amount with currency code
    */
   const formatAmount = (
