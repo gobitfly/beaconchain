da42f31962dcc9674870f4aaf6850120277206c5
remoterami <142154971+remoterami@users.noreply.github.com>
2025-03-20 10:30:51 +0100

    refactor: new apr calculation

--- backend/pkg/api/data_access/vdb_summary.go
+++ backend/pkg/api/data_access/vdb_summary.go
@@ -710,11 +710,7 @@ func (d *DataAccessService) GetValidatorDashboardGroupSummary(ctx context.Contex
 	ret.MissedRewards.ProposerRewards.Cl = utils.GWeiToWei(big.NewInt(totalMissedRewardsBlocksCl))
 	ret.MissedRewards.ProposerRewards.El = decimal.NewFromFloat(totalMissedRewardsEl)
 
-	investedAmount, err := d.GetValidatorDashboardEffectiveBalanceTotal(ctx, dashboardId, true)
-	if err != nil {
-		return nil, err
-	}
-	incomeInfo, err := d.getElClAPR(ctx, dashboardId, groupId, hours, investedAmount)
+	incomeInfo, err := d.getElClAPR(ctx, dashboardId, groupId, hours)
 	if err != nil {
 		return nil, err
 	}
