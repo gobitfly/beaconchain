4055e4278fbe2a4699d20d4364f696f0cc56a7c3
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2025-03-24 15:33:08 +0000

    refactor: update client call metrics buckets

--- backend/pkg/commons/metrics/metrics.go
+++ backend/pkg/commons/metrics/metrics.go
@@ -105,7 +105,7 @@ var (
 	ClientCallDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "client_call_duration_milliseconds",
 		Help:    "Duration of client calls",
-		Buckets: []float64{0.003, 0.005, 0.01, 0.03, .05, .1, .3, .5, 1, 3, 5, 10, 20, 30},
+		Buckets: []float64{0.003, 0.005, 0.01, 0.03, .05, .1, .3, .5, 1, 3, 5, 10},
 	}, []string{"client", "method"})
 )
 
