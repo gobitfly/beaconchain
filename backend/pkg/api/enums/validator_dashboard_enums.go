f2867d4ab8bb777dd8b2e8146b70a2c04969dae8
remoterami <142154971+remoterami@users.noreply.github.com>
2025-03-25 16:46:11 +0100

    feat(api): return tailored mocked data

--- backend/pkg/api/enums/validator_dashboard_enums.go
+++ backend/pkg/api/enums/validator_dashboard_enums.go
@@ -265,7 +265,7 @@ func (c VDBConsolidationsElColumn) Int() int {
 
 func (VDBConsolidationsElColumn) NewFromString(s string) VDBConsolidationsElColumn {
 	switch s {
-	case "", "block_processed", "age":
+	case "", "block_processed", "timestamp":
 		return VDBConsolidationElBlockProcessed
 	default:
 		return VDBConsolidationsElColumn(-1)
@@ -305,7 +305,7 @@ func (c VDBConsolidationsClColumn) Int() int {
 
 func (VDBConsolidationsClColumn) NewFromString(s string) VDBConsolidationsClColumn {
 	switch s {
-	case "", "slot_processed", "age":
+	case "", "slot_processed", "timestamp":
 		return VDBConsolidationClSlotProcessed
 	default:
 		return VDBConsolidationsClColumn(-1)
