d2ef59dc49dbc66646d699a05a8de012f0b4b230
Monika-Bitfly <181824500+Monika-Bitfly@users.noreply.github.com>
2025-03-26 12:06:21 +0000

    refactor: update bigtable metrics

--- backend/pkg/exporter/db/bigtable.go
+++ backend/pkg/exporter/db/bigtable.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gobitfly/beaconchain/pkg/commons/db"
 	"github.com/gobitfly/beaconchain/pkg/commons/types"
+	"github.com/gobitfly/beaconchain/pkg/exporter/metrics"
 )
 
 type SlotExporterBTRepository interface {
@@ -17,29 +18,51 @@ type SlotExporterBTRepository interface {
 }
 
 type SlotExporterBT struct {
-	client *db.Bigtable
+	client  *db.Bigtable
+	metrics metrics.MetricsRepository
 }
 
 func NewSlotExporterBT(btClient *db.Bigtable) *SlotExporterBT {
 	return &SlotExporterBT{
-		client: btClient,
+		client:  btClient,
+		metrics: metrics.NewMetrics(),
 	}
 }
 
 func (s *SlotExporterBT) GetValidatorBalanceHistory(validators []uint64, startEpoch uint64, endEpoch uint64) (map[uint64][]*types.ValidatorBalance, error) {
-	return s.client.GetValidatorBalanceHistory(validators, startEpoch, endEpoch)
+	balanceHistory, err := s.client.GetValidatorBalanceHistory(validators, startEpoch, endEpoch)
+	if err != nil {
+		s.metrics.Error("slot_exporter_get_validator_balance_history")
+		return nil, err
+	}
+	return balanceHistory, nil
 }
 
 func (s *SlotExporterBT) SaveAttestationDuties(attDuties map[types.Slot]map[types.ValidatorIndex][]types.Slot) error {
-	return s.client.SaveAttestationDuties(attDuties)
+	err := s.client.SaveAttestationDuties(attDuties)
+	if err != nil {
+		s.metrics.Error("slot_exporter_save_attestation_duties")
+		return err
+	}
+	return nil
 }
 
 func (s *SlotExporterBT) SaveSyncCommitteeDuties(syncDuties map[types.Slot]map[types.ValidatorIndex]bool) error {
-	return s.client.SaveSyncCommitteeDuties(syncDuties)
+	err := s.client.SaveSyncCommitteeDuties(syncDuties)
+	if err != nil {
+		s.metrics.Error("slot_exporter_save_committee_duties")
+		return err
+	}
+	return nil
 }
 
 func (s *SlotExporterBT) SaveValidatorBalances(epoch uint64, validators []*types.Validator) error {
-	return s.client.SaveValidatorBalances(epoch, validators)
+	err := s.client.SaveValidatorBalances(epoch, validators)
+	if err != nil {
+		s.metrics.Error("slot_exporter_save_validator_balances")
+		return err
+	}
+	return nil
 }
 
 func (s *SlotExporterBT) GetLastAttestationCacheMux() *sync.Mutex {
