8e9885c2333785374c5730a7d7a9e6e80e874c5f
Monika <181824500+Monika-Bitfly@users.noreply.github.com>
2025-04-01 14:22:54 +0200

    refactor: revise and add tests for `exporter/modules/sync_committees_count.go` (#1373)
    * refactor: updated sync committees count handlers
    
    * refactor: move and update sync committees count db functions
    
    * test: add ConsensusDBI mocks wth mockery
    
    * fix: updated db and regenerated mocks for ConsensusDBI
    
    * refactor: add ctx cancellation in exporter loop
    
    * test: added sync committees count tests
    
    * test: updated sync committees count tests for no records in db
    
    * tests: updated Export func tests
    
    * refactor: rename ConsensusDBI to ConsensusRepository and move it to /db2/consensus.go
    
    * test: update ConsensusRepository mocks with mockery

---
+++ backend/pkg/exporter/modules/sync_committees_count_test.go
@@ -0,0 +1,69 @@
+package modules
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	dbmocks "github.com/gobitfly/beaconchain/pkg/commons/db2/mocks"
+	"github.com/gobitfly/beaconchain/pkg/commons/types"
+	"github.com/gobitfly/beaconchain/pkg/commons/utils"
+)
+
+func TestSyncCommitteesCountExport(t *testing.T) {
+	utils.Config = &types.Config{
+		Chain: types.Chain{
+			ClConfig: types.ClChainConfig{
+				AltairForkEpoch:              1,
+				EpochsPerSyncCommitteePeriod: 32,
+			},
+		},
+		DeploymentType: "development",
+	}
+
+	t.Run("records exist in db", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+
+		mockConsDBClient := new(dbmocks.ConsensusRepository)
+		exporter := syncCommitteesCountExporter{
+			db:    mockConsDBClient,
+			delay: 0,
+			ctx:   ctx,
+		}
+
+		mockConsDBClient.On("GetSyncCommitteesCountPerValidator").Return(uint64(1), nil)
+		mockConsDBClient.On("GetLatestFinalizedEpoch").Return(uint64(1), nil)
+		mockConsDBClient.On("GetTotalPeriodSyncCommitteesCountPerValidator").Return(uint64(1), nil)
+		mockConsDBClient.On("GetCountSoFarSyncCommitteesCountPerValidator", uint64(1)).Return(float64(1.0), nil)
+
+		exporter.Export()
+
+		mockConsDBClient.AssertCalled(t, "GetSyncCommitteesCountPerValidator")
+		mockConsDBClient.AssertCalled(t, "GetLatestFinalizedEpoch")
+		mockConsDBClient.AssertCalled(t, "GetTotalPeriodSyncCommitteesCountPerValidator")
+		mockConsDBClient.AssertCalled(t, "GetCountSoFarSyncCommitteesCountPerValidator", uint64(1))
+	})
+
+	t.Run("no records in db", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+
+		mockConsDBClient := new(dbmocks.ConsensusRepository)
+		exporter := syncCommitteesCountExporter{
+			db:    mockConsDBClient,
+			delay: 0,
+			ctx:   ctx,
+		}
+
+		mockConsDBClient.On("GetSyncCommitteesCountPerValidator").Return(uint64(0), nil)
+		mockConsDBClient.On("GetLatestFinalizedEpoch").Return(uint64(0), nil)
+		mockConsDBClient.On("SaveSyncCommitteesCount", uint64(0), float64(0.0)).Return(nil)
+
+		exporter.Export()
+
+		mockConsDBClient.AssertCalled(t, "GetSyncCommitteesCountPerValidator")
+		mockConsDBClient.AssertCalled(t, "GetLatestFinalizedEpoch")
+		mockConsDBClient.AssertCalled(t, "SaveSyncCommitteesCount", uint64(0), float64(0.0))
+	})
+}
