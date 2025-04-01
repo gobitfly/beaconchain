43753de283304312f5f852419761ba37dcd0016f
Monika <181824500+Monika-Bitfly@users.noreply.github.com>
2025-04-01 15:25:47 +0200

    refactor: revise and add tests for `exporter/modules/pubkey_tags.go` (#1368)
    * refactor: updated pubkey tags handler
    
    * test: add ConsensusDBI mocks wth mockery
    
    * refactor: rename pubkeyUpdater to pubkeyTagsUpdater
    
    * refactor: add ctx cancellation in update loop
    
    * test: added pubkey tags Update func tests
    
    * test: rename test name
    
    * fix: lint
    
    * refactor: mv ConsensusDB to consensus_db.go
    
    * refactor: rename ConsensusDBI to ConsensusRepository and move it to /db2/consensus.go
    
    * test: update development type typo
    
    * test: update test ctx timeout
    
    * test: update ConsensusRepository mocks with mockery

---
+++ v2/backend/pkg/exporter/modules/pubkey_tags_test.go
@@ -0,0 +1,52 @@
+package modules
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	dbmocks "github.com/gobitfly/beaconchain/pkg/commons/db2/mocks"
+	"github.com/gobitfly/beaconchain/pkg/commons/types"
+	"github.com/gobitfly/beaconchain/pkg/commons/utils"
+	"github.com/pkg/errors"
+)
+
+func TestPubkeyTagsUpdate(t *testing.T) {
+	tests := []struct {
+		name      string
+		mockError error
+	}{
+		{
+			name:      "successful db update",
+			mockError: nil,
+		},
+		{
+			name:      "db update error",
+			mockError: errors.New("error"),
+		},
+	}
+
+	mockConsDBClient := new(dbmocks.ConsensusRepository)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	exporter := pubkeyTagsUpdater{
+		db:    mockConsDBClient,
+		delay: 0,
+		ctx:   ctx,
+	}
+
+	utils.Config = &types.Config{
+		DeploymentType: "development",
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockConsDBClient.On("UpdatePubkeyTags").Return(tt.mockError)
+
+			exporter.Update()
+
+			mockConsDBClient.AssertCalled(t, "UpdatePubkeyTags")
+		})
+	}
+}
