3e11cbe40a0e77b5aa61bb50f646a05d3134bff3
invis-bitfly <162128378+invis-bitfly@users.noreply.github.com>
2025-04-08 15:18:29 +0200

    refactor(dashboard_exporter): use custom base64 type

--- backend/pkg/consapi/types/debug.go
+++ backend/pkg/consapi/types/debug.go
@@ -1,13 +1,39 @@
 package types
 
 import (
+	"encoding/base64"
+	"fmt"
+
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/pkg/errors"
 )
 
+type Base64Bytes []byte
+
+func (b Base64Bytes) String() string {
+	return hexutil.Bytes(b).String()
+}
+
+func (b Base64Bytes) MarshalText() ([]byte, error) {
+	return hexutil.Bytes(b).MarshalText()
+}
+
+func (b *Base64Bytes) UnmarshalText(text []byte) error {
+	decoded, err := base64.StdEncoding.DecodeString(string(text))
+	if err != nil {
+		return errors.Wrap(err, "failed to decode base64")
+	}
+	if len(decoded) == 0 {
+		return fmt.Errorf("base64 decoded string is empty")
+	}
+	*b = Base64Bytes(decoded)
+	return nil
+}
+
 type ElectraDeposit struct {
-	Pubkey         hexutil.Bytes `json:"pubkey" db:"pubkey"`
-	Amount         int64         `json:"amount,string" db:"amount"`
-	SignatureValid bool          `json:"signature_valid" db:"signature_valid"`
+	Pubkey         Base64Bytes `json:"pubkey" db:"pubkey"`
+	Amount         int64       `json:"amount,string" db:"amount"`
+	SignatureValid bool        `json:"signature_valid" db:"signature_valid"`
 }
 
 type ElectraConsolidation struct {
