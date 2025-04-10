e02f7044dcb984684fe3a6bf462ef5723fa71019
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-04-10 10:18:14 +0200

    feat(useSettingsStore): show `relative time` by default
    See: BEDS-1044

--- v2/frontend/stores/useSettingsStore.ts
+++ v2/frontend/stores/useSettingsStore.ts
@@ -1,7 +1,7 @@
 export type DateTimeFormat = 'absolute' | 'relative'
 
 export const useSettingsStore = defineStore('settings', () => {
-  const dateTimeFormat = ref<DateTimeFormat>('absolute')
+  const dateTimeFormat = ref<DateTimeFormat>('relative')
   const toggleDateTimeFormat = () => {
     dateTimeFormat.value = dateTimeFormat.value === 'absolute' ? 'relative' : 'absolute'
   }
