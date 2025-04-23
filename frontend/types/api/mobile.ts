255d6803b9d4227515814d777daf1793bad8c4d1
remoterami <142154971+remoterami@users.noreply.github.com>
2025-04-23 09:55:32 +0200

    refactor(api): remove activation queue position field

--- frontend/types/api/mobile.ts
+++ frontend/types/api/mobile.ts
@@ -35,7 +35,6 @@ export interface MobileValidatorDashboardValidatorsTableRow {
   group_id: number /* uint64 */;
   balance: string /* decimal.Decimal */;
   status: 'slashed' | 'exited' | 'deposited' | 'pending' | 'slashing_offline' | 'slashing_online' | 'exiting_offline' | 'exiting_online' | 'active_offline' | 'active_online';
-  queue_position?: number /* uint64 */;
   withdrawal_credential: Hash;
   /**
    * additional mobile fields
