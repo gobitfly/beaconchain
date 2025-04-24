9b8c5371298d84d6bc884b330270b59ac7c7e8c6
enzo-bitfly <194075446+enzo-bitfly@users.noreply.github.com>
2025-04-24 12:05:21 +0200

    feat: add `CL withdrawals table`
    -Remove obsolete `DashboardTableWithdrawals.vue`
    
    See: BEDS-1352

--- frontend/pages/dashboard/[[id]]/index.vue
+++ frontend/pages/dashboard/[[id]]/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import {
   DashboardCreationController, DashboardTableBlocks, DashboardTableEmpty, DashboardTableRewards, DashboardTableSummary,
-  DashboardTableWithdrawals,
 } from '#components'
 import type { GuestDashboard } from '~/types/dashboard'
 import {
@@ -49,7 +48,6 @@ const tabs: HashTabs = [
     title: $t('dashboard.validator.tabs.deposits'),
   },
   {
-    component: DashboardTableWithdrawals,
     icon: 'money-bill',
     key: 'withdrawals',
     title: $t('dashboard.validator.tabs.withdrawals'),
@@ -246,7 +244,7 @@ const clDeposits = computed(() => {
 })
 const clDepositsTotalAmount = computed(() => clDepositsData.value?.[1])
 
-// Execution  Layer Withdrawals data
+// Execution Layer Withdrawals data
 const elWithdrawalsQueryParams = ref<TableQueryParams>({
   limit: 5,
   sort: 'timestamp:desc',
@@ -272,6 +270,33 @@ const elWithdrawals = computed(() => {
   return elWithdrawalsData.value?.[0]
 })
 const elWithdrawalsTotalAmount = computed(() => elWithdrawalsData.value?.[1])
+
+// Consensus Layer Withdrawals data
+const clWithdrawalsQueryParams = ref<TableQueryParams>({
+  limit: 5,
+  sort: 'timestamp:desc',
+})
+const {
+  data: clWithdrawalsData,
+  refresh: refreshClWithdrawalsData,
+  status: clWithdrawalsDataStatus,
+} = useAsyncData('cl_withdrawals', () => {
+  return Promise.all([
+    dashboardData.fetchClWithdrawals(
+      dashboardKey.value,
+      clWithdrawalsQueryParams.value,
+    ),
+    dashboardData.fetchClWithdrawalsTotalAmount(dashboardKey.value),
+  ])
+},
+{
+  immediate: false,
+  watch: [ clWithdrawalsQueryParams ],
+})
+const clWithdrawals = computed(() => {
+  return clWithdrawalsData.value?.[0]
+})
+const clWithdrawalsTotalAmount = computed(() => clWithdrawalsData.value?.[1])
 
 // Execution Layer Consolidations data
 const elConsolidationsQueryParams = ref<TableQueryParams>({
@@ -334,6 +359,7 @@ const refreshActiveTab = () => {
       break
     case '#withdrawals':
       refreshElWithdrawalsData()
+      refreshClWithdrawalsData()
       break
   }
 }
@@ -418,6 +444,16 @@ watch(
             :el-withdrawals
             :el-withdrawals-total-amount
             :is-loading="elWithdrawalsDataStatus === 'pending'"
+          />
+          <BcIcon
+            name="arrow-down"
+            class="down_icon"
+          />
+          <DashboardTableClWithdrawals
+            v-model:query="clWithdrawalsQueryParams"
+            :cl-withdrawals
+            :cl-withdrawals-total-amount
+            :is-loading="clWithdrawalsDataStatus === 'pending'"
           />
         </template>
         <template #tab-panel-consolidations>
