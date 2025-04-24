a6bbc27745fbdae2c70048227b0b333075d67b87
Tangui-Bitfly <181825613+Tangui-Bitfly@users.noreply.github.com>
2025-04-24 11:10:42 +0200

    fix(transformer): updates dependencies and adds EL data for pectra (#1490)
    * fix(transformer):  updates dependencies and adds EL data for pectra
    * feat(transformer): open door for configurable system contracts
    * refactor(transformer): better filter to find new requests

---
+++ v2/backend/pkg/commons/db/migrations/postgres/20250407121321_add_el_requests_columns.sql
@@ -0,0 +1,35 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE eth1_consolidation_requests ADD COLUMN IF NOT EXISTS itx_index int4 not null;
+ALTER TABLE eth1_consolidation_requests ADD COLUMN IF NOT EXISTS from_address bytea NOT NULL;
+ALTER TABLE eth1_consolidation_requests ADD COLUMN IF NOT EXISTS fee BIGINT NOT NULL;
+
+ALTER TABLE eth1_consolidation_requests DROP CONSTRAINT IF EXISTS eth1_consolidation_requests_pkey;
+ALTER TABLE eth1_consolidation_requests ADD CONSTRAINT eth1_consolidation_requests_pkey UNIQUE (tx_hash, tx_index, itx_index);
+
+ALTER TABLE eth1_consolidation_requests ADD COLUMN id BIGSERIAL;
+ALTER TABLE eth1_consolidation_requests ADD CONSTRAINT eth1_consolidation_requests_id_key UNIQUE (id);
+
+ALTER TABLE eth1_withdrawal_requests ADD COLUMN IF NOT EXISTS itx_index int4 not null;
+ALTER TABLE eth1_withdrawal_requests ADD COLUMN IF NOT EXISTS from_address bytea NOT NULL;
+ALTER TABLE eth1_withdrawal_requests ADD COLUMN IF NOT EXISTS fee BIGINT NOT NULL;
+
+ALTER TABLE eth1_withdrawal_requests DROP CONSTRAINT IF EXISTS eth1_withdrawal_requests_pkey;
+ALTER TABLE eth1_withdrawal_requests ADD CONSTRAINT eth1_withdrawal_requests_pkey UNIQUE (tx_hash, tx_index, itx_index);
+ALTER TABLE eth1_withdrawal_requests ADD COLUMN id BIGSERIAL;
+ALTER TABLE eth1_withdrawal_requests ADD CONSTRAINT eth1_withdrawal_requests_id_key UNIQUE (id);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE eth1_consolidation_requests DROP COLUMN itx_index;
+ALTER TABLE eth1_consolidation_requests DROP COLUMN from_address;
+ALTER TABLE eth1_consolidation_requests DROP COLUMN fee;
+ALTER TABLE eth1_consolidation_requests DROP COLUMN id;
+
+ALTER TABLE eth1_withdrawal_requests DROP COLUMN itx_index;
+ALTER TABLE eth1_withdrawal_requests DROP COLUMN from_address;
+ALTER TABLE eth1_withdrawal_requests DROP COLUMN fee;
+ALTER TABLE eth1_withdrawal_requests DROP COLUMN id;
+-- +goose StatementEnd
--- v2/backend/pkg/commons/db2/bridge.go
+++ v2/backend/pkg/commons/db2/bridge.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// BridgeStore contains all the queued requests made on execution layer that impact the consensus layer
 type BridgeStore interface {
 	SetConsolidationRequests(request ConsolidationRequest) error
 	SetWithdrawalRequest(request WithdrawalRequest) error
@@ -21,10 +22,10 @@ func NewBridgeStore(db *sqlx.DB) *BridgeSQLStore {
 
 func (store BridgeSQLStore) SetConsolidationRequests(request ConsolidationRequest) error {
 	_, err := store.db.Exec(`
-		INSERT INTO eth1_consolidation_requests (tx_hash, tx_index, block_number, block_ts, source_address, source_pubkey, target_pubkey) VALUES ($1, $2, $3, $4, $5, $6, $7)
-		ON CONFLICT (tx_hash, tx_index) DO UPDATE
-		SET block_number = EXCLUDED.block_number, block_ts = EXCLUDED.block_ts, source_address = EXCLUDED.source_address, source_pubkey = EXCLUDED.source_pubkey, target_pubkey = EXCLUDED.target_pubkey
-	`, request.TxHash, request.TxIndex, request.BlockNumber, request.BlockTimestamp, request.SourceAddress, request.SourcePubKey, request.TargetPubKey)
+		INSERT INTO eth1_consolidation_requests (tx_hash, tx_index, itx_index, block_number, block_ts, from_address, fee, source_address, source_pubkey, target_pubkey) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+		ON CONFLICT (tx_hash, tx_index, itx_index) DO UPDATE
+		SET block_number = EXCLUDED.block_number, block_ts = EXCLUDED.block_ts, from_address = EXCLUDED.from_address, fee = EXCLUDED.fee, source_address = EXCLUDED.source_address, source_pubkey = EXCLUDED.source_pubkey, target_pubkey = EXCLUDED.target_pubkey
+	`, request.TxHash, request.TxIndex, request.ItxIndex, request.BlockNumber, request.BlockTimestamp, request.From, request.Fee, request.SourceAddress, request.SourcePubKey, request.TargetPubKey)
 	if err != nil {
 		return err
 	}
@@ -33,10 +34,10 @@ func (store BridgeSQLStore) SetConsolidationRequests(request ConsolidationReques
 
 func (store BridgeSQLStore) SetWithdrawalRequest(request WithdrawalRequest) error {
 	_, err := store.db.Exec(`
-		INSERT INTO eth1_withdrawal_requests (tx_hash, tx_index, block_number, block_ts, source_address, validator_pubkey, amount) VALUES ($1, $2, $3, $4, $5, $6, $7)
-		ON CONFLICT (tx_hash, tx_index) DO UPDATE
-		SET block_number = EXCLUDED.block_number, block_ts = EXCLUDED.block_ts, source_address = EXCLUDED.source_address, validator_pubkey = EXCLUDED.validator_pubkey, amount = EXCLUDED.amount
-	`, request.TxHash, request.TxIndex, request.BlockNumber, request.BlockTimestamp, request.SourceAddress, request.ValidatorPubKey, request.Amount)
+		INSERT INTO eth1_withdrawal_requests (tx_hash, tx_index, itx_index, block_number, block_ts, from_address, fee, source_address, validator_pubkey, amount) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+		ON CONFLICT (tx_hash, tx_index, itx_index) DO UPDATE
+		SET block_number = EXCLUDED.block_number, block_ts = EXCLUDED.block_ts, from_address = EXCLUDED.from_address, fee = EXCLUDED.fee, source_address = EXCLUDED.source_address, validator_pubkey = EXCLUDED.validator_pubkey, amount = EXCLUDED.amount
+	`, request.TxHash, request.TxIndex, request.ItxIndex, request.BlockNumber, request.BlockTimestamp, request.From, request.Fee, request.SourceAddress, request.ValidatorPubKey, request.Amount)
 	if err != nil {
 		return err
 	}
