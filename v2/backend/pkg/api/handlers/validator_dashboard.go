fb322d2ac810939705661114d379e4e976cd76a5
remoterami <142154971+remoterami@users.noreply.github.com>
2025-04-29 14:09:05 +0200

    Merge branch 'BEDS-1372/da-withdrawals'

--- v2/backend/pkg/api/handlers/validator_dashboard.go
+++ v2/backend/pkg/api/handlers/validator_dashboard.go
@@ -255,6 +255,7 @@ func (i *inputGetValidatorDashboardExecutionLayerWithdrawals) Validate(params ma
 	i.dashboardId = v.checkDashboardId(params["dashboard_id"])
 	i.protocolModes = v.checkProtocolModes(params["modes"])
 	i.sort = checkSort[enums.VDBWithdrawalsElColumn](&v, params["sort"])
+	i.Paging = v.checkPagingMap(params)
 	return v.AsError()
 }
 
@@ -343,6 +344,7 @@ func (i *inputGetValidatorDashboardConsensusLayerWithdrawals) Validate(params ma
 	i.dashboardId = v.checkDashboardId(params["dashboard_id"])
 	i.protocolModes = v.checkProtocolModes(params["modes"])
 	i.sort = checkSort[enums.VDBWithdrawalsClColumn](&v, params["sort"])
+	i.Paging = v.checkPagingMap(params)
 	return v.AsError()
 }
 
--- v2/backend/pkg/api/types/data_access.go
+++ v2/backend/pkg/api/types/data_access.go
@@ -107,14 +107,21 @@ type ValidatorDutiesCursor struct {
 	Reward decimal.Decimal
 }
 
-type WithdrawalsCursor struct {
+type ELWithdrawalsCursor struct {
 	GenericCursor
 
-	Slot            uint64
-	WithdrawalIndex uint64
-	Index           uint64
-	Recipient       []byte
-	Amount          uint64
+	BlockQueued uint64
+	TxIndex     uint64
+	ITxIndex    uint64
+	Amount      decimal.Decimal
+}
+
+type CLWithdrawalsCursor struct {
+	GenericCursor
+
+	Slot      uint64
+	SlotIndex uint64
+	Amount    decimal.Decimal
 }
 
 type ELConsolidationsCursor struct {
