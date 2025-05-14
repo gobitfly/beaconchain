2ca59311e2c5473273aa290e1c8c630ad2e23b20
Lucca Dukic <109136188+LuccaBitfly@users.noreply.github.com>
2025-05-14 15:30:44 +0200

    refactor: remove csrf middleware
    - Since the API uses no HTML forms and validates the Content-Type of all unsafe requests, the middleware is unnecessary.
    - Added linter rule preventing any form parsing being added in the future.
    - Proper CORS config to be maintained with or without CSRF middleware anyways.

--- backend/pkg/api/auth.go
+++ backend/pkg/api/auth.go
@@ -1,16 +1,13 @@
 package api
 
 import (
-	"encoding/hex"
 	"net/http"
 	"time"
 
 	"github.com/alexedwards/scs/redisstore"
 	"github.com/alexedwards/scs/v2"
-	"github.com/gobitfly/beaconchain/pkg/commons/log"
 	"github.com/gobitfly/beaconchain/pkg/commons/types"
 	"github.com/gomodule/redigo/redis"
-	"github.com/gorilla/csrf"
 )
 
 var day time.Duration = time.Hour * 24
@@ -55,37 +52,6 @@ func getSlidingSessionExpirationMiddleware(scs *scs.SessionManager) func(http.Ha
 			next.ServeHTTP(w, r)
 		})
 	}
-}
-
-// returns goriila/csrf middleware with the given config settings
-func getCsrfProtectionMiddleware(cfg *types.Config) func(http.Handler) http.Handler {
-	csrfBytes, err := hex.DecodeString(cfg.Frontend.CsrfAuthKey)
-	if err != nil {
-		log.Fatal(err, "error decoding cfg.Frontend.CsrfAuthKey, set it to a valid hex string", 0)
-	}
-	if len(csrfBytes) == 0 {
-		log.Warn("CSRF auth key is empty, unsafe requests will not work! Set cfg.Frontend.Debug to true to disable CSRF protection or cfg.Frontend.CsrfAuthKey.")
-	}
-	sameSite := csrf.SameSiteStrictMode
-	if cfg.Frontend.SessionSameSiteNone {
-		sameSite = csrf.SameSiteNoneMode
-	}
-
-	return csrf.Protect(
-		csrfBytes,
-		csrf.Secure(!cfg.Frontend.CsrfInsecure),
-		csrf.Path("/"),
-		csrf.Domain(cfg.Frontend.SessionCookieDomain),
-		csrf.SameSite(sameSite),
-	)
-}
-
-// returns a middleware that injects the CSRF token into the response headers
-func csrfInjecterMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("X-CSRF-Token", csrf.Token(r))
-		next.ServeHTTP(w, r)
-	})
 }
 
 func contentTypeMiddleware(next http.Handler) http.Handler {
