6eb2c0e6222b050681139be4752bdb2ea0f3c8ba
invis-bitfly <162128378+invis-bitfly@users.noreply.github.com>
2025-05-21 17:19:17 +0200

    fix(migrations): drop materialized view before altering type

--- v2/backend/pkg/commons/db/migrations/postgres/20250521170353_convert_int2_to_int4_in_requests_tables.sql
+++ v2/backend/pkg/commons/db/migrations/postgres/20250521170353_convert_int2_to_int4_in_requests_tables.sql
@@ -1,5 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
+DROP MATERIALIZED VIEW IF EXISTS cached_blocks_deposit_requests_lookup;
 ALTER TABLE blocks_deposit_requests_v2 ALTER COLUMN index_processed TYPE int4;
 ALTER TABLE blocks_consolidation_requests_v2 ALTER COLUMN index_processed TYPE int4;
 ALTER TABLE blocks_withdrawal_requests_v2 ALTER COLUMN index_queued TYPE int4;
@@ -7,6 +8,7 @@ ALTER TABLE blocks_withdrawal_requests_v2 ALTER COLUMN index_queued TYPE int4;
 
 -- +goose Down
 -- +goose StatementBegin
+DROP MATERIALIZED VIEW IF EXISTS cached_blocks_deposit_requests_lookup;
 ALTER TABLE blocks_deposit_requests_v2 ALTER COLUMN index_processed TYPE int2;
 ALTER TABLE blocks_consolidation_requests_v2 ALTER COLUMN index_processed TYPE int2;
 ALTER TABLE blocks_withdrawal_requests_v2 ALTER COLUMN index_queued TYPE int2;
