ca892d5dd658ac87dd12876225b5d3f1f0e74601
invis-bitfly <162128378+invis-bitfly@users.noreply.github.com>
2025-05-30 10:44:44 +0200

    refactor(migrations): turn hotfix migrations into stubs

--- backend/pkg/commons/db/migrations/clickhouse/20250521145305_finalize_electra_fork_epoch_backfill_part_2.sql
+++ backend/pkg/commons/db/migrations/clickhouse/20250521145305_finalize_electra_fork_epoch_backfill_part_2.sql
@@ -1,8 +1,8 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE _final_validator_dashboard_data_epoch
-    MATERIALIZE COLUMN roi_dividend settings alter_sync=0;
+
 -- +goose StatementEnd
+
 
 -- +goose Down
 -- +goose StatementBegin
