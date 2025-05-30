bb93d55bd65af9c6d3737b58a76af61db57361d6
Invis [Bitfly] <162128378+invis-bitfly@users.noreply.github.com>
2025-05-30 10:53:18 +0200

    refactor(migrations): turn hotfix migrations into stubs (#1611)

--- v2/backend/pkg/commons/db/migrations/clickhouse/20250520135931_finalize_electra_fork_epoch_backfill.sql
+++ v2/backend/pkg/commons/db/migrations/clickhouse/20250520135931_finalize_electra_fork_epoch_backfill.sql
@@ -1,16 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
--- +goose ENVSUB ON
-ALTER TABLE _final_validator_dashboard_data_epoch
-    UPDATE 
-        withdrawals_count = withdrawals_count + dictGet(_dict_backfill_electra_fork_epoch_events, 'withdrawals_count', (epoch_timestamp, validator_index)),
-        withdrawals_amount = withdrawals_amount + dictGet(_dict_backfill_electra_fork_epoch_events, 'withdrawals_amount', (epoch_timestamp, validator_index))
-    where epoch_timestamp = ${ELECTRA_FORK_EPOCH_TIMESTAMP?please set to the epoch timestamp AFTER the dashboard exporter has prepared the dictionary. set to random date if backfill is not required (i.e the overall export was started after this fix was added)} settings alter_sync=2, mutations_sync=2, database_replicated_enforce_synchronous_settings=1;
--- +goose ENVSUB OFF
+
 -- +goose StatementEnd
+
 
 -- +goose Down
 -- +goose StatementBegin
--- purposefully fail
-select * from "downgrade not supported"."manual re-export recommended"
+
 -- +goose StatementEnd
