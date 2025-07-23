d8b49737b30562096bc02be283956d11494f9072
Invis [Bitfly] <162128378+invis-bitfly@users.noreply.github.com>
2025-07-23 14:08:02 +0200

    Revert "fix(migrations): remove gnosis db mention"
    This reverts commit b88cb65402a1039b346931f58650f3ce8b993ab1.

--- backend/pkg/commons/db/migrations/clickhouse/20250721104442_improve_status_reports_filtering.sql
+++ backend/pkg/commons/db/migrations/clickhouse/20250721104442_improve_status_reports_filtering.sql
@@ -6,10 +6,10 @@ ALTER TABLE status_reports ADD INDEX event_id_index event_id Type set(32) settin
 ALTER TABLE status_reports MATERIALIZE INDEX event_id_index;
 -- +goose StatementEnd
 -- +goose StatementBegin
-ALTER TABLE status_reports ADD INDEX expires_at_index expires_at Type minmax() settings mutations_sync=2, alter_sync=1;
+ALTER TABLE gnosis.status_reports ADD INDEX expires_at_index expires_at Type minmax() settings mutations_sync=2, alter_sync=1;
 -- +goose StatementEnd
 -- +goose StatementBegin
-ALTER TABLE status_reports MATERIALIZE INDEX expires_at_index;
+ALTER TABLE gnosis.status_reports MATERIALIZE INDEX expires_at_index;
 -- +goose StatementEnd
 
 -- +goose Down
