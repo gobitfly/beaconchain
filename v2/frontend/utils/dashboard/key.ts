ea5d9867bf6c17938778f0334e42bcf1a295896b
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-08-28 10:02:13 +0200

    fix: `isGuestDashboard` utility function returning `true` for `shared-dashboards`

--- v2/frontend/utils/dashboard/key.ts
+++ v2/frontend/utils/dashboard/key.ts
@@ -2,6 +2,9 @@ export function isGuestDashboardKey(value?: string): boolean {
   if (!value) {
     return true
   }
+  if (value.startsWith('v-')) {
+    return false
+  }
 
   const id = parseInt(value)
   return isNaN(id)
