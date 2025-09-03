3a3ffd3b7bd0c6b4fc05e53d53ef5edb4b6e656b
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-09-03 17:02:01 +0200

    feat: add `medium primary Basebutton` in `BaseLayer`

--- frontend/layers/base/app/components/BaseButton.vue
+++ frontend/layers/base/app/components/BaseButton.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 const { variant = 'primary' } = defineProps<{
-  // isDisabled: boolean,
   variant?: 'primary' | 'secondary',
 }>()
 </script>
 
 <template>
   <button
-    class="bg-linear-to-b py-md px-sm rounded-full text-sm opacity-90 hover:opacity-95 disabled:opacity-40 aria-disabled:opacity-40 active:opacity-80"
+    class="bg-linear-to-b py-md px-sm rounded-full text-sm font-semibold opacity-90 hover:opacity-95 disabled:opacity-40 aria-disabled:opacity-40 active:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-300"
     :class="[
       variant === 'primary' && 'from-gray-100 to-gray-300 text-black',
     ]"
--- frontend/layers/base/app/layouts/base.vue
+++ frontend/layers/base/app/layouts/base.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="font-urbanist">
     <slot />
   </div>
 </template>
