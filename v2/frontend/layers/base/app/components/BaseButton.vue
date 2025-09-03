4d38c783eb0e452ae5036afdc94daa622a5ad2be
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-09-03 16:29:41 +0200

    feat: add `primary button` in `base layer`

---
+++ v2/frontend/layers/base/app/components/BaseButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { variant = 'primary' } = defineProps<{
+  // isDisabled: boolean,
+  variant?: 'primary' | 'secondary',
+}>()
+</script>
+
+<template>
+  <button
+    class="bg-linear-to-b py-md px-sm rounded-full text-sm opacity-90 hover:opacity-95 disabled:opacity-40 aria-disabled:opacity-40 active:opacity-80"
+    :class="[
+      variant === 'primary' && 'from-gray-100 to-gray-300 text-black',
+    ]"
+  >
+    <span class="px-lg">
+      <slot />
+    </span>
+  </button>
+</template>
---
+++ v2/frontend/layers/base/app/pages/test.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="font-urbanist">
+    <NuxtLayout name="base">
+      hello from test
+      <div>
+        <BaseButton
+          aria-disabled="true"
+          variant="primary"
+        >
+          Login
+        </BaseButton>
+      </div>
+      <div>
+        <BaseButton variant="secondary">
+          Login
+        </BaseButton>
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
