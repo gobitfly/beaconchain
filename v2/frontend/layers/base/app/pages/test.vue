59a9436bdb91d86a77818c679ccd21ee77c2f28d
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-09-03 17:02:01 +0200

    feat: add `medium primary Basebutton` in `BaseLayer`

--- v2/frontend/layers/base/app/pages/test.vue
+++ v2/frontend/layers/base/app/pages/test.vue
@@ -2,22 +2,36 @@
 </script>
 
 <template>
-  <div class="font-urbanist">
+  <div>
     <NuxtLayout name="base">
-      hello from test
-      <div>
-        <BaseButton
-          aria-disabled="true"
-          variant="primary"
-        >
-          Login
-        </BaseButton>
-      </div>
-      <div>
-        <BaseButton variant="secondary">
-          Login
-        </BaseButton>
-      </div>
+      Primary Button
+      <section class="grid gap-sm">
+        <div>
+          <BaseButton
+            variant="primary"
+          >
+            Login
+          </BaseButton>
+        </div>
+        <div>
+          <BaseButton
+            aria-disabled="true"
+            variant="primary"
+          >
+            Login
+          </BaseButton>
+          (aria-disabled=true)
+        </div>
+        <div>
+          <BaseButton
+            disabled
+            variant="primary"
+          >
+            Login
+          </BaseButton>
+          (disabled)
+        </div>
+      </section>
     </NuxtLayout>
   </div>
 </template>
