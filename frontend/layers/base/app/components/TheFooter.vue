2bdc0670d8eaa73a4e86e6cfc0b45ff89dd90e49
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-09-10 15:21:52 +0200

    feat: add first draft of `navigation` to `base layout`
    See: DF-91

---
+++ frontend/layers/base/app/components/TheFooter.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="bg-gray-300">
+    <slot />
+  </footer>
+</template>
+
+<style scoped></style>
---
+++ frontend/layers/base/app/components/TheLogo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <span class="flex gap-sm">
+    <TheLogoMark
+      width="1.25rem"
+      class="aspect-square"
+    />
+    <TheLogoType
+      width="6.25rem"
+    />
+  </span>
+</template>
