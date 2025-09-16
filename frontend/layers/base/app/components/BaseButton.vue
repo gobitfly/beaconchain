b88d917718330d1da62908db045f22d0a14ec654
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-09-16 09:27:30 +0200

    feat: add `explorer section` to `landing-page`
    See: DF-95

--- frontend/layers/base/app/components/BaseButton.vue
+++ frontend/layers/base/app/components/BaseButton.vue
@@ -5,6 +5,7 @@ const {
   size = 'md',
   variant = 'primary',
 } = defineProps<{
+  full?: boolean,
   leadingIcon?: IconName,
   size?: 'md' | 'xl',
   trailingIcon?: IconName,
@@ -14,13 +15,14 @@ const {
 
 <template>
   <button
-    class="flex items-center bg-linear-to-b rounded-full font-semibold disabled:opacity-40 aria-disabled:opacity-40 active:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-300"
+    class="flex justify-center items-center bg-linear-to-b rounded-full font-semibold disabled:opacity-40 aria-disabled:opacity-40 active:opacity-80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-300"
     :class="[
       variant === 'primary' && 'from-gray-700 to-gray-900 text-white dark:from-gray-100 dark:to-gray-300 dark:text-black opacity-90 hover:opacity-95',
       variant === 'secondary' && 'from-gray-300 to-gray-200 text-black dark:from-charcoal-600 dark:to-charcoal-700 dark:text-white opacity-90 hover:opacity-95',
       variant === 'branded' && 'from-brand-500 to-brand-700 text-white hover:opacity-90',
       size === 'md' && 'text-sm py-md px-sm',
       size === 'xl' && 'text-md py-xl px-3xl',
+      full ? 'w-full' : 'w-fit',
     ]"
   >
     <LazyBaseIcon
--- frontend/layers/products/app/pages/p/index.vue
+++ frontend/layers/products/app/pages/p/index.vue
@@ -26,6 +26,7 @@ const items: BaseNavigationItem[] = [
   },
 ]
 const navigationLeft = useTemplateRef<HTMLDialogElement>('navigationLeft')
+const { navigateToV1Login } = useV1Login()
 </script>
 
 <template>
@@ -42,36 +43,152 @@ const navigationLeft = useTemplateRef<HTMLDialogElement>('navigationLeft')
           :is-open
           @close="isOpen = false"
         />
-        <section>
-          <h2 class="text-center mt-[4rem]">
-            {{ $t('products.landing_page.search.title') }}
-          </h2>
-        </section>
-        <section>
-          <h2
-            :id="id.api"
-            class="text-center mt-[40rem]"
-          >
-            {{ $t('products.landing_page.api.title') }}
-          </h2>
-        </section>
-        <section>
-          <h2
-            :id="id.stackingHub"
-            class="text-center mt-[40rem]"
-          >
-            {{ $t('products.landing_page.stacking_hub.title') }}
-          </h2>
-        </section>
-        <section>
-          <h2
-            :id="id.explorer"
-            class="text-center mt-[40rem]"
-          >
-            {{ $t('products.landing_page.explorer.title') }}
-          </h2>
-        </section>
       </template>
+      <section class="mt-11xl p-md">
+        <h2 class="text-center">
+          {{ $t('products.landing_page.search.title') }}
+        </h2>
+      </section>
+      <section class="mt-11xl p-md">
+        <h2
+          :id="id.api"
+          class="text-center "
+        >
+          {{ $t('products.landing_page.api.title') }}
+        </h2>
+      </section>
+      <section class="mt-11xl p-md">
+        <h2
+          :id="id.stackingHub"
+          class="text-center "
+        >
+          {{ $t('products.landing_page.stacking_hub.title') }}
+        </h2>
+      </section>
+      <section
+        class="flex flex-col gap-4xl justify-center items-center mt-11xl text-center max-w-24xl mx-auto background-image p-md"
+      >
+        <BaseHeading
+          is="h2"
+          :id="id.explorer"
+          size="lg"
+          class=""
+        >
+          {{ $t('products.landing_page.explorer.title') }}
+        </BaseHeading>
+        <p>{{ $t('products.landing_page.explorer.description') }}</p>
+        <BaseButton
+          leading-icon="compass"
+          trailing-icon="arrow-up-right"
+          variant="branded"
+          size="xl"
+          @click="navigateToV1Login"
+        >
+          {{ $t('products.landing_page.explorer.action.go_to_explorer') }}
+        </BaseButton>
+        <div class="grid gap-6xl mt-7xl grid-cols-[repeat(auto-fit,minmax(224px,1fr))] w-full">
+          <span class="flex flex-col items-center">
+            <BaseIcon
+              name="clock"
+              class="size-6xl"
+            />
+            <BaseText
+              size="2xl"
+              class="mt-4xl"
+            >
+              {{ $t('products.landing_page.explorer.transaction_tracking.title') }}
+            </BaseText>
+            <BaseText
+              size="sm"
+              class="mt-md"
+              dimmed
+            >
+              {{ $t('products.landing_page.explorer.transaction_tracking.description') }}
+            </BaseText>
+          </span>
+          <span class="flex flex-col items-center">
+            <BaseIcon
+              name="wallet"
+              class="size-6xl"
+            />
+            <BaseText
+              size="2xl"
+              class="mt-4xl"
+            >
+              {{ $t('products.landing_page.explorer.wallet_tocken_insights.title') }}
+            </BaseText>
+            <BaseText
+              size="sm"
+              class="mt-md"
+              dimmed
+            >
+              {{ $t('products.landing_page.explorer.wallet_tocken_insights.description') }}
+            </BaseText>
+          </span>
+          <span class="flex flex-col items-center">
+            <BaseIcon
+              name="code"
+              class="size-6xl"
+            />
+            <BaseText
+              size="2xl"
+              class="mt-4xl"
+            >
+              {{ $t('products.landing_page.explorer.smart_contract_analysis.title') }}
+            </BaseText>
+            <BaseText
+              size="sm"
+              class="mt-md"
+              dimmed
+            >
+              {{ $t('products.landing_page.explorer.smart_contract_analysis.description') }}
+            </BaseText>
+          </span>
+          <span class="flex flex-col items-center">
+            <BaseIcon
+              name="gas-station"
+              class="size-6xl"
+            />
+            <BaseText
+              size="2xl"
+              class="mt-4xl"
+            >
+              {{ $t('products.landing_page.explorer.gas_fee_monitoring.title') }}
+            </BaseText>
+            <BaseText
+              size="sm"
+              class="mt-md"
+              dimmed
+            >
+              {{ $t('products.landing_page.explorer.gas_fee_monitoring.description') }}
+            </BaseText>
+          </span>
+        </div>
+      </section>
     </NuxtLayout>
   </div>
 </template>
+
+<style scoped>
+.background-image {
+  position: relative;
+
+  &:before {
+    opacity: 0.1;
+    content: '';
+    position: absolute;
+    inset: 0;
+    background:
+       linear-gradient(
+      to bottom,
+      var(--color-black) 0%,
+      transparent 30%,
+      transparent 70%,
+      var(--color-black) 100%
+    ),
+    url('/assets-2usdf/img/bg-chain.webp');
+    background-size: cover;
+    background-position: center;
+  }
+}
+</style>
