0e79149c07fbf66bd4d41955b37f3ce149ae62ab
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-09-16 09:27:30 +0200

    feat: add `explorer section` to `landing-page`
    See: DF-95

---
+++ frontend/layers/base/app/components/BaseHeading.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  is:
+    | 'h2',
+  size: 'lg',
+}>()
+</script>
+
+<template>
+  <component
+    :is
+    class="font-semibold text-balance"
+    :class="[
+      size === 'lg' && 'text-4xl tracking-[-.05rem]',
+    ]"
+  >
+    <slot />
+  </component>
+</template>
+
+<style scoped></style>
