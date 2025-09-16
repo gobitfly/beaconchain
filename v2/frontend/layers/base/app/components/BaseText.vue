1c3b679a03ccc9fb050966a394f34b268a8232b1
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-09-16 09:27:30 +0200

    feat: add `explorer section` to `landing-page`
    See: DF-95

---
+++ v2/frontend/layers/base/app/components/BaseText.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const {
+  is = 'span',
+  size = 'md',
+} = defineProps<{
+  dimmed?: boolean,
+  is?:
+    | 'span',
+  size?:
+    | '2xl'
+    | 'sm',
+}>()
+</script>
+
+<template>
+  <component
+    :is
+    class="font-semibold text-pretty"
+    :class="[
+      size === 'sm' && 'text-sm',
+      size === '2xl' && 'text-2xl',
+      dimmed && 'text-gray-400',
+    ]"
+  >
+    <slot />
+  </component>
+</template>
