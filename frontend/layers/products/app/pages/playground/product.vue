b380b5dd6e53d197b3f2c16abc88012ab55ff54b
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-09-23 12:05:54 +0200

    feat: add `testimonials section`  to  `playground page`
    See: DF-100

---
+++ frontend/layers/products/app/pages/playground/product.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'base',
+})
+</script>
+
+<template>
+  <ProductLandingpageSection>
+    <ProductLandingpageSectionTestimonials />
+  </ProductLandingpageSection>
+</template>
