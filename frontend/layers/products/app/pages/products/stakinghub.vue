2dda0995049f558050c9c5d4afa3ef9904f4ba37
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-09-23 14:30:16 +0200

    fix: add `product pages` and their backlinks

---
+++ frontend/layers/products/app/pages/products/stakinghub.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'base',
+  middleware: () => {
+    return navigateTo('/dashboard')
+  },
+})
+</script>
+
+<template>
+  <div>
+    Page: stakinghub
+  </div>
+</template>
+
+<style scoped></style>
