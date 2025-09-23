981bf2d1c947810083006b482f8b4e73d3dcf9bd
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-09-23 12:05:54 +0200

    feat: add `testimonials section`  to  `playground page`
    See: DF-100

---
+++ v2/frontend/layers/products/app/components/ProductLandingpageSectionTestimonials.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="py-3xl w-full">
+    <BaseCard>
+      <div class="flex flex-col text-center gap-y-4xl">
+        <p class="text-xl">
+          {{ $t('products.landing_page.testimonials.title') }}
+        </p>
+        <div class="flex flex-wrap sm:flex-row gap-x-5xl gap-y-3xl justify-center">
+          <div
+            v-for="i in 5"
+            :key="i"
+            class="flex items-center gap-md"
+          >
+            <TheLogoMark
+              width="3rem"
+              height="3rem"
+              class="aspect-square"
+            />
+            <TheLogoType
+              width="6.25rem"
+            />
+          </div>
+        </div>
+      </div>
+    </BaseCard>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
