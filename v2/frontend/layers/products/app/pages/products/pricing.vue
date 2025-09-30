8e26f6a4f0e63562339112f4a65b5a9e5fc1baf2
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-09-30 10:44:13 +0200

    feat(products/pricing): add navigation
    See: FE-109

--- v2/frontend/layers/products/app/pages/products/pricing.vue
+++ v2/frontend/layers/products/app/pages/products/pricing.vue
@@ -1,12 +1,51 @@
 <script setup lang="ts">
-definePageMeta({
-  layout: 'base',
+import type { BaseNavigationItem } from '~/layers/base/app/components/BaseNavigationItem.vue'
+
+useHead({
+  bodyAttrs: {
+    // enforcing dark mode as light mode is not ready yet
+    'data-theme': 'dark',
+  },
 })
+const { t: $t } = useTranslation()
+
+const isOpen = ref(false)
+const items: BaseNavigationItem[] = [
+  {
+    icon: 'file-code-2',
+    label: $t('products.api'),
+    to: '/product#api',
+  },
+  {
+    icon: 'coins',
+    label: $t('products.staking_hub'),
+    to: '/products#staking-hub',
+  },
+  {
+    icon: 'compass',
+    label: $t('products.explorer'),
+    to: 'products#explorer',
+  },
+]
 </script>
 
 <template>
   <div>
-    Page: pricing
+    <NuxtLayout name="base">
+      <template #header>
+        <BaseNavigation
+          :items
+          @open="isOpen = !isOpen"
+        />
+        <BaseNavigationLeft
+          ref="navigationLeft"
+          :items
+          :is-open
+          @close="isOpen = false"
+        />
+      </template>
+      Page: /products/pricing
+    </NuxtLayout>
   </div>
 </template>
 
