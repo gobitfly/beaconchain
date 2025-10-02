a25ab994cacd1bbb1151727a6b4a9484d8a08aa6
enzo-bitfly <194075446+enzo-bitfly@users.noreply.github.com>
2025-10-02 11:00:06 +0200

    feat: add `search filters`
    See: FE-93

--- v2/frontend/layers/products/app/components/BlockchainSearchInput.vue
+++ v2/frontend/layers/products/app/components/BlockchainSearchInput.vue
@@ -2,31 +2,66 @@
 import type { PostSearchRequest } from '~/types/api/search'
 import type { InternalPostSearchResponseWithChainId } from '~/layers/products/server/api/bff/search'
 
-export type SearchParams = Omit<PostSearchRequest, 'networks'> & { networks: ChainId[] }
+export type BlockchainSearchParams = Omit<PostSearchRequest, 'networks' | 'types'> & { networks: ChainId[], types: BlockchainSearchTypes }
+export type BlockchainSearchTypes = Exclude<NonNullable<PostSearchRequest['types']>[number], 'validator_list'>[]
 
 const {
   hasError,
   isLoading,
   results,
+  typeFilters,
 } = defineProps<{
   hasError?: boolean,
   isLoading: boolean,
   results?: InternalPostSearchResponseWithChainId['data'],
+  typeFilters: BlockchainSearchParams['types'],
 }>()
 
 const { t: $t } = useTranslation()
 
 const emit = defineEmits<{
-  (e: 'search', searchParams: SearchParams): void,
+  (e: 'search'): void,
 }>()
 
-const searchParams = defineModel<SearchParams>({
+const searchParams = defineModel<BlockchainSearchParams>({
   required: true,
 })
 
-const handleSearch = (input: string) => {
-  searchParams.value.input = input
-  emit('search', searchParams.value)
+const chips: { label: string, value: BlockchainSearchParams['types'][number] }[] = [
+  {
+    label: $t('products.landing_page.search.types.addresses'),
+    value: 'address',
+  },
+  {
+    label: $t('products.landing_page.search.types.transactions'),
+    value: 'transaction',
+  },
+  {
+    label: $t('products.landing_page.search.types.validators_indices'),
+    value: 'validator_by_index',
+  },
+  {
+    label: $t('products.landing_page.search.types.blocks'),
+    value: 'block',
+  },
+  {
+    label: $t('products.landing_page.search.types.tokens'),
+    value: 'token',
+  },
+]
+
+const handleSearch = () => {
+  emit('search')
+}
+
+const handleTypeFilterChange = () => {
+  // When all filter chips are selected, we also want to include all available types
+  // that are not included in the chips
+  if (chips.every(chip => searchParams.value.types.includes(chip.value))) {
+    searchParams.value.types = typeFilters
+  }
+
+  handleSearch()
 }
 </script>
 
@@ -41,6 +76,18 @@ const handleSearch = (input: string) => {
     :results
     @search="handleSearch"
   >
+    <template #dropdown-fixed-header>
+      <div class="hidden sm:block">
+        <BaseChipGroup
+          v-model="searchParams.types"
+          :items="chips"
+          :aria-label="$t('products.landing_page.search.filter_aria_label')"
+          @update:model-value="handleTypeFilterChange"
+        />
+        <hr class="mx-2xl text-gray-600">
+      </div>
+    </template>
+
     <template #result-group-label="{ label }">
       <span v-if="label === 'address'">{{ $t('products.landing_page.search.types.addresses') }}</span>
       <span v-else-if="label === 'block'">{{ $t('products.landing_page.search.types.blocks') }}</span>
@@ -49,7 +96,7 @@ const handleSearch = (input: string) => {
       <span v-else-if="label === 'slot'">{{ $t('products.landing_page.search.types.slots') }}</span>
       <span v-else-if="label === 'token'">{{ $t('products.landing_page.search.types.tokens') }}</span>
       <span v-else-if="label === 'transaction'">{{ $t('products.landing_page.search.types.transactions') }}</span>
-      <span v-else-if="label === 'validator'">{{ $t('products.landing_page.search.types.validators') }}</span>
+      <span v-else-if="label === 'validator'">{{ $t('products.landing_page.search.types.validators_indices') }}</span>
       <span v-else-if="label === 'validators_by_deposit_address'">
         {{ $t('products.landing_page.search.types.validator_deposit_addresses') }}
       </span>
