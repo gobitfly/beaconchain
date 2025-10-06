2877921660d483e3f8f66972361b690b6b6b1bba
enzo-bitfly <194075446+enzo-bitfly@users.noreply.github.com>
2025-10-06 15:10:46 +0200

    feat: add `hero section` to `api pricing page`
    See: FE-108

--- frontend/layers/base/app/components/BaseHeading.vue
+++ frontend/layers/base/app/components/BaseHeading.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-export type BaseHeadings = 'h2' | 'h3' | 'h4'
-
+export type BaseHeadings = 'h1' | 'h2' | 'h3' | 'h4'
 defineProps<{
   is: BaseHeadings,
   size:
     | '2xl'
     | 'lg'
     | 'md'
+    | 'xl'
     | 'xs'
     | 'xxs',
 }>()
@@ -18,6 +18,7 @@ defineProps<{
     class="font-bold text-balance"
     :class="[
       size === '2xl' && 'text-6xl tracking-[-.09rem]',
+      size === 'xl' && 'text-5xl tracking-[-.075rem]',
       size === 'lg' && 'text-4xl tracking-[-.05rem]',
       size === 'xs' && 'text-xl tracking-[-0.0063rem]',
       size === 'xxs' && 'text-md',
