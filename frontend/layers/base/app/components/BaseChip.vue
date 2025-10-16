58794e26139e1dac8a8b81af3b30f29b638074c4
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-10-16 15:43:58 +0200

    feat: add `prefill buttons` to search on `product landing page`
    See: FE-94

--- frontend/layers/base/app/components/BaseChip.vue
+++ frontend/layers/base/app/components/BaseChip.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import type { IconName } from '#layers/base/app/components/BaseIcon.vue'
+
 const {
   size = 'sm',
   variant = 'neutral',
 } = defineProps<{
+  icon?: IconName,
   isSelected: boolean,
   size?: 'sm',
   variant?: 'neutral',
@@ -12,12 +15,18 @@ const {
 <template>
   <button
     class="text-nowrap border-1 font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-brand-300"
+    type="button"
     :class="[
       variant === 'neutral' && 'bg-gray-100 dark:bg-gray-900 border-gray-200 dark:border-gray-800 text-gray-600 dark:text-gray-400 aria-pressed:bg-gray-200 dark:aria-pressed:bg-gray-700 aria-pressed:border-gray-300 dark:aria-pressed:border-gray-700 aria-pressed:text-black dark:aria-pressed:text-white aria-pressed:shadow-none',
       size === 'sm' && 'px-md py-xs rounded-md ',
+      icon && 'flex items-center gap-md',
     ]"
     :aria-pressed="isSelected"
   >
+    <BaseIcon
+      v-if="icon"
+      :name="icon"
+    />
     <slot />
   </button>
 </template>
