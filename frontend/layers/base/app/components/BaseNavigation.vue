bf0af1267f2e58d508612e9aa5e11ffb8bf4e201
marcel-bitfly <174338434+marcel-bitfly@users.noreply.github.com>
2025-10-24 14:24:22 +0200

    style(eslint): apply further `tailwindcss` improvements
    Resolve warnings after installation of `eslint-plugin-tailwindcss`

--- frontend/layers/base/app/components/BaseNavigation.vue
+++ frontend/layers/base/app/components/BaseNavigation.vue
@@ -20,7 +20,7 @@ const v1Domain = useV1Domain()
 
 <template>
   <nav class="isolate bg-gray-50 p-2xl dark:bg-transparent">
-    <div class="mx-auto grid max-w-8xl gap-2xl [&>*]:[grid-area:1/1]">
+    <div class="mx-auto grid max-w-8xl gap-2xl *:[grid-area:1/1]">
       <span class="z-10 flex w-fit items-center gap-2xl">
         <BaseButtonIcon
           screenreader-text="base.common.open_navigation"
